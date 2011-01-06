686d84b68f3fe3b24c0b75ed3686efec5882f4ba
Won-Ki Jeong <graphor@gmail.com>
2011-01-05 19:46:06 -0500

    ENH: GPU Image and Filters in pipeline
    GPU image and filter class support object factory framework
    
    - Pixel/buffer access functions in image class are changed to virtual functions
      so that GPUImage class can correctly override those functions when
      instantiated through Object Factory.
    
    - ToDo: We need global variable/fuction to check if GPU is available.
      We do not want to create GPU objects when GPU is not available.
    
      Now GPU classes use time stamp as well as dirty flags for pipeline
    
      Modification in existing itk classes:
    
        - GetBufferPointer() in Image class is now virtual
        - DataHasBeenGenerated() in DataObject class is now virtual
        - Added a operator=() to the TimeStamp class.
        - Modified API of the itkRealTimeClock to replace GetTimeStamp() with
          GetTimeInSeconds(). (XML migration page will have to be filled up).
          Moved GetTimeStamp() to the private section, to avoid confusion by
          hiding this method from the users of the RealTimeClock class.
    
    Change-Id: I328ff698ba819396f24fe95e3a6bc7d7fe3a39f3

---
+++ Code/GPU/itkGPUContextManager.h
@@ -0,0 +1,45 @@
+#ifndef __itkGPUContextManager_h
+#define __itkGPUContextManager_h
+
+#include "itkOclUtil.h"
+#include <itkLightObject.h>
+#include <CL/opencl.h>
+
+//
+// Singleton class for GPUContextManager
+//
+
+namespace itk
+{
+  class ITK_EXPORT GPUContextManager: public LightObject
+  {
+  public:
+
+    static GPUContextManager* GetInstance();
+    void DestroyInstance();
+
+    cl_command_queue GetCommandQueue(int i);
+
+    unsigned int GetNumCommandQueue() { return m_NumDevices; }
+
+    cl_context GetCurrentContext() { return m_Context; }
+
+    cl_device_id GetDeviceId(int i);
+
+  private:
+
+    GPUContextManager();
+    ~GPUContextManager();
+
+    cl_platform_id     m_Platform;
+    cl_context         m_Context;
+    cl_device_id*      m_Devices;
+    cl_command_queue*  m_CommandQueue; // one queue per device
+
+    cl_uint m_NumDevices, m_NumPlatforms;
+
+    static GPUContextManager* m_Instance;
+  };
+} // namespace itk
+
+#endif
