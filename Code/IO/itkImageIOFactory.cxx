7b91dd39e1ef243b57a89846844c32f4cb4dddc0
Luis Ibanez <luis.ibanez@kitware.com>
2011-02-09 21:57:09 -0500

    ENH: Ported BioFormat ImageIO.
    The ImageIO classes that interface to the Bio-Formats library have
    been moved into the Code/IO directory. They can be enabled with the
    CMake flag ITKIO_SUPPORT_BIOFORMATS_IMAGEIO.
    
    A FindBioFormats.cmake file has been added to locate the dependencies
    of the BioFormat ImageIO. This includes bio-formats, jace and jni.
    
    Change-Id: I6797ece2b46f8951d062d7ac38e67f77b246d5d9

--- Code/IO/itkImageIOFactory.cxx
+++ Code/IO/itkImageIOFactory.cxx
@@ -23,6 +23,9 @@
 #include "itkMutexLock.h"
 #include "itkMutexLockHolder.h"
 
+#if defined(ITKIO_SUPPORTS_BIOFORMATS_IMAGEIO)
+#include "itkBioFormatsImageIOFactory.h"
+#endif
 
 #if defined(ITKIO_SUPPORTS_BIORAD_IMAGEIO)
 #include "itkBioRadImageIOFactory.h"
@@ -146,6 +149,10 @@ ImageIOFactory::RegisterBuiltInFactories()
     MutexLockHolder< SimpleMutexLock > mutexHolder(mutex);
     if ( firstTime )
       {
+#if defined(ITKIO_SUPPORTS_BIOFORMATS_IMAGEIO)
+      ObjectFactoryBase::RegisterFactory( BioFormatsImageIOFactory::New() );
+#endif
+
 #if defined(ITKIO_SUPPORTS_BIORAD_IMAGEIO)
       // BioRad should be registered before GDCM
       ObjectFactoryBase::RegisterFactory( BioRadImageIOFactory::New() );
