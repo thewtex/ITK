e00cbc52781f7b2f1550802ebd347b33bc1306e0
Daniel Blezek <blezek.daniel@mayo.edu>
2011-02-18 10:55:45 -0600

    BUG: Was mixing named and unnamed semaphore calls, now named
    Unnamed semaphores are not supported on the Mac (and possibly Sun).
    Switching the POSIX (Pthreads) semaphores to named semaphores.
    
    BUG: debugging statements
    
    Added debugging statements to the PThread Semaphore code to help find
    the problems on certain platforms.
    
    Change-Id: I3bf8e7165d1d19627bd835439b1a93132b73911d

--- Code/Common/itkSemaphorePThreads.cxx
+++ Code/Common/itkSemaphorePThreads.cxx
@@ -16,6 +16,7 @@
  *
  *=========================================================================*/
 #include "itkSemaphore.h"
+#include "itkSimpleFastMutexLock.h"
 
 extern "C" {
 #include <stdio.h>
@@ -25,17 +26,8 @@ extern "C" {
 
 namespace itk
 {
+static SimpleFastMutexLock mutex;
 static int SemaphoreCount = 0;
-
-static std::string GetUniqueName()
-{
-  char   s[255];
-  time_t t = time(0);
-
-  snprintf(s, 254, "MACSEM%d%d", static_cast< int >( t ), SemaphoreCount);
-  SemaphoreCount++;
-  return std::string(s);
-}
 
 Semaphore::Semaphore ()
 {
@@ -45,16 +37,19 @@ Semaphore::Semaphore ()
 
 void Semaphore::Initialize(unsigned int value)
 {
-  std::string name = GetUniqueName();
   m_PThreadsSemaphoreRemoved = false;
 
-  m_Sema  = sem_open(name.c_str(), O_CREAT, 0x0644, value);
+  mutex.Lock();
+  time_t t = time(0);
+  snprintf(m_SemaphoreName, 254, "MACSEM%d%d", static_cast< int >( t ), SemaphoreCount);
+  SemaphoreCount++;
+  mutex.Unlock();
+
+  m_Sema  = sem_open(m_SemaphoreName, O_CREAT, 0x0644, value);
   if ( m_Sema == (sem_t *)SEM_FAILED )
     {
-    //  perror("FAILED WITH ERROR:" );
-    itkExceptionMacro( << "sem_open call failed on " << name.c_str() );
+    itkExceptionMacro( << "sem_open call failed on " << m_SemaphoreName );
     }
-
 }
 
 void Semaphore::Up()
@@ -67,7 +62,6 @@ void Semaphore::Up()
 
 void Semaphore::Down()
 {
-
   if ( sem_wait(m_Sema) != 0 )
     {
     itkExceptionMacro(<< "sem_wait call failed.");
@@ -85,126 +79,10 @@ Semaphore::~Semaphore()
 void Semaphore::Remove()
 {
   m_PThreadsSemaphoreRemoved = true;
-  if ( sem_destroy(m_Sema) != 0 )
+  if ( sem_unlink(m_SemaphoreName) != 0 )
     {
-    itkExceptionMacro(<< "sem_destroy call failed. ");
+    itkExceptionMacro(<< "sem_unlink call failed. ");
     }
 }
 
 } //end if namespace itk
-#ifdef ITK_USE_UNIX_IPC_SEMAPHORES
-
-// UnixIpcSemaphoreCreate: will return the semaphore id (system wide)
-// of the semaphore number (key) you give.
-// If no semaphore has been established for this number, one is created.
-int Semaphore::UnixIpcSemaphoreCreate(int unix_semaphore_key)
-{
-  int         sid = -1;
-  std::string s;
-
-  if ( ( sid = semget( (key_t)unix_semaphore_key, 1, 0666 | IPC_CREAT ) ) == -1 )
-    {
-    s =  "Error#%i in function UnixIpcSemaphoreCreate. - ";
-    switch ( errno )
-      {
-      case EEXIST:
-        s += "Semaphore already exists. - ";
-        break;
-      case ENOSPC:
-        s += "System imposed limit on the number of semaphores is exceeded - ";
-        break;
-      case EACCES:
-        s += "Permission is denied - ";
-        break;
-      }
-    itkExceptionMacro( << s.c_str() );
-    }
-
-  return ( sid );
-}
-
-// UnixIpcSemaphoreDown: the semaphore signal operation.
-// sid must be the system wide semaphore number
-// returned by UnixIpcSemaphoreCreate above
-void Semaphore::UnixIpcSemaphoreDown(int sid)
-{
-  Semaphore::UnixIpcSemaphoreCall(sid, -1);
-}
-
-// UnixIpcSemaphoreUp: the semaphore release operation.
-// sid must be the system wide semaphore number
-// returned by UnixIpcSemaphoreCreate above
-void Semaphore::UnixIpcSemaphoreUp(int sid)
-{
-  Semaphore::UnixIpcSemaphoreCall(sid, 1);
-}
-
-// UixIpcSemaphoreRemove: remove a semaphore from the system.
-// sid must be the system wide semaphore number
-// returned from UnixIpcSemaphoreCreate
-void Semaphore::UnixIpcSemaphoreRemove(int sid)
-{
-  std::string s;
-
-  if ( semctl(sid, 0, IPC_RMID, 0) == -1 )
-    {
-    s =  "Error removing semaphore %i - ";
-    switch ( errno )
-      {
-      case EINVAL:
-        s += "Semaphore id#is not valid. - ";
-        break;
-      case EACCES:
-        s += "Permission is denied - ";
-        break;
-      case EPERM:
-        s += "Permission is denied - ";
-        break;
-      }
-    itkExceptionMacro ( << s.c_str() );
-    }
-}
-
-// UnixIpcSemaphoreCall: makes the system call semop for your given
-// semaphore and operation.
-void Semaphore::UnixIpcSemaphoreCall(int sid, int op)
-{
-  struct sembuf sb;
-  std::string   s;
-
-  sb.sem_num = 0;
-  sb.sem_op = op;
-  sb.sem_flg = 0;
-  if ( semop(sid, &sb, 1) == -1 )
-    {
-    if ( op == -1 )
-      {
-      s =  "Error %i in UnixIpcSemaphoreDown call for semaphore %i - ";
-      }
-    else
-      {
-      s =  "Error %i in UnixIpcSemaphoreUp call for semaphore %i - ";
-      }
-    switch ( errno )
-      {
-      case EINVAL:
-        s += "Semaphore id#is not valid. -";
-        break;
-      case EFBIG:
-        s += "Invalid sem_num for semaphore - ";
-        break;
-      case EACCES:
-        s += "Permission denied for semaphore - ";
-        break;
-      case EAGAIN:
-        s += "The operation would result in suspension of the calling process but NOWAIT is true.";
-        break;
-      case EIDRM:
-        s += "The semid is removed from the system.";
-        break;
-      }
-    itkExceptionMacro( << s.c_str() );
-    }
-}
-
-#endif
