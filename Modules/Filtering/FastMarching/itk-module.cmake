a18e80739b181f1836c188e23a4328818a27b7a0
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-04-04 16:14:25 -0400

    WIP: Integration of the refactored work done for FastMarching
    With the new framework, fast marching can be computed on itk::Image,
    and itk::QuadEdgeMesh. When running on itk::Image, one can constrain the
    topology of the front. One can also make its own stopping criterion
    to stop the evolution of the front.
    
    Stopping criterion are templated over the same type as the one used
    to define the FastMarching filter. It is now possible to make funky
    stopping criterion based on the volume of the front (if on a 3d image),
    surface of the front (if on a surface mesh).
    
    FastMarchingTraits is partially specialized to provide the revelant
    types to use when Image, QuadEdgeMesh or Mesh or used.
    
    FastMarchingUpwindGradientImageFilter* now computes the gradient image. If
    users don't want the gradient image to be computed as the front propagates,
    they should rather use standard filters. Note that now the resulting
    GradientImage is one output of the filter.
    
    Add one helper class FastMarchingImageToNodePairContainerAdaptor, that
    provides a convenient way of converting images into (node pair)
    containers to set up the evolution.
    
    With this change both frameworks (ITKv3 and refactored one) co-exist,
    classes have different name to avoid conflicts.
    
    Change-Id: I8e6a1cd6e620621062076ede903fd7d11d9b1437

--- Modules/Filtering/FastMarching/itk-module.cmake
+++ Modules/Filtering/FastMarching/itk-module.cmake
@@ -1,1 +1,11 @@
-itk_module(ITK-FastMarching DEPENDS ITK-Common TEST_DEPENDS ITK-TestKernel)
+itk_module(ITK-FastMarching DEPENDS
+ITK-Common
+ITK-QuadEdgeMesh
+ITK-QuadEdgeMeshFiltering # cause of QuadEdgeMeshToQuadEdgeMeshFilter
+ITK-ConnectedComponents
+### test ###
+TEST_DEPENDS
+ITK-IO-NIFTI
+ITK-Review # cause of LabelContourImageFilter
+ITK-TestKernel
+)
