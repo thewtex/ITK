258c6ce5ad2bc6bdd6039dfa6b0ca2cc0eac8074
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2011-04-30 19:43:47 +0200

    ENH: unify the numeric traits API
    All the numeric traits provide the same kind of API. The smallest
    subset is for the varialbe length arrays. Some methods are added for
    the fixed length array and some other for the scalars.
    
      VariableLengthArray
            ^
            |
       FixedLengthArray
            ^
            |
          Scalar
    
    Numeric traits for variable length arrays provide the methods:
    
      static const Self max(const Self & a)
      static const Self min(const Self & a)
      static const Self ZeroValue(const Self  & a)
      static const Self OneValue(const Self & a)
      static const Self NonpositiveMin(const Self & a)
      static void SetLength(Self & m, const unsigned int s)
      static unsigned int GetLength(const Self & m)
      template<class TArray> static void AssignArray( const Self & v, TArray & mv )
    
    Numeric traits for fixed length array add:
    
      static const Self max()
      static const Self min()
      static const Self NonpositiveMin()
      static const Self ZeroValue()
      static const Self OneValue()
      static unsigned int GetLength()
    
    And numeric traits for scalars add:
    
      static bool IsPositive(Self val)
      static bool IsNonpositive(Self val)
      static bool IsNegative(Self val)
      static bool IsNonnegative(Self val)
    
    This should help to write code which works with VariableLengthVector
    and VectorImage.
    
    ENH: add a constructor in itk::Array to cast from an itk::Array with
    different element type. This is required for the PrintType of
    itk::Array< char >.
    
    ENH: add a constructor in itk::Point to support the assigment of the
    same value to all the elements in the point.
    
    ENH: add the Zero and One static member to itk::Point numeric traits
    and to a few more in numeric traits of RGBPixel and RGBAPixel.
    
    ENH: test most of the methods of most of the traits
    
    Change-Id: I9677cc8f8f72e1c7dfad5cef09930eb7a760f908

--- Modules/Core/Common/include/itkNumericTraitsTensorPixel.h
+++ Modules/Core/Common/include/itkNumericTraitsTensorPixel.h
@@ -82,6 +82,16 @@ public:
    * \note minimum value for floating pointer types is defined as
    * minimum positive normalize value.
    */
+  static const Self max(const Self &)
+  {
+    return Self( NumericTraits< T >::max() );
+  }
+
+  static const Self min(const Self &)
+  {
+    return Self( NumericTraits< T >::min() );
+  }
+
   static const Self max()
   {
     return Self( NumericTraits< T >::max() );
@@ -107,6 +117,21 @@ public:
     return Self( NumericTraits< T >::OneValue() );
   }
 
+  static const Self NonpositiveMin(const Self &)
+  {
+    return NonpositiveMin();
+  }
+
+  static const Self ZeroValue(const Self &)
+  {
+    return ZeroValue();
+  }
+
+  static const Self OneValue(const Self &)
+  {
+    return OneValue();
+  }
+
   /** Fixed length vectors cannot be resized, so an exception will
    *  be thrown if the input size is not valid.  Here, the size refers
    *  to the dimensionality of the unerlying FixedArray, not the
@@ -125,7 +150,27 @@ public:
   /** Return the size of the underlying FixedArray. */
   static unsigned int GetLength(const SymmetricSecondRankTensor< T, D > &)
   {
+    return GetLength();
+  }
+
+  /** Return the size of the underlying FixedArray. */
+  static unsigned int GetLength()
+  {
     return D *( D + 1 ) / 2;
+  }
+
+  static void AssignArray( MeasurementVectorType & mv, const Self & v )
+  {
+    mv = v;
+  }
+
+  template<class TArray>
+  static void AssignArray( const Self & v, TArray & mv )
+  {
+    for( unsigned int i=0; i<GetLength(); i++ )
+      {
+      mv[i] = v[i];
+      }
   }
 
   /** \note: the functions are prefered over the member variables as
