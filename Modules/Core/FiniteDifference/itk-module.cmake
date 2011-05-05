d22ea529d385981ee6110242b5dd3100d88872f9
Luis Ibanez <luis.ibanez@kitware.com>
2011-05-05 18:42:43 -0400

    ENH: Adding doxygen documentation in the modules.
    The documentation added here (typically one paragraph) is exported
    to the Doxygen pages during the configuration of the ITK build.
    
    Change-Id: I4b7272418af19b6ae422f96d391a1b0433279dcc

--- Modules/Core/FiniteDifference/itk-module.cmake
+++ Modules/Core/FiniteDifference/itk-module.cmake
@@ -1,1 +1,6 @@
-itk_module(ITK-FiniteDifference DEPENDS ITK-Common TEST_DEPENDS ITK-TestKernel)
+set(DOCUMENTATION "This module contains the base classes needed to implement
+fininte differences images filtes. They include both spase and dense finite
+differences. Most of the classes in this module are abtract and therefore are
+not intended to be instantiated by themselves.")
+
+itk_module(ITK-FiniteDifference DEPENDS ITK-Common TEST_DEPENDS ITK-TestKernel DESCRIPTION "${DOCUMENTATION}")
