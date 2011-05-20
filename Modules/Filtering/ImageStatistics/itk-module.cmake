cb8f0c8774fddb779179860f833a5d0825598526
Brad King <brad.king@kitware.com>
2011-05-20 11:30:03 -0400

    COMP: Only ImageStatistics tests depend on SpatialObjects
    The parent commit accidentally introduced a circular dependency by
    making ImageStatistics depend on SpatialObjects.  Break the cycle by
    noting that only an ImageStatistics test actually needs SpatialObjects.
    
    Change-Id: I64259235ba05e9568f3369ea0b8c009cb3741a23

--- Modules/Filtering/ImageStatistics/itk-module.cmake
+++ Modules/Filtering/ImageStatistics/itk-module.cmake
@@ -1,3 +1,4 @@
-itk_module(ITK-ImageStatistics DEPENDS ITK-ImageFilterBase ITK-Statistics  ITK-SpatialObjects TEST_DEPENDS ITK-ImageIntensity ITK-TestKernel ITK-Thresholding ITK-ImageLabel)
+itk_module(ITK-ImageStatistics DEPENDS ITK-ImageFilterBase ITK-Statistics TEST_DEPENDS ITK-ImageIntensity ITK-TestKernel ITK-Thresholding ITK-ImageLabel ITK-SpatialObjects)
 #extra test dependency on Thresholding is introduced by itkNormalizedCorrelationImageFilterTest;
 #extra test dependency on ImageLabel is introduced by tkBinaryProjectionImageFilterTest.
+#extra test dependency on SpatialObjects is introduced by itkImageMomentsTest
