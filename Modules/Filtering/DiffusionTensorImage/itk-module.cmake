5feadce88170663cf9496c9ff8460a2b0062b83e
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2011-05-26 23:30:18 -0400

    ENH: Relocate a few classes into different modules.
    A few classes are adjusted into different modules for better categorization.
    
    Change-Id: Ibc6854b75797c3b0bba821eeb31e1f6adcb7340d

--- Modules/Filtering/DiffusionTensorImage/itk-module.cmake
+++ Modules/Filtering/DiffusionTensorImage/itk-module.cmake
@@ -1,1 +1,1 @@
-itk_module(ITK-DiffusionTensorImage DEPENDS ITK-ImageFilterBase ITK-SpatialObjects TEST_DEPENDS ITK-TestKernel)
+itk_module(ITK-DiffusionTensorImage DEPENDS ITK-ImageFeature ITK-SpatialObjects TEST_DEPENDS ITK-TestKernel)
