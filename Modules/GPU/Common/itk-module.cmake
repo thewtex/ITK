ca3b701b7d409dbd6c6631d0db7fe7e9b1a215cb
Won-Ki Jeong <graphor@gmail.com>
2011-06-14 17:43:58 -0400

    ENH: GPU Alpha - missing file
    Change-Id: I54d903ddc3a26f71cb614f5fdd7a408fb9f82553

---
+++ Modules/GPU/Common/itk-module.cmake
@@ -0,0 +1,1 @@
+itk_module(ITK-GPUCommon DEPENDS  ITK-Common ITK-PDEDeformableRegistration ITK-ImageIntensity ITK-Smoothing ITK-Thresholding ITK-FiniteDifference ITK-AnisotropicSmoothing TEST_DEPENDS ITK-TestKernel)
