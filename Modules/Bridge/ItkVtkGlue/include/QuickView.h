11913c6e43340cc67e896c1b735916bc12cfbb21
Bill Lorensen <bill.lorensen@gmail.com>
2011-06-21 16:52:24 -0400

    WIP: DO NOT COMMIT
    ENH: ItkVtkGlue classes
    
    These classes provide a mechanism to process ITK images and display
    them in a convenient way with VTK.
    
    Change-Id: I3c87371580e33c6e3aa09bd70bb2893743853741

---
+++ Modules/Bridge/ItkVtkGlue/include/QuickView.h
@@ -0,0 +1,87 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __QuickView_h
+#define __QuickView_h
+
+#include <vector>
+
+#include <itkImage.h>
+#include <itkRGBPixel.h>
+
+class ImageInfo
+{
+public:
+  typedef itk::Image<unsigned char, 2> ImageType;
+
+  ImageInfo(ImageType *image, std::string description="")
+  {
+    m_Image = image;
+    m_Description = description;
+  }
+
+  ImageType::Pointer m_Image;
+  std::string        m_Description;
+};
+
+class RGBImageInfo
+{
+public:
+  typedef itk::Image<itk::RGBPixel<unsigned char>, 2> ImageType;
+  RGBImageInfo(ImageType *image, std::string description="")
+  {
+    m_Image = image;
+    m_Description = description;
+  }
+
+  ImageType::Pointer m_Image;
+  std::string        m_Description;
+};
+
+class QuickView
+{
+
+public:
+  QuickView()
+  {
+    m_ShareCamera = true;
+    m_Interpolate = true;
+  }
+  template<class TImage> void AddImage(
+    TImage *,
+    bool FlipVertical=true,
+    std::string Description="");
+  template<class TImage> void AddRGBImage(
+    TImage *,
+    bool FlipVertical=true,
+    std::string Description="");
+  void Visualize(bool interact=true);
+  void ShareCameraOff()
+  {
+    m_ShareCamera = false;
+  }
+  void ShareCameraOn()
+  {
+    m_ShareCamera = true;
+  }
+private:
+  std::vector<ImageInfo>    Images;
+  std::vector<RGBImageInfo> RGBImages;
+  bool                      m_ShareCamera;
+};
+
+#endif
