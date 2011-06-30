5c5946f31bec7f3786b17eec25d4f035f295b6e4
David Doria <daviddoria@gmail.com>
2011-06-30 18:44:21 -0400

    ENH: Added a class to apply an ImageFunction to every pixel.
    It was not possible to use UnaryFunctorImageFilter with the
    ImageFunction classes. This class applies an ImageFunction
    to every pixel in an input image.
    
    Change-Id: I56f27f6fd14d1052d2124d91541014c509f98e5e

---
+++ Modules/Core/Common/include/itkImageFunctionImageFilter.h
@@ -0,0 +1,125 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkImageFunctionImageFilter_h
+#define __itkImageFunctionImageFilter_h
+
+#include "itkInPlaceImageFilter.h"
+#include "itkImageRegionIteratorWithIndex.h"
+
+namespace itk
+{
+/** \class ImageFunctionImageFilter
+ * \brief Implements pixel-wise generic operations on an image.
+ *
+ * This class is parameterized over the type of the input image and
+ * the type of the output image.  It is also parameterized by the
+ * operation to be applied, using an ImageFunction style.
+ *
+ * ImageFunctionImageFilter allows the output dimension of the filter
+ * to be larger than the input dimension. Thus subclasses of the
+ * ImageFunctionImageFilter (like the CastImageFilter) can be used
+ * to promote a 2D image to a 3D image, etc.
+ *
+ * \sa UnaryFunctorImageFilter
+ *
+ * \ingroup   IntensityImageFilters     Multithreaded
+ * \ingroup ITK-Common
+ */
+template< class TInputImage, class TOutputImage, class TFunction >
+class ITK_EXPORT ImageFunctionImageFilter:public InPlaceImageFilter< TInputImage, TOutputImage >
+{
+public:
+  /** Standard class typedefs. */
+  typedef ImageFunctionImageFilter                        Self;
+  typedef InPlaceImageFilter< TInputImage, TOutputImage > Superclass;
+  typedef SmartPointer< Self >                            Pointer;
+  typedef SmartPointer< const Self >                      ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ImageFunctionImageFilter, InPlaceImageFilter);
+
+  /** Some typedefs. */
+  typedef TFunction FunctionType;
+
+  typedef TInputImage                              InputImageType;
+  typedef typename    InputImageType::ConstPointer InputImagePointer;
+  typedef typename    InputImageType::RegionType   InputImageRegionType;
+  typedef typename    InputImageType::PixelType    InputImagePixelType;
+
+  typedef TOutputImage                             OutputImageType;
+  typedef typename     OutputImageType::Pointer    OutputImagePointer;
+  typedef typename     OutputImageType::RegionType OutputImageRegionType;
+  typedef typename     OutputImageType::PixelType  OutputImagePixelType;
+
+  /** Get the function object. */
+  typename FunctionType::Pointer       GetFunction() { return m_Function; }
+
+  /** Set the functoin object. This replaces the current Function pointer
+   * with a new Function pointer.*/
+  void SetFunction(typename FunctionType::Pointer function)
+  {
+    if ( m_Function != function )
+      {
+      m_Function = function;
+      this->Modified();
+      }
+  }
+
+protected:
+  ImageFunctionImageFilter();
+  virtual ~ImageFunctionImageFilter() {}
+
+  /** ImageFunctionImageFilter can produce an image which is a different
+   * resolution than its input image.  As such, ImageFunctionImageFilter
+   * needs to provide an implementation for
+   * GenerateOutputInformation() in order to inform the pipeline
+   * execution model.  The original documentation of this method is
+   * below.
+   *
+   * \sa ProcessObject::GenerateOutputInformaton()  */
+  virtual void GenerateOutputInformation();
+
+  /** ImageFunctionImageFilter can be implemented as a multithreaded filter.
+   * Therefore, this implementation provides a ThreadedGenerateData() routine
+   * which is called for each processing thread. The output image data is
+   * allocated automatically by the superclass prior to calling
+   * ThreadedGenerateData().  ThreadedGenerateData can only write to the
+   * portion of the output image specified by the parameter
+   * "outputRegionForThread"
+   *
+   * \sa ImageToImageFilter::ThreadedGenerateData(),
+   *     ImageToImageFilter::GenerateData()  */
+  void ThreadedGenerateData(const OutputImageRegionType & outputRegionForThread,
+                            ThreadIdType threadId);
+
+private:
+  ImageFunctionImageFilter(const Self &); //purposely not implemented
+  void operator=(const Self &);          //purposely not implemented
+
+  typename FunctionType::Pointer m_Function;
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageFunctionImageFilter.txx"
+#endif
+
+#endif
---
+++ Modules/Core/Common/test/itkImageFunctionImageFilterTest.cxx
@@ -0,0 +1,97 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkImageFunctionImageFilter.h"
+#include "itkImage.h"
+#include "itkImage.h"
+#include "itkIndex.h"
+#include "itkGaussianBlurImageFunction.h"
+
+#include <iostream>
+
+namespace // anonymous
+{
+  typedef itk::Image< unsigned char, 2 > UnsignedCharImageType;
+  typedef itk::Image< float, 2 > FloatImageType;
+
+  static void CreateImage(UnsignedCharImageType::Pointer image);
+}
+//-------------------------
+//
+//   This file test the interface to the ImageFunctionImageFilter class
+//
+//-------------------------
+int itkImageFunctionImageFilterTest(int, char* [] )
+{
+  UnsignedCharImageType::Pointer image = UnsignedCharImageType::New();
+  CreateImage(image);
+
+  typedef itk::GaussianBlurImageFunction< UnsignedCharImageType > GaussianBlurImageFunctionType;
+  GaussianBlurImageFunctionType::Pointer gaussianFunction = GaussianBlurImageFunctionType::New();
+  gaussianFunction->SetInputImage( image ); // Do we need to do this since we are going to give the input to the ImageFunctionImageFilter?
+  GaussianBlurImageFunctionType::ErrorArrayType setError;
+  setError.Fill( 0.01 );
+  gaussianFunction->SetMaximumError( setError );
+  gaussianFunction->SetSigma( 1.0);
+  gaussianFunction->SetMaximumKernelWidth( 3 );
+
+  typedef itk::ImageFunctionImageFilter<UnsignedCharImageType,FloatImageType,
+                                  GaussianBlurImageFunctionType> ImageFunctionImageFilterType;
+  ImageFunctionImageFilterType::Pointer imageFunctionImageFilter = ImageFunctionImageFilterType::New();
+  imageFunctionImageFilter->SetInput(image);
+  imageFunctionImageFilter->SetFunction(gaussianFunction);
+  imageFunctionImageFilter->SetNumberOfThreads(1);
+  imageFunctionImageFilter->Update();
+
+  return EXIT_SUCCESS;
+}
+
+namespace // anonymous
+{
+void CreateImage(UnsignedCharImageType::Pointer image)
+{
+  itk::Index<2> start;
+  start.Fill(0);
+
+  itk::Size<2> size;
+  size.Fill(100);
+
+  itk::ImageRegion<2> region(start,size);
+
+  image->SetRegions(region);
+  image->Allocate();
+  image->FillBuffer(0);
+
+  itk::ImageRegionIterator<UnsignedCharImageType> imageIterator(image,region);
+
+  while(!imageIterator.IsAtEnd())
+    {
+    if(imageIterator.GetIndex()[0] >= 50 && imageIterator.GetIndex()[1] >= 50 &&
+       imageIterator.GetIndex()[0] <= 70 && imageIterator.GetIndex()[1] <= 70)
+      {
+      imageIterator.Set(255);
+      }
+
+    ++imageIterator;
+    }
+
+}
+} // end namespace
