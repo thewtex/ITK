f1b3c7820a1db1af30ae7ca76be723960bcc6ff9
Kent Williams <norman-k-williams@uiowa.edu>
2011-07-25 14:29:16 -0500

    ENH: Add weight parameter for Landmark init of Affine XFRM
    Change-Id: I59ac6fde61f1bf162b53cd692e04555734147054

--- Modules/Core/Transform/include/itkLandmarkBasedTransformInitializer.h
+++ Modules/Core/Transform/include/itkLandmarkBasedTransformInitializer.h
@@ -52,6 +52,12 @@ namespace itk
  * using unit quaternions,"
  * http://people.csail.mit.edu/bkph/papers/Absolute_Orientation.pdf
  *
+ * The Affine Transform initializer  is based on an algorithm by H
+ * Spaeth, and is described in the Insight Journal Article
+ * "Affine Transformation for Landmark Based Registration Initializer
+ * in ITK" by Kim E.Y., Johnson H., Williams N.
+ * available at  http://midasjournal.com/browse/publication/825
+ */
  *
  * \ingroup Transforms
  * \ingroup ITKTransform
@@ -105,6 +111,8 @@ public:
   typedef typename LandmarkPointContainer::const_iterator         PointsContainerConstIterator;
   typedef typename TransformType::ParametersType                  ParametersType;
   typedef typename ParametersType::ValueType                      ParameterValueType;
+  typedef std::vector< double >                                   LandmarkWeightType;
+  typedef LandmarkWeightType::const_iterator                      LandmarkWeightConstIterator;
 
   /** Set the Fixed landmark point containers */
   void SetFixedLandmarks(const LandmarkPointContainer & fixedLandmarks)
@@ -116,6 +124,14 @@ public:
   void SetMovingLandmarks(const LandmarkPointContainer & movingLandmarks)
   {
     this->m_MovingLandmarks = movingLandmarks;
+  }
+
+  /** Set the landmark weight point containers
+   *  Weight includes diagonal elements of weight matrix
+   */
+  void SetLandmarkWeight(LandmarkWeightType & landmarkWeight)
+  {
+    this->m_LandmarkWeight= landmarkWeight;
   }
 
   /**  Supported Transform typedefs */
@@ -159,6 +175,9 @@ private:
   LandmarkPointContainer m_MovingLandmarks;
 
   TransformPointer m_Transform;
+  /** weights for affine landmarks */
+  LandmarkWeightType m_LandmarkWeight;
+
 }; //class LandmarkBasedTransformInitializer
 }  // namespace itk
 
