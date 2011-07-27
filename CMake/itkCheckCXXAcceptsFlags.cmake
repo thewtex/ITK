727ab21efd1758d1c9342593646d9c3994f09bc2
David Cole <david.cole@kitware.com>
2011-07-27 14:39:10 -0400

    WIP: Detect/use supported warning flags by default
    Add file CheckCompilerWarningFlags.cmake. Use the check_compiler_warning_flags
    function to determine at CMake time what is the set of common C and C++ compiler
    warning flags supported by the current compilers. Add those warning flags to
    ITK_REQUIRED_C_FLAGS and ITK_REQUIRED_CXX_FLAGS so they are enabled by default.
    
    Converted to use the default CMake capability (which filter on compiler
      options that only give warnings, and removes those also) rather than
    the custom code that was only in ITK.  This allowed re-writing code
    in ITKPlatformSpecificChecks.cmake and make a more consistent
    set of tools used to identify which compiler options should be used.
    
    Issue-Ids:  ITK-145
    Change-Id: Iab95e55064b69f3eee489b65a389a556db8fc65e

--- CMake/itkCheckCXXAcceptsFlags.cmake
+++ CMake/itkCheckCXXAcceptsFlags.cmake
@@ -2,55 +2,49 @@
 # - Test CXX compiler for a flag
 # Check if the CXX compiler accepts a flag
 #
-# itkCHECK_CXX_ACCEPTS_FLAGS(FLAGS VAR)
+# ITK_CHECK_CXX_ACCEPTS_FLAGS(FLAGS VAR)
 # - macro which checks if the code compiles with the given flags
 #  FLAGS - cxx flags to try
 #  VAR   - variable to store whether compiler accepts the FLAGS (TRUE or FALSE)
 #
-macro(itkCHECK_CXX_ACCEPTS_FLAGS FLAGS VAR)
-    if(NOT DEFINED ${VAR})
-    set(_SOURCE "int main() { return 0;}\n")
-    file(WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.cxx"
-      "${_SOURCE}")
 
-    message(STATUS "Checking to see if CXX compiler accepts flag ${FLAGS}")
-    try_compile(${VAR}
-      ${CMAKE_BINARY_DIR}
-      ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.cxx
-      CMAKE_FLAGS -DCOMPILE_DEFINITIONS:STRING=${FLAGS}
-      OUTPUT_VARIABLE OUTPUT)
-    if(${VAR})
-      set(${VAR} TRUE CACHE INTERNAL "CXX compiler accepts flag ${FLAGS}")
-    else(${VAR})
-      set(${VAR} FALSE CACHE INTERNAL "CXX compiler accepts flag ${FLAGS}")
-    endif(${VAR})
 
-   set(_UNKNOWN_FLAG_MSGS
-     "ignoring unknown option"
-     "unrecognized option"
-     "Incorrect command line option"
-     "warning D9002" # MSVC, language independent
-   )
-   foreach(MSG ${_UNKNOWN_FLAG_MSGS})
-     string(REGEX MATCH "${MSG}" _FOUNDIT "${OUTPUT}")
-     if("${_FOUNDIT}" MATCHES "${MSG}")
-       set(${VAR} FALSE CACHE INTERNAL "CXX compiler accepts flag ${FLAGS}")
-     endif("${_FOUNDIT}" MATCHES "${MSG}")
-   endforeach(MSG ${_UNKNOWN_FLAG_MSGS})
+## Modified from CheckCXXCompilerFlag.cmake
+include(CheckCXXSourceCompiles)
+include(CheckCSourceCompiles)
 
-   if(${VAR})
-     message(STATUS "Checking to see if CXX compiler accepts flag ${FLAGS} - Yes")
-     file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
-       "Determining if the CXX compiler accepts the flag ${FLAGS} passed with "
-       "the following output:\n${OUTPUT}\n"
-       "Source file was:\n${_SOURCE}\n")
-   else(${VAR})
-     message(STATUS "Checking to see if CXX compiler accepts flag ${FLAGS} - No")
-     file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
-       "Determining if the CXX compiler accepts the flag ${FLAGS} passed with "
-       "the following output:\n${OUTPUT}\n"
-       "Source file was:\n${_SOURCE}\n")
-   endif(${VAR})
+## A thin wrapper that add a few extra FAIL_REGEX expressions
+macro (ITK_CHECK_CXX_COMPILER_FLAG _FLAG _RESULT)
+   set(SAFE_CMAKE_REQUIRED_DEFINITIONS "${CMAKE_REQUIRED_DEFINITIONS}")
+   set(CMAKE_REQUIRED_DEFINITIONS "${_FLAG}")
+   CHECK_CXX_SOURCE_COMPILES("int main() { return 0;}" ${_RESULT}
+     # Some compilers do not fail with a bad flag
+         FAIL_REGEX "unrecognized .*option"                     # GNU
+         FAIL_REGEX "ignoring unknown option"                   # MSVC
+         FAIL_REGEX "warning D9002"                             # MSVC, any lang
+         FAIL_REGEX "warning:.*is a C++0x extension"            # clang
+         FAIL_REGEX "[Uu]nknown option"                         # HP
+         FAIL_REGEX "[Ww]arning: [Oo]ption"                     # SunPro
+         FAIL_REGEX "command option .* is not recognized"       # XL
+         FAIL_REGEX "Incorrect command line option"             # Any
+     )
+   set (CMAKE_REQUIRED_DEFINITIONS "${SAFE_CMAKE_REQUIRED_DEFINITIONS}")
+endmacro (ITK_CHECK_CXX_COMPILER_FLAG)
 
-   endif(NOT DEFINED ${VAR})
-endmacro(itkCHECK_CXX_ACCEPTS_FLAGS)
+## A thin wrapper that add a few extra FAIL_REGEX expressions
+macro (ITK_CHECK_C_COMPILER_FLAG _FLAG _RESULT)
+   set(SAFE_CMAKE_REQUIRED_DEFINITIONS "${CMAKE_REQUIRED_DEFINITIONS}")
+   set(CMAKE_REQUIRED_DEFINITIONS "${_FLAG}")
+   CHECK_C_SOURCE_COMPILES("int main() { return 0;}" ${_RESULT}
+     # Some compilers do not fail with a bad flag
+         FAIL_REGEX "unrecognized .*option"                     # GNU
+         FAIL_REGEX "ignoring unknown option"                   # MSVC
+         FAIL_REGEX "warning D9002"                             # MSVC, any lang
+         FAIL_REGEX "warning:.*is a C++0x extension"            # clang
+         FAIL_REGEX "[Uu]nknown option"                         # HP
+         FAIL_REGEX "[Ww]arning: [Oo]ption"                     # SunPro
+         FAIL_REGEX "command option .* is not recognized"       # XL
+         FAIL_REGEX "Incorrect command line option"             # Any
+     )
+   set (CMAKE_REQUIRED_DEFINITIONS "${SAFE_CMAKE_REQUIRED_DEFINITIONS}")
+endmacro (ITK_CHECK_C_COMPILER_FLAG)
