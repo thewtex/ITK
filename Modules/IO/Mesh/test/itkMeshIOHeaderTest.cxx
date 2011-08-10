0d5737f1ba78277753a1b4acaf491200366636f5
Wanlin Zhu <wanlinzhu@gmail.com>
2011-08-10 11:56:14 +1000

    ENH: Add mesh IO
    This change adds a range of classes for supporting reading and
    writing various mesh format.
    Currently the supported file format includes:
    
    BYU Geometry File Format(*.byu)
    Freesurfer surface file format (*.fia/*.fsb)
    Freesurfer curvature file format (*.fcv)
    Geometry format under the neuroimaging informatics technology initiative (*.gii)
    Object file format (*.obj)
    VTK legacy file format (*.vtk)
    
    Using the IO factory, reading and writting mesh file could be same as
    reading and writting image in the ITK way.
    
    Change-Id: I794b62a703d663a4912a718ec2958071df7d9570

---
+++ Modules/IO/Mesh/test/itkMeshIOHeaderTest.cxx
@@ -0,0 +1,50 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <iostream>
+
+#include "itkBYUMeshIOFactory.h"
+#include "itkBYUMeshIO.h"
+#include "itkConvertArrayPixelBuffer.h"
+#include "itkConvertVariableLengthVectorPixelBuffer.h"
+#include "itkFreeSurferAsciiMeshIOFactory.h"
+#include "itkFreeSurferAsciiMeshIO.h"
+#include "itkFreeSurferBinaryMeshIOFactory.h"
+#include "itkFreeSurferBinaryMeshIO.h"
+#include "itkGiftiMeshIOFactory.h"
+#include "itkGiftiMeshIO.h"
+#include "itkMeshConvertPixelTraits.h"
+#include "itkMeshFileReader.h"
+#include "itkMeshFileWriter.h"
+#include "itkMeshIOBase.h"
+#include "itkMeshIOFactory.h"
+#include "itkOBJMeshIOFactory.h"
+#include "itkOBJMeshIO.h"
+#include "itkOFFMeshIOFactory.h"
+#include "itkOFFMeshIO.h"
+#include "itkVTKPolyDataMeshIOFactory.h"
+#include "itkVTKPolyDataMeshIO.h"
+
+int itkMeshIOHeaderTest ( int , char * [] )
+{
+
+  return EXIT_SUCCESS;
+}
