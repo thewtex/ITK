e570e237b0d0ad8e29324ff275370ba221183426
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-08-19 14:53:56 -0400

    WIP: Level Set Equation term and term container classes
    One term refers to a term in the PDE.
    Term container refers to the combination of terms in one PDE (that
    could be part of several PDE, i.e. when several level-sets evolve).
    
    Change-Id: I113e9f4fc9a2d1dfe095043dde7549024f7455b9

---
+++ Modules/Segmentation/LevelSetsv4/include/itkLevelSetEquationCurvatureTerm.h
@@ -0,0 +1,150 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __itkLevelSetEquationCurvatureTerm_h
+#define __itkLevelSetEquationCurvatureTerm_h
+
+#include "itkLevelSetEquationTermBase.h"
+#include "itkZeroFluxNeumannBoundaryCondition.h"
+#include "itkConstNeighborhoodIterator.h"
+#include "itkVector.h"
+#include "vnl/vnl_matrix_fixed.h"
+
+namespace itk
+{
+/**
+ *  \class LevelSetEquationCurvatureTerm
+ *  \brief Derived class to represents a curvature term in the level-set evolution PDE
+ *
+ *  \tparam TInput Input Image Type
+ *  \tparam TLevelSetContainer Level set function container type
+ *  \ingroup ITKLevelSetsv4
+ */
+template< class TInput, // Input image
+          class TLevelSetContainer >
+class LevelSetEquationCurvatureTerm :
+    public LevelSetEquationTermBase< TInput, TLevelSetContainer >
+{
+public:
+  typedef LevelSetEquationCurvatureTerm         Self;
+  typedef SmartPointer< Self >                  Pointer;
+  typedef SmartPointer< const Self >            ConstPointer;
+  typedef LevelSetEquationTermBase< TInput, TLevelSetContainer >
+                                                Superclass;
+
+  /** Method for creation through object factory */
+  itkNewMacro( Self );
+
+  /** Run-time type information */
+  itkTypeMacro( LevelSetEquationCurvatureTerm,
+                LevelSetEquationTermBase );
+
+  typedef typename Superclass::InputImageType     InputImageType;
+  typedef typename Superclass::InputImagePointer  InputImagePointer;
+  typedef typename Superclass::InputPixelType     InputPixelType;
+  typedef typename Superclass::InputPixelRealType InputPixelRealType;
+
+  typedef typename Superclass::LevelSetContainerType      LevelSetContainerType;
+  typedef typename Superclass::LevelSetContainerPointer   LevelSetContainerPointer;
+  typedef typename Superclass::LevelSetType               LevelSetType;
+  typedef typename Superclass::LevelSetPointer            LevelSetPointer;
+  typedef typename Superclass::LevelSetOutputPixelType    LevelSetOutputPixelType;
+  typedef typename Superclass::LevelSetOutputRealType     LevelSetOutputRealType;
+  typedef typename Superclass::LevelSetInputIndexType     LevelSetInputIndexType;
+  typedef typename Superclass::LevelSetGradientType       LevelSetGradientType;
+  typedef typename Superclass::LevelSetHessianType        LevelSetHessianType;
+  typedef typename Superclass::LevelSetIdentifierType     LevelSetIdentifierType;
+
+  typedef typename Superclass::HeavisideType    HeavisideType;
+  typedef typename Superclass::HeavisidePointer HeavisidePointer;
+
+  typedef typename Superclass::LevelSetDataType LevelSetDataType;
+
+  itkStaticConstMacro(ImageDimension, unsigned int, InputImageType::ImageDimension);
+
+  /** Neighborhood radius type */
+  typedef ZeroFluxNeumannBoundaryCondition< InputImageType > DefaultBoundaryConditionType;
+  typedef typename ConstNeighborhoodIterator< InputImageType >::RadiusType RadiusType;
+  typedef ConstNeighborhoodIterator< InputImageType, DefaultBoundaryConditionType > NeighborhoodType;
+
+  typedef Vector< LevelSetOutputRealType, itkGetStaticConstMacro(ImageDimension) > NeighborhoodScalesType;
+
+  /** Update the term parameter values at end of iteration */
+  virtual void Update()
+  {}
+
+  /** Initialize the parameters in the terms prior to an iteration */
+  virtual void InitializeParameters()
+  {
+    this->SetUp();
+  }
+
+  /** Initialize term parameters in the dense case by computing for each pixel location */
+  virtual void Initialize( const LevelSetInputIndexType& ) {}
+
+  /** Supply updates at pixels to keep the term parameters always updated */
+  virtual void UpdatePixel( const LevelSetInputIndexType& itkNotUsed( iP ),
+                            const LevelSetOutputRealType& itkNotUsed( oldValue ),
+                            const LevelSetOutputRealType& itkNotUsed( newValue ) )
+  {
+  }
+
+protected:
+  LevelSetEquationCurvatureTerm() : Superclass()
+  {
+    for( unsigned int i = 0; i < ImageDimension; i++ )
+      {
+      m_NeighborhoodScales[i] = 1.0;
+      }
+  }
+
+  virtual ~LevelSetEquationCurvatureTerm() {}
+
+  /** Set the term name */
+  virtual void SetDefaultTermName()
+    {
+    this->m_TermName = "Curvature term";
+    }
+
+  virtual void SetRequiredData()
+    {
+    this->m_RequiredData.insert( "MeanCurvature" );
+    }
+
+  /** Returns the term contribution for a given location iP, i.e.
+   *  \f$ \omega_i( p ) \f$. */
+  virtual LevelSetOutputRealType Value( const LevelSetInputIndexType& iP );
+
+  /** Returns the term contribution for a given location iP, i.e.
+   *  \f$ \omega_i( p ) \f$. */
+  virtual LevelSetOutputRealType Value( const LevelSetInputIndexType& iP, const LevelSetDataType& iData );
+
+  LevelSetOutputRealType  m_NeighborhoodScales[ImageDimension];
+
+private:
+  LevelSetEquationCurvatureTerm( const Self& );
+  void operator = ( const Self& );
+};
+
+}
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkLevelSetEquationCurvatureTerm.hxx"
+#endif
+
+#endif
