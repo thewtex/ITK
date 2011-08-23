bbfef3e81d9a27e052095ca22c63cbbb775d0994
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2011-08-23 10:06:46 +0200

    WIP: automatic threshold calculators
    This is the code of the insight journal contribution "Histogram-based
    thresholding - some missing methods".
    
      http://www.insight-journal.org/browse/publication/811
      http://hdl.handle.net/10380/3279
    
    The Otsu filters and calculators have also been modified to use the
    architecture of the other calculators.
    The histogram is no more computed by the calculator. This is delegated
    to another filter, such as ImageToHistogramFilter. This change lets us
    easily implementing the multithreading and the ability to mask the
    image to compute the histogram, for example by using
    MaskedImageToHistogramFilter or by using the histogram attached to a
    LabelObject.
    
    The old OtsuThresholdImageCalculator is available in the
    ITKV3Compatibility module.
    
    Change-Id: I697873136fbca86258626284eae5df7b552c6241

--- Modules/Filtering/Thresholding/include/itkOtsuThresholdImageFilter.hxx
+++ Modules/Filtering/Thresholding/include/itkOtsuThresholdImageFilter.hxx
@@ -1,99 +1,95 @@
-/*=========================================================================
- *
- *  Copyright Insight Software Consortium
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0.txt
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *=========================================================================*/
 #ifndef __itkOtsuThresholdImageFilter_hxx
 #define __itkOtsuThresholdImageFilter_hxx
 #include "itkOtsuThresholdImageFilter.h"
 
+#include "itkImageToHistogramFilter.h"
 #include "itkBinaryThresholdImageFilter.h"
-#include "itkOtsuThresholdImageCalculator.h"
+#include "itkOtsuThresholdCalculator.h"
 #include "itkProgressAccumulator.h"
 
-namespace itk
-{
-template< class TInputImage, class TOutputImage >
-OtsuThresholdImageFilter< TInputImage, TOutputImage >
+namespace itk {
+
+template<class TInputImage, class TOutputImage>
+OtsuThresholdImageFilter<TInputImage, TOutputImage>
 ::OtsuThresholdImageFilter()
 {
-  m_OutsideValue   = NumericTraits< OutputPixelType >::Zero;
-  m_InsideValue    = NumericTraits< OutputPixelType >::max();
-  m_Threshold      = NumericTraits< InputPixelType >::Zero;
-  m_NumberOfHistogramBins = 128;
+  m_OutsideValue   = NumericTraits<OutputPixelType>::Zero;
+  m_InsideValue    = NumericTraits<OutputPixelType>::max();
+  m_Threshold      = NumericTraits<InputPixelType>::Zero;
+//   m_NumberOfHistogramBins = 128;
 }
 
-template< class TInputImage, class TOutputImage >
+template<class TInputImage, class TOutputImage>
 void
-OtsuThresholdImageFilter< TInputImage, TOutputImage >
+OtsuThresholdImageFilter<TInputImage, TOutputImage>
 ::GenerateData()
 {
   typename ProgressAccumulator::Pointer progress = ProgressAccumulator::New();
   progress->SetMiniPipelineFilter(this);
 
+  typedef itk::Statistics::ImageToHistogramFilter<InputImageType> HistogramGeneratorType;
+  typedef typename HistogramGeneratorType::HistogramType          HistogramType;
+  typename HistogramGeneratorType::Pointer histogramGenerator = HistogramGeneratorType::New();
+  histogramGenerator->SetInput( this->GetInput() );
+  // histogramGenerator->SetAutoMinimumMaximum( true );
+/*  typename HistogramGeneratorType::HistogramSizeType hsize(1);
+  hsize[0] = m_NumberOfHistogramBins;
+  histogramGenerator->SetHistogramSize( hsize );*/
+  histogramGenerator->SetNumberOfThreads( this->GetNumberOfThreads() );
+  progress->RegisterInternalFilter(histogramGenerator,.4f);
+
   // Compute the Otsu Threshold for the input image
-  typename OtsuThresholdImageCalculator< TInputImage >::Pointer otsu =
-    OtsuThresholdImageCalculator< TInputImage >::New();
-  otsu->SetImage ( this->GetInput() );
-  otsu->SetNumberOfHistogramBins (m_NumberOfHistogramBins);
-  otsu->Compute();
-  m_Threshold = otsu->GetThreshold();
+  typedef OtsuThresholdCalculator<HistogramType, InputPixelType> CalculatorType;
+  typename CalculatorType::Pointer calculator = CalculatorType::New();
+  calculator->SetInput( histogramGenerator->GetOutput() );
+  calculator->SetNumberOfThreads( this->GetNumberOfThreads() );
+  progress->RegisterInternalFilter(calculator,.2f);
 
-  typename BinaryThresholdImageFilter< TInputImage, TOutputImage >::Pointer threshold =
-    BinaryThresholdImageFilter< TInputImage, TOutputImage >::New();
+  typedef BinaryThresholdImageFilter<TInputImage,TOutputImage> ThresholderType;
+  typename ThresholderType::Pointer thresholder = ThresholderType::New();
+  thresholder->SetInput(this->GetInput());
+  thresholder->SetLowerThreshold( NumericTraits<InputPixelType>::NonpositiveMin() );
+  thresholder->SetUpperThresholdInput( calculator->GetOutput() );
+  thresholder->SetInsideValue( this->GetInsideValue() );
+  thresholder->SetOutsideValue( this->GetOutsideValue() );
+  thresholder->SetNumberOfThreads( this->GetNumberOfThreads() );
+  progress->RegisterInternalFilter(thresholder,.4f);
 
-  progress->RegisterInternalFilter(threshold, .5f);
-  threshold->GraftOutput ( this->GetOutput() );
-  threshold->SetInput ( this->GetInput() );
-  threshold->SetLowerThreshold( NumericTraits< InputPixelType >::NonpositiveMin() );
-  threshold->SetUpperThreshold( otsu->GetThreshold() );
-  threshold->SetInsideValue (m_InsideValue);
-  threshold->SetOutsideValue (m_OutsideValue);
-  threshold->Update();
-
-  this->GraftOutput( threshold->GetOutput() );
+  thresholder->GraftOutput( this->GetOutput() );
+  thresholder->Update();
+  this->GraftOutput( thresholder->GetOutput() );
+  m_Threshold = calculator->GetOutput()->Get();
 }
 
-template< class TInputImage, class TOutputImage >
+template<class TInputImage, class TOutputImage>
 void
-OtsuThresholdImageFilter< TInputImage, TOutputImage >
+OtsuThresholdImageFilter<TInputImage, TOutputImage>
 ::GenerateInputRequestedRegion()
 {
-  TInputImage *input = const_cast< TInputImage * >( this->GetInput() );
-
-  if ( input )
+  TInputImage * input = const_cast<TInputImage *>(this->GetInput());
+  if( input )
     {
     input->SetRequestedRegionToLargestPossibleRegion();
     }
 }
 
-template< class TInputImage, class TOutputImage >
+template<class TInputImage, class TOutputImage>
 void
-OtsuThresholdImageFilter< TInputImage, TOutputImage >
-::PrintSelf(std::ostream & os, Indent indent) const
+OtsuThresholdImageFilter<TInputImage,TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
 {
-  Superclass::PrintSelf(os, indent);
+  Superclass::PrintSelf(os,indent);
 
   os << indent << "OutsideValue: "
-     << static_cast< typename NumericTraits< OutputPixelType >::PrintType >( m_OutsideValue ) << std::endl;
+     << static_cast<typename NumericTraits<OutputPixelType>::PrintType>(m_OutsideValue) << std::endl;
   os << indent << "InsideValue: "
-     << static_cast< typename NumericTraits< OutputPixelType >::PrintType >( m_InsideValue ) << std::endl;
-  os << indent << "NumberOfHistogramBins: "
-     << m_NumberOfHistogramBins << std::endl;
+     << static_cast<typename NumericTraits<OutputPixelType>::PrintType>(m_InsideValue) << std::endl;
+//   os << indent << "NumberOfHistogramBins: " << m_NumberOfHistogramBins << std::endl;
   os << indent << "Threshold (computed): "
-     << static_cast< typename NumericTraits< InputPixelType >::PrintType >( m_Threshold ) << std::endl;
+     << static_cast<typename NumericTraits<InputPixelType>::PrintType>(m_Threshold) << std::endl;
+
 }
-} // end namespace itk
+
+
+}// end namespace itk
 #endif
