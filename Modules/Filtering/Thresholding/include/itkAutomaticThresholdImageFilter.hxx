c80f87cd79137505dc413d661f1b8620f43a7c92
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2011-08-23 10:06:46 +0200

    ENH: automatic threshold calculators
    This is the code of the insight journal contribution "Histogram-based
    thresholding - some missing methods".
    
      http://www.insight-journal.org/browse/publication/811
      http://hdl.handle.net/10380/3279
    
    The Otsu filters and calculators have also been modified to use the
    architecture of the other calculators.
    The histogram is no more computed by the calculator. This is delegated
    to another filter, such as ImageToHistogramFilter. This change lets us
    easily implementing the multithreading and the ability to mask the
    image to compute the histogram, for example by using
    MaskedImageToHistogramFilter or by using the histogram attached to a
    LabelObject.
    
    The old OtsuThresholdImageCalculator is available in the
    ITKV3Compatibility module.
    
    Change-Id: I697873136fbca86258626284eae5df7b552c6241

---
+++ Modules/Filtering/Thresholding/include/itkAutomaticThresholdImageFilter.hxx
@@ -0,0 +1,94 @@
+#ifndef __itkAutomaticThresholdImageFilter_hxx
+#define __itkAutomaticThresholdImageFilter_hxx
+#include "itkAutomaticThresholdImageFilter.h"
+
+#include "itkImageToHistogramFilter.h"
+#include "itkBinaryThresholdImageFilter.h"
+#include "itkKittlerIllingworthThresholdCalculator.h"
+#include "itkProgressAccumulator.h"
+
+namespace itk {
+
+template<class TInputImage, class TOutputImage>
+AutomaticThresholdImageFilter<TInputImage, TOutputImage>
+::AutomaticThresholdImageFilter()
+{
+  m_OutsideValue   = NumericTraits<OutputPixelType>::Zero;
+  m_InsideValue    = NumericTraits<OutputPixelType>::max();
+  m_Threshold      = NumericTraits<InputPixelType>::Zero;
+  m_Calculator     = NULL;
+}
+
+template<class TInputImage, class TOutputImage>
+void
+AutomaticThresholdImageFilter<TInputImage, TOutputImage>
+::GenerateData()
+{
+  if( m_Calculator.IsNull() )
+    {
+    itkExceptionMacro(<<"No threshold calculator set.");
+    }
+  typename ProgressAccumulator::Pointer progress = ProgressAccumulator::New();
+  progress->SetMiniPipelineFilter(this);
+
+  typedef itk::Statistics::ImageToHistogramFilter<InputImageType> HistogramGeneratorType;
+  typename HistogramGeneratorType::Pointer histogramGenerator = HistogramGeneratorType::New();
+  histogramGenerator->SetInput( this->GetInput() );
+  // histogramGenerator->SetAutoMinimumMaximum( true );
+  histogramGenerator->SetNumberOfThreads( this->GetNumberOfThreads() );
+  progress->RegisterInternalFilter(histogramGenerator,.4f);
+
+  m_Calculator->SetInput( histogramGenerator->GetOutput() );
+  m_Calculator->SetNumberOfThreads( this->GetNumberOfThreads() );
+  this->ConfigureCalculator();
+  progress->RegisterInternalFilter(m_Calculator,.2f);
+
+  typedef BinaryThresholdImageFilter<TInputImage,TOutputImage> ThresholderType;
+  typename ThresholderType::Pointer thresholder = ThresholderType::New();
+  thresholder->SetInput(this->GetInput());
+  thresholder->SetLowerThreshold( NumericTraits<InputPixelType>::NonpositiveMin() );
+  thresholder->SetUpperThresholdInput( m_Calculator->GetOutput() );
+  thresholder->SetInsideValue( this->GetInsideValue() );
+  thresholder->SetOutsideValue( this->GetOutsideValue() );
+  thresholder->SetNumberOfThreads( this->GetNumberOfThreads() );
+  progress->RegisterInternalFilter(thresholder,.4f);
+
+  thresholder->GraftOutput( this->GetOutput() );
+  thresholder->Update();
+  this->GraftOutput( thresholder->GetOutput() );
+  m_Threshold = m_Calculator->GetThreshold();
+  m_Calculator->SetInput( NULL );
+}
+
+template<class TInputImage, class TOutputImage>
+void
+AutomaticThresholdImageFilter<TInputImage, TOutputImage>
+::GenerateInputRequestedRegion()
+{
+  TInputImage * input = const_cast<TInputImage *>(this->GetInput());
+  if( input )
+    {
+    input->SetRequestedRegionToLargestPossibleRegion();
+    }
+}
+
+template<class TInputImage, class TOutputImage>
+void
+AutomaticThresholdImageFilter<TInputImage,TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "OutsideValue: "
+     << static_cast<typename NumericTraits<OutputPixelType>::PrintType>(m_OutsideValue) << std::endl;
+  os << indent << "InsideValue: "
+     << static_cast<typename NumericTraits<OutputPixelType>::PrintType>(m_InsideValue) << std::endl;
+  os << indent << "Calculator: ";
+  m_Calculator->Print( os, indent.GetNextIndent() );
+  os << indent << "Threshold (computed): "
+     << static_cast<typename NumericTraits<InputPixelType>::PrintType>(m_Threshold) << std::endl;
+}
+
+
+}// end namespace itk
+#endif
