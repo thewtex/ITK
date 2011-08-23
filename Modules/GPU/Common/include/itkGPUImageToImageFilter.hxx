cf812eaf56f5566e02a3fd886f4269dfda21eed3
Won-Ki Jeong <graphor@gmail.com>
2011-08-24 07:49:27 +0900

    STYLE: Missing hxx files are checked in.
    Change-Id: I9ca8bbd2309a8360569b8e077b66f3764e35b5c9

--- Modules/GPU/Common/include/itkGPUImageToImageFilter.hxx
+++ Modules/GPU/Common/include/itkGPUImageToImageFilter.hxx
@@ -23,40 +23,52 @@
 namespace itk
 {
 
-  template< class TInputImage, class TOutputImage, class TParentImageFilter >
-  GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::GPUImageToImageFilter(): m_GPUEnabled(true)
-  {
-    m_KernelManager = GPUKernelManager::New();
-  }
+template< class TInputImage, class TOutputImage, class TParentImageFilter >
+GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::GPUImageToImageFilter() : m_GPUEnabled(true)
+{
+  m_GPUKernelManager = GPUKernelManager::New();
+}
 
-  template< class TInputImage, class TOutputImage, class TParentImageFilter >
-  GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::~GPUImageToImageFilter()
-  {}
+template< class TInputImage, class TOutputImage, class TParentImageFilter >
+GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::~GPUImageToImageFilter()
+{
+}
 
-  template< class TInputImage, class TOutputImage, class TParentImageFilter >
-  void
-    GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::PrintSelf(std::ostream & os, Indent indent) const
-  {
-    Superclass::PrintSelf(os, indent);
-    os << indent << "GPU: " << ( m_GPUEnabled ? "Enabled" : "Disabled" ) << std::endl;
-  }
+template< class TInputImage, class TOutputImage, class TParentImageFilter >
+void
+GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::PrintSelf(std::ostream & os,
+                                                                                  Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+  os << indent << "GPU: " << ( m_GPUEnabled ? "Enabled" : "Disabled" ) << std::endl;
+}
 
-  template< class TInputImage, class TOutputImage, class TParentImageFilter >
-  void
-    GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::GenerateData()
-  {
-    if( !m_GPUEnabled ) // call CPU update function
+template< class TInputImage, class TOutputImage, class TParentImageFilter >
+void
+GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::GenerateData()
+{
+  if( !m_GPUEnabled ) // call CPU update function
     {
-      Superclass::GenerateData();
+    Superclass::GenerateData();
     }
-    else // call GPU update function
+  else // call GPU update function
     {
-      // Call a method to allocate memory for the filter's outputs
-      this->AllocateOutputs();
+    // Call a method to allocate memory for the filter's outputs
+    this->AllocateOutputs();
 
-      GPUGenerateData();
+    GPUGenerateData();
     }
-  }
+}
+
+template< class TInputImage, class TOutputImage, class TParentImageFilter >
+void
+GPUImageToImageFilter< TInputImage, TOutputImage, TParentImageFilter >::GraftOutput(DataObject *output)
+{
+  typedef typename itk::GPUTraits< TOutputImage >::Type GPUOutputImage;
+  typename GPUOutputImage::Pointer otPtr = dynamic_cast< GPUOutputImage * >( this->GetOutput() );
+
+  otPtr->Graft( output );
+}
 
 } // end namespace itk
 
