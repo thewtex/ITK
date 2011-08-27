5a00314aa048e83e8b215a691715254a57703e5d
Nick Tustison <ntustison@gmail.com>
2011-08-27 18:29:57 -0400

    DOC:  Adding doc. for B-spline displacement field transform.
    Although ITK already has a B-spline transform, its
    use in image registration is less than optimal.
    This class encodes one way of speeding up a B-spline
    for image registration.
    
    Change-Id: Iafc1b02706e1f0b446fa0978cca0db90a480432d

--- Modules/Filtering/DisplacementField/test/itkBSplineDisplacementFieldTransformTest.cxx
+++ Modules/Filtering/DisplacementField/test/itkBSplineDisplacementFieldTransformTest.cxx
@@ -25,12 +25,12 @@
 
 #define NDIMENSIONS 2
 typedef itk::BSplineDisplacementFieldTransform<double, NDIMENSIONS>
-DisplacementTransformType;
-typedef DisplacementTransformType::ScalarType ScalarType;
+                                               DisplacementTransformType;
+typedef DisplacementTransformType::ScalarType  ScalarType;
 
 const ScalarType epsilon = 1e-10;
 
-int itkBSplineDisplacementFieldTransformTest(int, char *[] )
+int itkBSplineDisplacementFieldTransformTest( int ,char *[] )
 {
 
   /* NOTE
@@ -39,17 +39,17 @@ int itkBSplineDisplacementFieldTransformTest(int, char *[] )
    * vectors as well. Currently this just tests transforming
    * points. */
 
-  typedef  itk::Matrix<ScalarType, NDIMENSIONS, NDIMENSIONS> Matrix2Type;
-  typedef  itk::Vector<ScalarType, NDIMENSIONS>              Vector2Type;
+  typedef  itk::Matrix<ScalarType, NDIMENSIONS, NDIMENSIONS>  Matrix2Type;
+  typedef  itk::Vector<ScalarType, NDIMENSIONS>               Vector2Type;
 
   /* Create a displacement field transform */
   DisplacementTransformType::Pointer displacementTransform =
-    DisplacementTransformType::New();
+      DisplacementTransformType::New();
   typedef DisplacementTransformType::DisplacementFieldType FieldType;
-  FieldType::Pointer field = FieldType::New(); // This is based on itk::Image
+  FieldType::Pointer field = FieldType::New(); //This is based on itk::Image
 
-  FieldType::SizeType   size;
-  FieldType::IndexType  start;
+  FieldType::SizeType size;
+  FieldType::IndexType start;
   FieldType::RegionType region;
   size.Fill( 30 );
   start.Fill( 0 );
@@ -68,7 +68,7 @@ int itkBSplineDisplacementFieldTransformTest(int, char *[] )
   FieldType::IndexType nonZeroFieldIndex;
   nonZeroFieldIndex[0] = 3;
   nonZeroFieldIndex[1] = 4;
-  ScalarType                                  data1[] = {4, -2.5};
+  ScalarType data1[] = {4,-2.5};
   DisplacementTransformType::OutputVectorType nonZeroFieldVector(data1);
   field->SetPixel( nonZeroFieldIndex, nonZeroFieldVector );
 
@@ -87,49 +87,47 @@ int itkBSplineDisplacementFieldTransformTest(int, char *[] )
 
   std::cout << "displacementTransform: " << std::endl
             << displacementTransform << std::endl;
+  std::cout << "Spline order: " << displacementTransform->GetSplineOrder()
+    << std::endl;
+  std::cout << "Number of control points: "
+    << displacementTransform->GetNumberOfControlPoints() << std::endl;
+  std::cout << "Number of fitting levels: "
+    << displacementTransform->GetNumberOfFittingLevels() << std::endl;
+  std::cout << "Inverse?: " << ( displacementTransform->
+    GetCalculateApproximateInverseDisplacementField() ? '1' : '0' )
+    << std::endl;
+
+  displacementTransform->Print( std::cout, 4 );
 
   /* Test transforming some points. */
 
-  DisplacementTransformType::InputPointType  testPoint;
+  DisplacementTransformType::InputPointType testPoint;
   DisplacementTransformType::OutputPointType deformOutput, deformTruth;
 
   /* Test a point with non-zero displacement */
   testPoint[0] = nonZeroFieldIndex[0];
   testPoint[1] = nonZeroFieldIndex[1];
-  deformTruth[0] = nonZeroFieldIndex[0] + nonZeroFieldVector[0];
-  deformTruth[1] = nonZeroFieldIndex[1] + nonZeroFieldVector[1];
   deformOutput = displacementTransform->TransformPoint( testPoint );
   std::cout << "point 1 transformed: " << deformOutput;
-  std::cout << "(compared with input:  " << deformTruth << ")" << std::endl;
-//  if( !samePoint( deformOutput, deformTruth ) )
-//      {
-//      std::cout << "Failed transforming point 1." << std::endl;
-//      return EXIT_FAILURE;
-//      }
 
   /* Test a non-integer point using the linear interpolator.
    * The default interpolator thus far is linear, but set it
    * just in case, and to test the set function and test TransforPoint's
    * handling of changed displacement field and interpolator objects. */
   typedef itk::VectorLinearInterpolateImageFunction<FieldType, ScalarType>
-  LinearInterpolatorType;
+    LinearInterpolatorType;
   LinearInterpolatorType::Pointer interpolator = LinearInterpolatorType::New();
   std::cout << "Interpolator:" << std::endl << interpolator;
   displacementTransform->SetInterpolator( interpolator );
   std::cout << "\n***Transform after add interpolator: \n"
-            << displacementTransform << std::endl;
+    << displacementTransform << std::endl;
 
   ScalarType xoffset = 0.4;
   testPoint[0] = nonZeroFieldIndex[0] + xoffset;
   testPoint[1] = nonZeroFieldIndex[1];
-  deformTruth[0] = (nonZeroFieldIndex[0] + 1) * xoffset
-    + ( nonZeroFieldIndex[0] + nonZeroFieldVector[0] ) * (1 - xoffset);
-  deformTruth[1] = nonZeroFieldIndex[1] * xoffset
-    + ( nonZeroFieldIndex[1] + nonZeroFieldVector[1] ) * (1 - xoffset);
   deformOutput = displacementTransform->TransformPoint( testPoint );
   std::cout << "Transform point with offset: " << std::endl
             << "  Test point: " << testPoint << std::endl
-            << "  Truth: " << deformTruth << std::endl
             << "  Output: " << deformOutput << std::endl;
 
   /* Test IsLinear()
@@ -137,7 +135,7 @@ int itkBSplineDisplacementFieldTransformTest(int, char *[] )
   if( displacementTransform->IsLinear() )
     {
     std::cout << "DisplacementFieldTransform returned 'true' for IsLinear()."
-    " Expected 'false'." << std::endl;
+      " Expected 'false'." << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -158,47 +156,26 @@ int itkBSplineDisplacementFieldTransformTest(int, char *[] )
   inverseFieldIndex[0] = 7;
   inverseFieldIndex[1] = 11;
   DisplacementTransformType::OutputVectorType inverseFieldVector;
-  DisplacementTransformType::OutputPointType  inverseTruth, inverseOutput;
+  DisplacementTransformType::OutputPointType inverseTruth, inverseOutput;
   testPoint[0] = inverseFieldIndex[0];
   testPoint[1] = inverseFieldIndex[1];
-  inverseTruth[0] = testPoint[0] + inverseFieldVector[0];
-  inverseTruth[1] = testPoint[1] + inverseFieldVector[1];
   inverseOutput = inverseTransform->TransformPoint( testPoint );
 
-  /* Get inverse transform again, but using other accessor. */
-//  DisplacementTransformType::Pointer inverseTransform2;
-//  inverseTransform2 = dynamic_cast<DisplacementTransformType*>
-//    ( displacementTransform->GetInverseTransform().GetPointer() );
-//  if( ! inverseTransform2 )
-//    {
-//    std::cout << "Failed calling GetInverseTransform()." << std::endl;
-//    return EXIT_FAILURE;
-//    }
-//  inverseOutput = inverseTransform2->TransformPoint( testPoint );
-
-  /* Test ComputeJacobianWithRespectToParameters - Since there are no parameters for this transform,
+  /* Test GetJacobian - Since there are no parameters for this transform,
    * the Jacobian shouldn't be requested and will throw an exception. */
-  DisplacementTransformType::JacobianType   jacobian;
+  DisplacementTransformType::JacobianType jacobian;
   DisplacementTransformType::InputPointType inputPoint;
-  inputPoint[0] = 1;
-  inputPoint[1] = 2;
-  bool caughtException = false;
+  inputPoint[0]=1;
+  inputPoint[1]=2;
   try
     {
-    displacementTransform->ComputeJacobianWithRespectToParameters( inputPoint, jacobian );
+    displacementTransform->ComputeJacobianWithRespectToParameters(
+      inputPoint, jacobian );
     }
   catch( itk::ExceptionObject & e )
     {
-    std::string description(e.GetDescription() );
-    caughtException =
-      description.find("DisplacementFieldTransform") != std::string::npos;
-    }
-  if( !caughtException )
-    {
-    std::cout << "Expected ComputeJacobianWithRespectToParameters to throw exception." << std::endl;
+    std::cout << e.GetDescription() << std::endl;
     return EXIT_FAILURE;
     }
-  std::cout << "Passed Jacobian test." << std::endl;
-
   return EXIT_SUCCESS;
 }
