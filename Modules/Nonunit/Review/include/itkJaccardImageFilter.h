ca4d91aa0373737370d82de4bd2245734dadc168
Arindam Bhattacharya <arindamb86@gmail.com>
2011-08-31 13:44:38 -0400

    ENH: adding itkJaccardImageFilter
    this class compares two images and provides a similarity coefficent.
    based on the Jaccard Metric.
    
    Change-Id: Iff90f528c1c86f4d25e8b180d40617eee576609c

---
+++ Modules/Nonunit/Review/include/itkJaccardImageFilter.h
@@ -0,0 +1,77 @@
+/*
+ Program: Insight Toolkit
+ Module:  JaccardImageFilter
+ Language: C++
+ Date: Thu Aug 25 11:36:08 EDT 2011
+ Author: Arindam Bhattacharya
+ The Ohio State University
+ */
+#ifndef __itkJaccardImageFilter_h
+#define __itkJaccardImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkArray.h"
+
+namespace itk
+{
+    //This filter implements the JaccardImageFilter
+    //The Filter  expects as input,
+    //two binary images and then finds the Jaccard similarity co-efficient between them.
+    //It outputs a floating value (0-1) and an (grayscale)image where the intersection of the two input image
+    //black ( is grayscale value '0') the union is 'gray' (grayscale value '127').
+    //The Jaccard coefficient measures similarity between the images,
+    //and is defined as the size of the intersection divided by the size of the union of the images.
+    //The distance which is a 'metric' is then given by subtracting the coefficient from 1.
+
+  template< class TImage>
+  class JaccardImageFilter:public ImageToImageFilter< TImage, TImage>
+  {
+  public:
+    /** Standard class typedefs. */
+    typedef JaccardImageFilter             Self;
+    typedef ImageToImageFilter< TImage, TImage > Superclass;
+    typedef SmartPointer< Self >        Pointer;
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    /** Run-time type information (and related methods). */
+    itkTypeMacro(JaccardImageFilter, ImageToImageFilter);
+
+      //
+    void SetInputImage1(const TImage* image);
+    void SetInputImage2(const TImage* image);
+
+    const double getJaccardDistance();
+
+  protected:
+    JaccardImageFilter();
+    ~JaccardImageFilter(){}
+
+    virtual void BeforeThreadedGenerateData();
+
+    void AfterThreadedGenerateData ();
+
+    /** Does the real work. */
+    virtual void ThreadedGenerateData(const typename Superclass::OutputImageRegionType& outputRegionForThread,
+                                      int threadId);
+
+  private:
+    JaccardImageFilter(const Self &); //purposely not implemented
+    void operator=(const Self &);  //purposely not implemented
+
+      //Count of the number of intersections between the two images.
+    Array<long>      m_Count_intersection;
+      //Count of the number of Union between the two images.
+    Array<long>      m_Count_union;
+      //The output dis-similarity is stored here
+    double JaccardDistance;
+  };
+} //namespace ITK
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkJaccardImageFilter.hxx"
+#endif
+
+#endif // __itkJaccardImageFilter_h
---
+++ Modules/Nonunit/Review/include/itkJaccardImageFilter.hxx
@@ -0,0 +1,121 @@
+/*
+ Program: Insight Toolkit
+ Module:  JaccardImageFilter
+ Language: C++
+ Date: Thu Aug 25 11:36:08 EDT 2011
+ Author: Arindam Bhattacharya
+ The Ohio State University
+ */
+#ifndef __itkJaccardImageFilter_hxx
+#define __itkJaccardImageFilter_hxx
+
+
+using namespace std;
+namespace itk
+{
+  template<class TImage>
+  JaccardImageFilter<TImage>::JaccardImageFilter()
+  {
+    this->SetNumberOfRequiredInputs(2);
+  }
+
+
+    //Set the first image
+  template< class TImage>
+  void JaccardImageFilter<TImage>:: SetInputImage1(const TImage* image)
+  {
+    SetNthInput(0, const_cast<TImage*>(image));
+  }
+
+
+    //Set second image
+  template< class TImage>
+  void JaccardImageFilter<TImage>:: SetInputImage2(const TImage* image)
+  {
+    SetNthInput(1, const_cast<TImage*>(image));
+  }
+
+    //Before threaded
+  template<class TImage>
+  void JaccardImageFilter<TImage>::BeforeThreadedGenerateData()
+  {
+    int numberOfThreads = this->GetNumberOfThreads();
+
+      // Resize the thread temporaries
+    m_Count_intersection.SetSize(numberOfThreads);
+    m_Count_union.SetSize(numberOfThreads);
+
+      // Initialize the temporaries
+    m_Count_intersection.Fill(NumericTraits<long>::Zero);
+    m_Count_union.Fill(NumericTraits<long>::Zero);
+
+  }
+    //After Thread generate
+  template<class TImage>
+  void JaccardImageFilter<TImage>
+  ::AfterThreadedGenerateData()
+  {
+      //Calculate Jaccard distance
+    long int countIntersection;
+    long int countUnion;
+    int numberOfThreads = this->GetNumberOfThreads();
+    countIntersection = 0;
+    countUnion = 0;
+
+    for(int i = 0; i < numberOfThreads; i++)
+    {
+      countIntersection += m_Count_intersection[i];
+      countUnion += m_Count_union[i];
+    }
+    if(countUnion > 0)
+    {
+      this->JaccardDistance = (1 - (double(countIntersection)/double(countUnion)));
+    }
+    else
+    {
+        //default Jaccard distance
+      this->JaccardDistance= -1;
+    }
+  }
+
+
+  template<class TImage>
+  void JaccardImageFilter<TImage>
+  ::ThreadedGenerateData(const typename Superclass::OutputImageRegionType& outputRegionForThread,
+                         int threadId)
+  {
+    typename TImage::ConstPointer input1 = this->GetInput(0);
+    typename TImage::ConstPointer input2 = this->GetInput(1);
+    typename TImage::Pointer output = this->GetOutput();
+
+    itk::ImageRegionIterator<TImage> outputIterator(output, outputRegionForThread);
+    itk::ImageRegionConstIterator<TImage> inputIterator1(input1, outputRegionForThread);
+    itk::ImageRegionConstIterator<TImage> inputIterator2(input2, outputRegionForThread);
+
+    while(!outputIterator.IsAtEnd())
+    {
+      if((inputIterator1.Get()!=0) || (inputIterator2.Get() != 0))
+      {
+        outputIterator.Set(128);
+        m_Count_union[threadId]++;
+      }
+
+      if((inputIterator1.Get() != 0) && (inputIterator2.Get() != 0))
+      {
+        outputIterator.Set(0);
+        m_Count_intersection[threadId]++;
+      }
+      ++inputIterator1;
+      ++inputIterator2;
+      ++outputIterator;
+    }
+  }
+
+    // Returns the calculated  Jaccard distance
+  template<class TImage>
+  const double  JaccardImageFilter<TImage>:: getJaccardDistance()
+  {
+    return  this->JaccardDistance;
+  };
+}// end namespace
+#endif //__itkJaccardImageFilter_hxx
