80eae9d20b09fa03caf3c7dab5cbb56578b20cce
Nick Tustison <ntustison@gmail.com>
2011-08-31 12:45:01 -0400

    BUG: Return type mismatch for class function.
    Change-Id: I643ada501d1d52fc45bc477d842d3c9ae38d008e

--- Modules/Core/Transform/include/itkBSplineDeformableTransform.hxx
+++ Modules/Core/Transform/include/itkBSplineDeformableTransform.hxx
@@ -150,25 +150,24 @@ BSplineDeformableTransform<TScalarType, NDimensions, VSplineOrder>
 
 // Get the number of parameters
 template <class TScalarType, unsigned int NDimensions, unsigned int VSplineOrder>
-unsigned int
+typename BSplineDeformableTransform<TScalarType, NDimensions, VSplineOrder>::NumberOfParametersType
 BSplineDeformableTransform<TScalarType, NDimensions, VSplineOrder>
 ::GetNumberOfParameters(void) const
 {
   // The number of parameters equal SpaceDimension * number of
   // of pixels in the grid region.
-  return static_cast<unsigned int>( SpaceDimension )
-         * static_cast<unsigned int>( this->m_GridRegion.GetNumberOfPixels() );
+  return static_cast<NumberOfParametersType>( SpaceDimension * this->m_GridRegion.GetNumberOfPixels() );
 }
 
 // Get the number of parameters per dimension
 template <class TScalarType, unsigned int NDimensions, unsigned int VSplineOrder>
-unsigned int
+typename BSplineDeformableTransform<TScalarType, NDimensions, VSplineOrder>::NumberOfParametersType
 BSplineDeformableTransform<TScalarType, NDimensions, VSplineOrder>
 ::GetNumberOfParametersPerDimension(void) const
 {
   // The number of parameters per dimension equal number of
   // of pixels in the grid region.
-  return static_cast<unsigned int>( this->m_GridRegion.GetNumberOfPixels() );
+  return static_cast<NumberOfParametersType>( this->m_GridRegion.GetNumberOfPixels() );
 }
 
 // Set the grid region
