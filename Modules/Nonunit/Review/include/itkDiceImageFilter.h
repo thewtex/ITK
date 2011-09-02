7635e12eee4746598b64bf17cbf4fb954bdb3fdd
Arindam Bhattacharya <arindamb86@gmail.com>
2011-09-02 13:55:16 -0400

    ENH: adding itkDiceImageFilter
    This class compares two images and provides a similarity coefficient
    based on Dice Metric.
    
    Change-Id: Ic0a4f06ae37e326a745e13a3e7be9ed0037ca6f0

---
+++ Modules/Nonunit/Review/include/itkDiceImageFilter.h
@@ -0,0 +1,84 @@
+/*
+ Program: Insight Toolkit
+ Module:  DiceImageFilter
+ Language: C++
+ Date: Thu Aug 25 11:36:08 EDT 2011
+ Author: Arindam Bhattacharya
+ The Ohio State University
+ */
+#ifndef __itkDiceImageFilter_h
+#define __itkDiceImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkArray.h"
+
+namespace itk
+{
+
+    //This example introduces the use of the Dice Filter.This filter expects as input
+    //two binary images and then finds the Dice similarity co-efficient between them.
+    //It outputs a floating value (0-1) and an (grayscale)image where the intersection of the two input image
+    //black ( is grayscale value '0') the union is 'gray' (grayscale value '127').
+    //The formula for the calculations is given 'as twice the
+    //shared information (intersection) over the sum of cardinalities'.The Dice distance which measures dissimilarities
+    //is given subtracting the value from 1.
+
+
+  template< class TImage>
+  class DiceImageFilter:public ImageToImageFilter< TImage, TImage >
+  {
+  public:
+    /** Standard class typedefs. */
+    typedef DiceImageFilter             Self;
+    typedef ImageToImageFilter< TImage, TImage > Superclass;
+    typedef SmartPointer< Self >        Pointer;
+
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self);
+
+    /** Run-time type information (and related methods). */
+    itkTypeMacro(DiceImageFilter, ImageToImageFilter);
+
+    void SetInputImage1(const TImage* image);
+    void SetInputImage2(const TImage* image);
+
+    const double getDiceDistance();
+
+
+
+  protected:
+    DiceImageFilter();
+    ~DiceImageFilter(){}
+
+    virtual void BeforeThreadedGenerateData();
+
+    void AfterThreadedGenerateData ();
+
+    /** Does the real work. */
+    virtual void ThreadedGenerateData(const typename Superclass::OutputImageRegionType& outputRegionForThread,
+                                      int threadId);
+
+  private:
+    DiceImageFilter(const Self &); //purposely not implemented
+    void operator=(const Self &);  //purposely not implemented
+
+
+      //Count of the number of non-zero pixels in image1.
+    Array<long>      m_Count_image1;
+      //Count of the number of non-zero pixels in image2.
+    Array<long>      m_Count_image2;
+      //Count of the number of intersection pixels.
+    Array<long>      m_Count_intersection;
+      //Count of the number of union pixels.
+    Array<long>      m_Count_union;
+    double DiceDistance;
+  };
+} //namespace ITK
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkDiceImageFilter.hxx"
+#endif
+
+#endif // __itkDiceImageFilter_h
---
+++ Modules/Nonunit/Review/include/itkDiceImageFilter.hxx
@@ -0,0 +1,145 @@
+#ifndef __itkDiceImageFilter_txx
+#define __itkDiceImageFilter_txx
+
+#include "itkDiceImageFilter.h"
+#include "itkObjectFactory.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionConstIterator.h"
+#include "itkNeighborhoodIterator.h"
+#include "itkMinimumMaximumImageCalculator.h"
+
+using namespace std;
+namespace itk
+{
+  template<class TImage>
+  DiceImageFilter<TImage>::DiceImageFilter()
+  {
+    this->SetNumberOfRequiredInputs(2);
+  }
+
+
+    //set image 1
+  template< class TImage>
+  void DiceImageFilter<TImage>:: SetInputImage1(const TImage* image)
+  {
+    SetNthInput(0, const_cast<TImage*>(image));
+  }
+
+
+    //set image 2
+  template< class TImage>
+  void DiceImageFilter<TImage>:: SetInputImage2(const TImage* image)
+  {
+    SetNthInput(1, const_cast<TImage*>(image));
+  }
+
+
+
+
+
+    //Before threaded
+  template<class TImage>
+  void DiceImageFilter<TImage>::BeforeThreadedGenerateData()
+  {
+    int numberOfThreads = this->GetNumberOfThreads();
+      // Resize the thread temporaries
+
+    m_Count_intersection.SetSize(numberOfThreads);
+    m_Count_union.SetSize(numberOfThreads);
+    m_Count_image1.SetSize(numberOfThreads);
+    m_Count_image2.SetSize(numberOfThreads);
+
+      // Initialize the temporaries
+
+    m_Count_intersection.Fill(NumericTraits<long>::Zero);
+    m_Count_union.Fill(NumericTraits<long>::Zero);
+    m_Count_image1.Fill(NumericTraits<long>::Zero);
+
+  }
+
+  template<class TImage>
+  void DiceImageFilter<TImage>
+  ::AfterThreadedGenerateData()
+  {
+      //Calcualte Dice distance
+    long int countIntersection;
+    long int countUnion;
+    int numberOfThreads = this->GetNumberOfThreads();
+    countIntersection = 0;
+    countUnion = 0;
+    long int countImage1 = 0;
+    long int countImage2 = 0;
+
+    for(int i = 0; i < numberOfThreads; i++)
+    {
+      countIntersection += m_Count_intersection[i];
+      countImage1 += m_Count_image1[i];
+      countImage2 += m_Count_image2[i];
+    }
+
+      //Calculation for dice
+    if((countImage1 + countImage2 > 0))
+    {
+      this->DiceDistance = 1- double(2) *(countIntersection)/(countImage1 + countImage2);
+    }
+    else
+    {
+      this->DiceDistance= -1;
+    }
+  }
+
+
+  template<class TImage>
+  void DiceImageFilter<TImage>
+  ::ThreadedGenerateData(const typename Superclass::OutputImageRegionType& outputRegionForThread,
+                         int threadId)
+  {
+    typename TImage::ConstPointer input1 = this->GetInput(0);
+    typename TImage::ConstPointer input2 = this->GetInput(1);
+    typename TImage::Pointer output = this->GetOutput();
+
+    itk::ImageRegionIterator<TImage> outputIterator(output, outputRegionForThread);
+    itk::ImageRegionConstIterator<TImage> inputIterator1(input1, outputRegionForThread);
+    itk::ImageRegionConstIterator<TImage> inputIterator2(input2, outputRegionForThread);
+
+    while(!outputIterator.IsAtEnd())
+    {
+        //find the number of 'on'  pixels in image1
+      if(inputIterator1.Get()!=0)
+      {
+        m_Count_image1[threadId]++;
+      }
+        //find the number of 'on'  pixels in image2
+      if(inputIterator2.Get()!=0)
+      {
+        m_Count_image2[threadId]++;
+      }
+        //or
+      if((inputIterator1.Get() != 0) || (inputIterator2.Get() != 0))
+      {
+        outputIterator.Set(128);
+      }
+        //and
+      if((inputIterator1.Get() != 0) && (inputIterator2.Get() != 0))
+      {
+        outputIterator.Set(0);
+        m_Count_intersection[threadId]++;
+      }
+
+      ++inputIterator1;
+      ++inputIterator2;
+      ++outputIterator;
+    }
+  }
+
+    // Function to get back the Dice distance
+  template<class TImage>
+  const double  DiceImageFilter<TImage>:: getDiceDistance()
+  {
+    return  this->DiceDistance;
+  };
+
+
+
+}// end namespace
+#endif //__itkDiceImageFilter_txx
