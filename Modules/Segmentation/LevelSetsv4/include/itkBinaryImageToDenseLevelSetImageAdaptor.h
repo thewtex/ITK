5196c9d9a0d7fc0b83e12ed6ed0a17bbb7ba29be
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-09-09 21:10:25 -0400

    WIP: Refactoring BinaryImageTo****LevelSetAdaptor
    ITK-2686
    
    Change-Id: I186c35c437a3d7b7d91ffe05734a077c38d8678d

---
+++ Modules/Segmentation/LevelSetsv4/include/itkBinaryImageToDenseLevelSetImageAdaptor.h
@@ -0,0 +1,102 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __itkBinaryImageToDenseLevelSetImageAdaptor_h
+#define __itkBinaryImageToDenseLevelSetImageAdaptor_h
+
+#include "itkBinaryImageToLevelSetImageAdaptorBase.h"
+#include "itkImageToImageFilter.h"
+
+namespace itk
+{
+/** \class BinaryImageToDenseLevelSetImageAdaptor
+ *  \ingroup ITKLevelSetsv4
+ */
+template< class TInput, class TLevelSet >
+class BinaryImageToDenseLevelSetImageAdaptor :
+    public BinaryImageToLevelSetImageAdaptorBase< TInput, TLevelSet >
+{
+public:
+  typedef BinaryImageToDenseLevelSetImageAdaptor                      Self;
+  typedef SmartPointer< Self >                                        Pointer;
+  typedef SmartPointer< const Self >                                  ConstPointer;
+  typedef BinaryImageToLevelSetImageAdaptorBase< TInput, TLevelSet >  Superclass;
+
+  /** Method for creation through object factory */
+  itkNewMacro( Self );
+
+  /** Run-time type information */
+  itkTypeMacro( BinaryImageToDenseLevelSetImageAdaptor,
+                BinaryImageToLevelSetImageAdaptorBase );
+
+  typedef TInput                                InputImageType;
+  typedef typename InputImageType::PixelType    InputImagePixelType;
+  typedef typename InputImageType::IndexType    InputImageIndexType;
+  typedef typename InputImageType::Pointer      InputImagePointer;
+  typedef typename InputImageType::RegionType   InputImageRegionType;
+  typedef typename NumericTraits< InputImagePixelType >::RealType
+                                                InputPixelRealType;
+
+  itkStaticConstMacro ( ImageDimension, unsigned int,
+                       InputImageType::ImageDimension );
+
+  typedef TLevelSet                         LevelSetType;
+  typedef typename LevelSetType::Pointer    LevelSetPointer;
+  typedef typename LevelSetType::ImageType  LevelSetImageType;
+
+  typedef ImageToImageFilter< InputImageType, LevelSetType >  InternalFilterType;
+  typedef typename InternalFilterType::Pointer                InternalFilterPointer;
+
+  itkSetObjectMacro( SignedDistanceTransformFilter, InternalFilterType );
+  itkGetObjectMacro( SignedDistanceTransformFilter, InternalFilterType );
+
+  /**
+   * Input is a binary image m_InputImage
+   * Output is a WhitakerSparseLevelSetImagePointer  */
+  void Initialize()
+    {
+    if( m_SignedDistanceTransformFilter.IsNotNull() )
+      {
+      m_SignedDistanceTransformFilter->SetInput( this->m_InputImage );
+      m_SignedDistanceTransformFilter->Update();
+
+      typename LevelSetImageType::Pointer tempImage = LevelSetImageType::New();
+      tempImage->Graft( m_SignedDistanceTransformFilter->GetOutput() );
+
+      this->m_LevelSet = LevelSetType::New();
+      this->m_LevelSet->SetImage( tempImage );
+      }
+    }
+
+protected:
+  /** Constructor */
+  BinaryImageToDenseLevelSetImageAdaptor() {}
+
+  /** Destructor */
+  virtual ~BinaryImageToDenseLevelSetImageAdaptor() {}
+
+  InternalFilterPointer   m_SignedDistanceTransformFilter;
+
+private:
+  BinaryImageToDenseLevelSetImageAdaptor( const Self& ); // purposely not implemented
+  void operator = ( const Self& ); // purposely not implemented
+
+};
+}
+
+#endif // __itkBinaryImageToDenseLevelSetImageAdaptor_h
