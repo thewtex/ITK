731f6ac4900fb5dc31bbfa0409c72681b03db235
Cory Quammen <cquammen@cs.unc.edu>
2011-09-20 13:10:28 -0400

    ENH: Changed superclass to ImageRegionConstIteratorWithIndex
    Changing the inheritance of this class enables better code reuse.
    
    Change-Id: Id0a74adba97433410b68840bc59f01f285eb6419

--- Modules/Core/Common/include/itkImageRegionExclusionConstIteratorWithIndex.h
+++ Modules/Core/Common/include/itkImageRegionExclusionConstIteratorWithIndex.h
@@ -18,7 +18,7 @@
 #ifndef __itkImageRegionExclusionConstIteratorWithIndex_h
 #define __itkImageRegionExclusionConstIteratorWithIndex_h
 
-#include "itkImageConstIteratorWithIndex.h"
+#include "itkImageRegionConstIteratorWithIndex.h"
 
 namespace itk
 {
@@ -33,8 +33,8 @@ namespace itk
  * templated over the image type.  ImageRegionExclusionConstIteratorWithIndex
  * is constrained to walk only within the specified region. The exclusion
  * region is set after construction. By default the exclusion region is empty
- * and the iterator will behave as the itk::ImageRegionIteratorWithIndex with a
- * penalty in performance due to internal bounds checking.
+ * and the iterator will behave as the itk::ImageRegionConstIteratorWithIndex
+ * with a penalty in performance due to internal bounds checking.
  *
  * As with other ITK image iterators,
  * ImageRegionExclusionConstIteratorWithIndex requires more information be
@@ -127,12 +127,13 @@ namespace itk
  * \endwiki
  */
 template< typename TImage >
-class ITK_EXPORT ImageRegionExclusionConstIteratorWithIndex:public ImageConstIteratorWithIndex< TImage >
+class ITK_EXPORT ImageRegionExclusionConstIteratorWithIndex:
+  public ImageRegionConstIteratorWithIndex< TImage >
 {
 public:
   /** Standard class typedefs. */
-  typedef ImageRegionExclusionConstIteratorWithIndex Self;
-  typedef ImageConstIteratorWithIndex< TImage >      Superclass;
+  typedef ImageRegionExclusionConstIteratorWithIndex  Self;
+  typedef ImageRegionConstIteratorWithIndex< TImage > Superclass;
 
   /** Types inherited from the Superclass */
   typedef typename Superclass::IndexType             IndexType;
@@ -147,22 +148,21 @@ public:
   typedef typename Superclass::AccessorType          AccessorType;
 
   /** Default constructor. Needed since we provide a cast constructor. */
-  ImageRegionExclusionConstIteratorWithIndex():ImageConstIteratorWithIndex< TImage >() {}
+  ImageRegionExclusionConstIteratorWithIndex();
 
   /** Constructor establishes an iterator to walk a particular image and a
    * particular region of that image. */
-  ImageRegionExclusionConstIteratorWithIndex(const TImage *ptr,
-                                             const RegionType & region):
-    ImageConstIteratorWithIndex< TImage >(ptr, region) {}
+  ImageRegionExclusionConstIteratorWithIndex(const ImageType *ptr,
+                                             const RegionType & region);
 
-  /** Constructor that can be used to cast from an ImageIterator to an
-   * ImageRegionExclusionConstIteratorWithIndex. Many routines return an ImageIterator, but for a
-   * particular task, you may want an ImageRegionExclusionConstIteratorWithIndex.  Rather than
-   * provide overloaded APIs that return different types of Iterators, itk
-   * returns ImageIterators and uses constructors to cast from an
-   * ImageIterator to a ImageRegionExclusionConstIteratorWithIndex. */
-  ImageRegionExclusionConstIteratorWithIndex(const ImageConstIteratorWithIndex< TImage > & it)
-  { this->ImageConstIteratorWithIndex< TImage >::operator=(it); }
+  /** Constructor that can be used to cast from an ImageRegionConstIteratorWithIndex
+   * to an ImageRegionExclusionConstIteratorWithIndex. Many routines return an
+   * ImageIterator, but for a particular task, you may want an
+   * ImageRegionExclusionConstIteratorWithIndex. Rather than provide overloaded
+   * APIs that return different types of Iterators, itk returns ImageIterators
+   * and uses constructors to cast from an ImageIterator to a
+   * ImageRegionExclusionConstIteratorWithIndex. */
+  ImageRegionExclusionConstIteratorWithIndex(const Superclass & it);
 
   /** Increment (prefix) the fastest moving dimension of the iterator's index.
    * This operator will constrain the iterator within the region (i.e. the
@@ -208,16 +208,6 @@ private:
 
   IndexType m_ExclusionBegin;
   IndexType m_ExclusionEnd;
-
-  /** Helper method that increments to the next index regardless of
-   * whether it is in the exclusion region. */
-  void Increment();
-
-  /** Helper method that increments to the previous index regardless of
-   * whether it is in the exclusion region. */
-  void Decrement();
-
-
 };
 } // end namespace itk
 
