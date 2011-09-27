023d5c8097cecf47c9362d6a3ce9f56a677e8940
Nick Tustison <ntustison@gmail.com>
2011-09-27 12:14:10 -0400

    WIP: Do not merge.
    Moving colormapping functionality out of Review directory.
    
    Change-Id: I1cbbaceb735a85637f13a4b994d5e9028e91717b

---
+++ Modules/Filtering/Colormap/itk-module.cmake
@@ -0,0 +1,9 @@
+set(DOCUMENTATION "This module contains filter/functions for converting
+grayscale images to colormapped rgb images.")
+
+itk_module(ITKColorMap
+  DEPENDS
+  TEST_DEPENDS
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
