e86a78619f7fff6b3667098d420169919be181c4
Ziv Yaniv <zyaniv@childrensnational.org>
2011-09-29 11:10:41 -0400

    BUG: ITK-2730 fixed
    Setting the versor using a matrix checks that the matrix represents a rotation. Also added test for this, and found out that previous test had a bug, also fixed.
    
    Change-Id: I3d1207f587d809b3b1cbc4124707797aa99f5b6c

--- Modules/Core/Common/test/itkVersorTest.cxx
+++ Modules/Core/Common/test/itkVersorTest.cxx
@@ -647,8 +647,8 @@ int itkVersorTest(int, char* [] )
     mm[0][1] =  1.0;
     mm[0][2] =  0.0;
 
-    mm[1][0] =  0.0;
-    mm[1][1] = -1.0;
+    mm[1][0] =  -1.0;
+    mm[1][1] =  0.0;
     mm[1][2] =  0.0;
 
     mm[2][0] =  0.0;
@@ -668,6 +668,17 @@ int itkVersorTest(int, char* [] )
       std::cout << "Error in Versor Set(Matrix) method ! " << std::endl;
       std::cout << "vv  = " << vv << std::endl;
       return EXIT_FAILURE;
+      }
+      //matrix no longer represents a rotation
+    mm[0][0] = 1.0;
+    try
+      {
+      vv.Set( mm );
+      return EXIT_FAILURE;
+      }    //should always get here, mm isn't a rotation
+    catch(itk::ExceptionObject &)
+      {
+        //nothing to do here
       }
     std::cout << " PASSED !" << std::endl;
   }
