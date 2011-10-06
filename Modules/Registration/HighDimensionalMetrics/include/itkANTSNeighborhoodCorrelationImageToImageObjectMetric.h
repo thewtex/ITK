f9f478a20df5c89a3044f55de2f376e2a5c807d0
Matt McCormick <matt.mccormick@kitware.com>
2011-10-06 12:23:55 -0400

    WIP: Create new DomainThreader class.
    A DomainThreader class is created to act a facilitator to threaded operation
    within a class.  It is designed to be derived as a nested classes in an
    enclosing class that wants to perform a threading operation.
    
    The DomainThreader class has an associated ThreadedDomainPartitioner to
    partition the domain, e.g. ImageRegion, Array, Sample, pair of STL iterators,
    etc.,  into sub-domains to be processed in each thread.
    
    A new ProcessObjectDomainThreader base class is created within ProcessObject It
    also allows for reuse of the MultiThreader that was already a member of
    ProcessObject and respecting the pre-existing NumberOfThreads member in ProcessObject.
    
    The ThreadedArrayPartitioner is renamed to ThreadedIndexedContainerPartitioner
    to avoid incorrect association with itk::Array and to associated the class to
    itk::IndexedContainerInterface.
    
    GradientDescentObjectOptimizer,
    GradientDescentObjectOptimizerBase,
    ImageToImageObjectMetric,
    JointHistogramMutualInformationImageToImageObjectMetric,
    DemonsImageToImageObjectMetric,
    ANTSNeighborhoodCorrelationImageToImageObjectMetric
    have been updated for the refactored threading classes.
    
    Change-Id: I758328634a12a258f00c06c5f442494e981a46f9

--- Modules/Registration/HighDimensionalMetrics/include/itkANTSNeighborhoodCorrelationImageToImageObjectMetric.h
+++ Modules/Registration/HighDimensionalMetrics/include/itkANTSNeighborhoodCorrelationImageToImageObjectMetric.h
@@ -50,7 +50,7 @@ namespace itk {
  * 1) It is assumed that the derivative is only affected by changes in the
  * transform at the center of the window. This is obviously not true but speeds
  * the evaluation up considerably and works well in practice. This assumption
- * is the main differentiattion of this approach from a more generic one.
+ * is the main differentiation of this approach from a more generic one.
  *
  * 2) The evaluation uses on-the-fly queues with multi-threading and a sliding
  * neighborhood window. This is described in the above paper and specifically
@@ -79,7 +79,7 @@ namespace itk {
  *  metric->GetValueAndDerivative(valueReturn, derivativeReturn);
  *
  *
- * This Class is templated over the type of the two input objects.
+ * This class is templated over the type of the two input objects.
  * This is the base class for a hierarchy of similarity metrics that may, in
  * derived classes, operate on meshes, images, etc.  This class computes a
  * value that measures the similarity between the two objects.
@@ -110,6 +110,7 @@ public:
   /** superclass types */
   typedef typename Superclass::MeasureType                    MeasureType;
   typedef typename Superclass::DerivativeType                 DerivativeType;
+  typedef typename Superclass::DerivativeValueType            DerivativeValueType;
   typedef typename Superclass::VirtualPointType               VirtualPointType;
   typedef typename Superclass::FixedImagePointType            FixedImagePointType;
   typedef typename Superclass::FixedImagePixelType            FixedImagePixelType;
@@ -129,11 +130,10 @@ public:
   typedef typename Superclass::FixedImageType                 FixedImageType;
   typedef typename Superclass::MovingImageType                MovingImageType;
   typedef typename Superclass::VirtualImageType               VirtualImageType;
-  typedef typename Superclass::DenseThreaderInputObjectType        DenseThreaderInputObjectType;
   typedef typename Superclass::FixedOutputPointType           FixedOutputPointType;
   typedef typename Superclass::MovingOutputPointType          MovingOutputPointType;
 
-  typedef double InternalComputationValueType;
+  typedef typename Superclass::InternalComputationValueType  InternalComputationValueType;
 
   typedef typename Superclass::FixedTransformType::JacobianType
                             FixedTransformJacobianType;
@@ -157,19 +157,8 @@ public:
   itkStaticConstMacro(VirtualImageDimension, ImageDimensionType,
         ::itk::GetImageDimension<VirtualImageType>::ImageDimension);
 
-  /** Initialize. Must be called before first call to GetValue or
-   *  GetValueAndDerivative, after metric settings are changed. */
-  virtual void Initialize(void) throw (itk::ExceptionObject);
-
-  /** Evaluate and return the value and derivative */
-  using Superclass::GetValueAndDerivative;
-  void GetValueAndDerivative(MeasureType & value,
-      DerivativeType & derivative) const;
-
   /** Evaluate and return the metric value */
-  MeasureType GetValue() const {
-    itkExceptionMacro("GetValue not yet implemented.");
-  }
+  virtual MeasureType GetValue() const;
 
   // Set the radius of the neighborhood window centered at each pixel
   itkSetMacro(Radius, RadiusType);
@@ -179,85 +168,167 @@ public:
   itkGetConstMacro(Radius, RadiusType);
 
 protected:
+  ANTSNeighborhoodCorrelationImageToImageObjectMetric();
+  virtual ~ANTSNeighborhoodCorrelationImageToImageObjectMetric();
 
   // interested values here updated during scanning
   typedef InternalComputationValueType                 QueueRealType;
   typedef std::deque<QueueRealType>                    SumQueueType;
-  typedef ConstNeighborhoodIterator<VirtualImageType>  ScanningIteratorType;
+  typedef ConstNeighborhoodIterator<VirtualImageType>  ScanIteratorType;
+
   // one ScanMemType for each thread
   typedef struct ScanMemType {
     // queues used in the scanning
-    SumQueueType Qsuma2;
-    SumQueueType Qsumb2;
-    SumQueueType Qsuma;
-    SumQueueType Qsumb;
-    SumQueueType Qsumab;
+    // sum of the fixed value squared
+    SumQueueType QsumFixed2;
+    // sum of the moving value squared
+    SumQueueType QsumMoving2;
+    SumQueueType QsumFixed;
+    SumQueueType QsumMoving;
+    SumQueueType QsumFixedMoving;
     SumQueueType Qcount;
 
-    QueueRealType Ia;
-    QueueRealType Ja;
-    QueueRealType sfm;
-    QueueRealType sff;
-    QueueRealType smm;
+    QueueRealType fixedA;
+    QueueRealType movingA;
+    QueueRealType sFixedMoving;
+    QueueRealType sFixedFixed;
+    QueueRealType sMovingMoving;
 
-    FixedImageGradientType gradI;
-    MovingImageGradientType gradJ;
+    FixedImageGradientType  fixedImageGradient;
+    MovingImageGradientType movingImageGradient;
 
-    MovingImagePointType mappedMovingPoint;
+    MovingImagePointType    mappedMovingPoint;
 
   } ScanMemType;
 
-  typedef struct ScanParaType {
+  typedef struct ScanParametersType {
     // const values during scanning
-    ImageRegionType scan_region;
-    SizeValueType number_of_fill_zero; // for each queue
-    SizeValueType window_length; // number of voxels in the scanning window
-    IndexValueType scan_region_begin_index_dim0;
+    ImageRegionType scanRegion;
+    SizeValueType   numberOfFillZero; // for each queue
+    SizeValueType   windowLength; // number of voxels in the scanning window
+    IndexValueType  scanRegionBeginIndexDim0;
 
-    typename FixedImageType::ConstPointer I;
-    typename MovingImageType::ConstPointer J;
-    typename VirtualImageType::ConstPointer V;
-    RadiusType r;
+    typename FixedImageType::ConstPointer   fixedImage;
+    typename MovingImageType::ConstPointer  movingImage;
+    typename VirtualImageType::ConstPointer virtualImage;
+    RadiusType radius;
 
-  } ScanParaType;
+  } ScanParametersType;
 
   // computation routines for normalized cross correlation
 
-  void InitializeScanning(const ImageRegionType &scan_region,
-    ScanningIteratorType &scan_it, ScanMemType &scan_mem,
-    ScanParaType &scan_para ) const;
+  /** Create an iterator over the virtual sub region */
+  void InitializeScanning(const ImageRegionType &scanRegion,
+    ScanIteratorType &scanIt, ScanMemType &scanMem,
+    ScanParametersType &scanParameters ) const;
 
-  void UpdateQueuesAtBeginingOfLine(
-    const ScanningIteratorType &scan_it, ScanMemType &scan_mem,
-    const ScanParaType &scan_para,
-    const ThreadIdType threadID) const;
+  /** \class NeighborhoodScanningWindowGetValueAndDerivativeThreader
+   * \brief Processes points for NeighborhoodScanningWindow calculation.
+   * \ingroup ITKHighDimensionalMetrics
+   * */
+  template < class TDomainPartitioner >
+  class NeighborhoodScanningWindowGetValueAndDerivativeThreader
+    : public ImageToImageObjectMetric< TFixedImage, TMovingImage, TVirtualImage >::template GetValueAndDerivativeThreader< TDomainPartitioner >
+  {
+  public:
+    /** Standard class typedefs. */
+    typedef NeighborhoodScanningWindowGetValueAndDerivativeThreader    Self;
+    typedef typename ImageToImageObjectMetric< TFixedImage, TMovingImage, TVirtualImage >
+      ::template GetValueAndDerivativeThreader< TDomainPartitioner >                 Superclass;
+    typedef SmartPointer< Self >                                            Pointer;
+    typedef SmartPointer< const Self >                                      ConstPointer;
 
-  // Increment the iterator and check to see if we're at the end of the
-  // line.  If so, go to the next line.  Otherwise, add the
-  // the values for the next hyperplane.
-  void UpdateQueuesToNextScanWindow(
-    const ScanningIteratorType &scan_it, ScanMemType &scan_mem,
-    const ScanParaType &scan_para,
-    const ThreadIdType threadID) const;
+    itkTypeMacro( NeighborhoodScanningWindowImageToImageObjectMetric::NeighborhoodScanningWindowGetValueAndDerivativeThreader,
+      ImageToImageObjectMetric::GetValueAndDerivativeThreader );
 
-  void UpdateQueues(const ScanningIteratorType &scan_it,
-    ScanMemType &scan_mem, const ScanParaType &scan_para,
-    ThreadIdType threadID) const;
+    typedef typename Superclass::DomainType         DomainType;
+    typedef typename Superclass::EnclosingClassType EnclosingClassType;
 
-  bool ComputeInformationFromQueues(
-    const ScanningIteratorType &scan_it, ScanMemType &scan_mem,
-    const ScanParaType &scan_para,
-    const ThreadIdType threadID) const;
+  protected:
+    /** \c ProcessVirtualPoint and \c ProcessPoint are not used in the
+     * NeighborhoodScanningWindowGetValueAndDerivativeThreader implementation.
+     * */
+    virtual bool ProcessPoint(
+          const VirtualPointType &          itkNotUsed(virtualPoint),
+          const FixedImagePointType &       itkNotUsed(mappedFixedPoint),
+          const FixedImagePixelType &       itkNotUsed(mappedFixedPixelValue),
+          const FixedImageGradientType &    itkNotUsed(mappedFixedImageGradient),
+          const MovingImagePointType &      itkNotUsed(mappedMovingPoint),
+          const MovingImagePixelType &      itkNotUsed(mappedMovingPixelValue),
+          const MovingImageGradientType &   itkNotUsed(mappedMovingImageGradient),
+          MeasureType &                     itkNotUsed(metricValueReturn),
+          DerivativeType &                  itkNotUsed(localDerivativeReturn),
+          const ThreadIdType                itkNotUsed(threadID) ) const
+      {
+      return false;
+      }
+  };
 
- void ComputeMovingTransformDerivative(
-    const ScanningIteratorType &scan_it, ScanMemType &scan_mem,
-    const ScanParaType &scan_para, DerivativeType &deriv,
-    MeasureType &local_cc, const ThreadIdType threadID) const;
+  /** \class NeighborhoodScanningWindowDenseGetValueAndDerivativeThreader
+   * \brief Run on the points defined by an ImageRegion.
+   * \ingroup ITKHighDimensionalMetrics
+   * */
+  class NeighborhoodScanningWindowDenseGetValueAndDerivativeThreader
+    : public NeighborhoodScanningWindowGetValueAndDerivativeThreader< ThreadedImageRegionPartitioner< VirtualImageDimension > >
+  {
+  public:
+    /** Standard class typedef. */
+    typedef NeighborhoodScanningWindowDenseGetValueAndDerivativeThreader                              Self;
+    typedef NeighborhoodScanningWindowGetValueAndDerivativeThreader< ThreadedImageRegionPartitioner< VirtualImageDimension > >
+                                                                            Superclass;
+    typedef SmartPointer< Self >                                            Pointer;
+    typedef SmartPointer< const Self >                                      ConstPointer;
 
-protected:
+    itkNewMacro( Self );
 
-  ANTSNeighborhoodCorrelationImageToImageObjectMetric();
-  virtual ~ANTSNeighborhoodCorrelationImageToImageObjectMetric();
+    itkTypeMacro( NeighborhoodScanningWindowImageToImageObjectMetric::NeighborhoodScanningWindowDenseGetValueAndDerivativeThreader,
+      NeighborhoodScanningWindowImageToImageObjectMetric::GetValueAndDerivativeThreader );
+
+    typedef typename Superclass::DomainType         DomainType;
+    typedef typename Superclass::EnclosingClassType EnclosingClassType;
+
+  protected:
+    NeighborhoodScanningWindowDenseGetValueAndDerivativeThreader() {}
+
+    virtual void ThreadedExecution( EnclosingClassType * enclosingClass,
+                                    const DomainType& domain,
+                                    const ThreadIdType threadId );
+
+    /** Update the queues for the next point.  Calls either \c
+     * UpdateQueuesAtBeginningOfLine or \c UpdateQueuesToNextScanWindow. */
+    void UpdateQueues(const ScanIteratorType &scanIt,
+      ScanMemType &scanMem, const ScanParametersType &scanParameters,
+      ThreadIdType threadID) const;
+
+    void UpdateQueuesAtBeginningOfLine(
+      const ScanIteratorType &scanIt, ScanMemType &scanMem,
+      const ScanParametersType &scanParameters,
+      const ThreadIdType threadID) const;
+
+    /** Increment the iterator and check to see if we're at the end of the
+     * line.  If so, go to the next line.  Otherwise, add the
+     * the values for the next hyperplane. */
+    void UpdateQueuesToNextScanWindow(
+      const ScanIteratorType &scanIt, ScanMemType &scanMem,
+      const ScanParametersType &scanParameters,
+      const ThreadIdType threadID) const;
+
+    /** Test to see if there are any voxels we need to handle in the current
+     * window. */
+    bool ComputeInformationFromQueues(
+      const ScanIteratorType &scanIt, ScanMemType &scanMem,
+      const ScanParametersType &scanParameters,
+      const ThreadIdType threadID) const;
+
+    void ComputeMovingTransformDerivative(
+      const ScanIteratorType &scanIt, ScanMemType &scanMem,
+      const ScanParametersType &scanParameters, DerivativeType &deriv,
+      MeasureType &local_cc, const ThreadIdType threadID) const;
+
+  private:
+    NeighborhoodScanningWindowDenseGetValueAndDerivativeThreader( const Self & ); // purposely not implemented
+    void operator=( const Self & ); // purposely not implemented
+  };
 
   virtual void PrintSelf(std::ostream & os, Indent indent) const;
 
@@ -267,10 +338,6 @@ private:
       const Self &);
   //purposely not implemented
   void operator=(const Self &);
-
-  static void NeighborhoodScanningWindowGetValueAndDerivativeThreadedCallback(
-      const DenseThreaderInputObjectType& virtualImageSubRegion,
-      ThreadIdType threadID, Superclass * dataHolder);
 
   // Radius of the neighborhood window centered at each pixel
   RadiusType m_Radius;
