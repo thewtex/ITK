71ea590d6933fd269f69a9dae356dff00c670831
Baohua Wu <baowu99@gmail.com>
2011-10-23 02:28:22 -0400

    ENH: added QuasiNewtonObjectOptimizer and its tests
    1. QuasiNewtonObjectOptimizer with BFGS method
    
    2. Local support for QuasiNewtonObjectOptimizer
    
    3. itkQuasiNewtonObjectOptimizerTest
    
    4. itkQuasiNewtonObjectOptimizerMultiMetricTest
    
    5. Fixed m_Value in GetValueAndDerivative of
    JointHistogramMutualInformationImageToImageObjectMetric
    
    6. added local learning rates in QuasiNewtonObjectOptimizer
    
    Change-Id: Ib7a351b218c9120b24628062b7238310d3851cf0

---
+++ Modules/Numerics/HighDimensionalOptimizers/test/itkQuasiNewtonObjectOptimizerMultiMetricTest.cxx
@@ -0,0 +1,328 @@
+/*=========================================================================
+*
+*  Copyright Insight Software Consortium
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+
+/**
+ * Test program for DemonsImageToImageObjectMetric and
+ * QuasiNewtonObjectOptimizer classes.
+ *
+ * Perform a registration using user-supplied images.
+ * No numerical verification is performed. Test passes as long
+ * as no exception occurs.
+ */
+#include "itkDemonsImageToImageObjectMetric.h"
+#include "itkJointHistogramMutualInformationImageToImageObjectMetric.h"
+#include "itkQuasiNewtonObjectOptimizer.h"
+#include "itkRegistrationParameterScalesFromShift.h"
+
+#include "itkIdentityTransform.h"
+#include "itkTranslationTransform.h"
+#include "itkAffineTransform.h"
+#include "itkEuler2DTransform.h"
+#include "itkCompositeTransform.h"
+#include "itkGaussianSmoothingOnUpdateDisplacementFieldTransform.h"
+#include "itkRegistrationParameterScalesFromJacobian.h"
+
+#include "itkCastImageFilter.h"
+#include "itkLinearInterpolateImageFunction.h"
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCommand.h"
+#include "itksys/SystemTools.hxx"
+#include "itkResampleImageFilter.h"
+
+//FIXME We need these as long as we have to define ImageToData and
+// Array1DToData as a fwd-declare in itkImageToImageObjectMetric.h
+#include "itkImageToData.h"
+#include "itkArray1DToData.h"
+
+int itkQuasiNewtonObjectOptimizerMultiMetricTest(int argc, char *argv[])
+{
+
+  if( argc < 5 ||
+      ( strcmp(argv[1], "dm") != 0
+      && strcmp(argv[1], "mi") != 0
+      )
+    )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " dm|mi";
+    std::cerr << " fixedImageFile movingImageFile ";
+    std::cerr << " outputImageFile ";
+    std::cerr << " [numberOfIterations numberOfDisplacementIterations] ";
+    std::cerr << std::endl;
+    std::cerr << " The metric types are: " << std::endl;
+    std::cerr << "   dm   - DemonsImageToImageObjectMetric" << std::endl;
+    std::cerr << "   mi   - JointHistogramMutualInformationImageToImageObjectMetric" << std::endl;
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::string metricString = argv[1];
+  unsigned int numberOfIterations = 10;
+  unsigned int numberOfDisplacementIterations = 10;
+
+  if( argc >= 6 )
+    {
+    numberOfIterations = atoi( argv[5] );
+    }
+  if( argc >= 7 )
+    {
+    numberOfDisplacementIterations = atoi( argv[6] );
+    }
+  std::cout << " iterations "<< numberOfIterations
+    << " displacementIterations " << numberOfDisplacementIterations << std::endl;
+
+  const unsigned int Dimension = 2;
+  typedef double PixelType; //I assume png is unsigned short
+
+  typedef itk::Image< PixelType, Dimension >  FixedImageType;
+  typedef itk::Image< PixelType, Dimension >  MovingImageType;
+
+  typedef itk::ImageFileReader< FixedImageType  > FixedImageReaderType;
+  typedef itk::ImageFileReader< MovingImageType > MovingImageReaderType;
+
+  FixedImageReaderType::Pointer fixedImageReader   = FixedImageReaderType::New();
+  MovingImageReaderType::Pointer movingImageReader = MovingImageReaderType::New();
+
+  fixedImageReader->SetFileName( argv[2] );
+  movingImageReader->SetFileName( argv[3] );
+
+  //get the images
+  fixedImageReader->Update();
+  FixedImageType::Pointer  fixedImage = fixedImageReader->GetOutput();
+  movingImageReader->Update();
+  MovingImageType::Pointer movingImage = movingImageReader->GetOutput();
+
+  /** define a resample filter that will ultimately be used to deform the image */
+  typedef itk::ResampleImageFilter<
+                            MovingImageType,
+                            FixedImageType >    ResampleFilterType;
+  ResampleFilterType::Pointer resample = ResampleFilterType::New();
+
+
+  /** create a composite transform holder for other transforms  */
+  typedef itk::CompositeTransform<double, Dimension>    CompositeType;
+  typedef CompositeType::ScalarType                     ScalarType;
+
+  CompositeType::Pointer compositeTransform = CompositeType::New();
+
+  //create an affine transform
+  typedef itk::AffineTransform<double, Dimension>
+                                                    AffineTransformType;
+  AffineTransformType::Pointer affineTransform = AffineTransformType::New();
+  affineTransform->SetIdentity();
+  std::cout <<" affineTransform params " << affineTransform->GetParameters() << std::endl;
+  typedef itk::GaussianSmoothingOnUpdateDisplacementFieldTransform<
+                                                    double, Dimension>
+                                                     DisplacementTransformType;
+  DisplacementTransformType::Pointer displacementTransform =
+                                              DisplacementTransformType::New();
+  typedef DisplacementTransformType::DisplacementFieldType
+                                                         DisplacementFieldType;
+  DisplacementFieldType::Pointer field = DisplacementFieldType::New();
+
+  //set the field to be the same as the fixed image region, which will
+  // act by default as the virtual domain in this example.
+  field->SetRegions( fixedImage->GetLargestPossibleRegion() );
+  //make sure the field has the same spatial information as the image
+  field->CopyInformation( fixedImage );
+  std::cout << "fixedImage->GetLargestPossibleRegion(): "
+            << fixedImage->GetLargestPossibleRegion() << std::endl
+            << "fixedImage->GetBufferedRegion(): "
+            << fixedImage->GetBufferedRegion() << std::endl;
+  field->Allocate();
+  // Fill it with 0's
+  DisplacementTransformType::OutputVectorType zeroVector;
+  zeroVector.Fill( 0 );
+  field->FillBuffer( zeroVector );
+  // Assign to transform
+  displacementTransform->SetDisplacementField( field );
+  displacementTransform->SetGaussianSmoothingVarianceForTheUpdateField( 5 );
+  displacementTransform->SetGaussianSmoothingVarianceForTheTotalField( 6 );
+
+  //identity transform for fixed image
+  typedef itk::IdentityTransform<double, Dimension> IdentityTransformType;
+  IdentityTransformType::Pointer identityTransform = IdentityTransformType::New();
+  identityTransform->SetIdentity();
+
+  // The metric
+  typedef itk::ImageToImageObjectMetric
+    < FixedImageType, MovingImageType >         MetricBaseType;
+  MetricBaseType::Pointer metric;
+
+  if (metricString.compare("dm") == 0)
+    {
+    typedef itk::DemonsImageToImageObjectMetric
+      < FixedImageType, MovingImageType >           DemonsMetricType;
+    DemonsMetricType::Pointer demonsMetric = DemonsMetricType::New();
+    metric = demonsMetric.GetPointer();
+    }
+  else if (metricString.compare("mi") == 0)
+    {
+    typedef itk::JointHistogramMutualInformationImageToImageObjectMetric
+      < FixedImageType, MovingImageType >           MIMetricType;
+    typedef MIMetricType::FixedSampledPointSetType  PointSetType;
+    MIMetricType::Pointer miMetric = MIMetricType::New();
+    metric = miMetric.GetPointer();
+
+    miMetric->SetNumberOfHistogramBins(20);
+    typedef PointSetType::PointType     PointType;
+    PointSetType::Pointer               pset(PointSetType::New());
+    unsigned long ind=0,ct=0;
+    itk::ImageRegionIteratorWithIndex<FixedImageType> It(fixedImage, fixedImage->GetLargestPossibleRegion() );
+    for( It.GoToBegin(); !It.IsAtEnd(); ++It )
+      {
+      // take every N^th point
+      if ( ct % 20 == 0  ) // about a factor of 5 speed-up over dense
+        {
+          PointType pt;
+          fixedImage->TransformIndexToPhysicalPoint( It.GetIndex(), pt);
+          pset->SetPoint(ind, pt);
+          ind++;
+        }
+        ct++;
+      }
+      // brief profiling notes on mutual information affine registration macbook air , mi using every 20th point for sparse
+      //  1 thread dense = 10 sec
+      //  2 thread dense = 7.5  sec
+      //  1 thread sparse = 2.2 sec
+      //  2 thread sparse = 1.8 sec
+      // this uses only 1500 points so it's probably not a great multi-thread test for the sparse case
+    std::cout << "Setting point set with " << ind << " points of " << fixedImage->GetLargestPossibleRegion().GetNumberOfPixels() << " total " << std::endl;
+    miMetric->SetFixedSampledPointSet( pset );
+    miMetric->SetUseFixedSampledPointSet( true );
+    std::cout << "Testing metric with point set..." << std::endl;
+    }
+  // Assign images and transforms.
+  // By not setting a virtual domain image or virtual domain settings,
+  // the metric will use the fixed image for the virtual domain.
+//  metric->SetVirtualDomainImage( fixedImage );
+  metric->SetFixedImage( fixedImage );
+  metric->SetMovingImage( movingImage );
+  metric->SetFixedTransform( identityTransform );
+  metric->SetMovingTransform( affineTransform );
+  metric->SetDoFixedImagePreWarp( true );
+  metric->SetDoMovingImagePreWarp( true );
+  bool gaussian = false;
+  metric->SetUseMovingImageGradientFilter( gaussian );
+  metric->SetUseFixedImageGradientFilter( gaussian );
+  metric->Initialize();
+
+  typedef itk::RegistrationParameterScalesFromShift< MetricBaseType >
+    RegistrationParameterScalesFromShiftType;
+  RegistrationParameterScalesFromShiftType::Pointer shiftScaleEstimator
+    = RegistrationParameterScalesFromShiftType::New();
+  shiftScaleEstimator->SetMetric(metric);
+
+  std::cout << "First do an affine registration " << std::endl;
+  //typedef itk::GradientDescentObjectOptimizer  OptimizerType;
+  typedef itk::QuasiNewtonObjectOptimizer  OptimizerType;
+  OptimizerType::Pointer  optimizer = OptimizerType::New();
+  optimizer->SetMetric( metric );
+  optimizer->SetNumberOfIterations( numberOfIterations );
+  optimizer->SetScalesEstimator( shiftScaleEstimator );
+  try
+    {
+    optimizer->StartOptimization();
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cout << "Exception thrown ! " << std::endl;
+    std::cout << "An error ocurred during deformation Optimization:" << std::endl;
+    std::cout << e.GetLocation() << std::endl;
+    std::cout << e.GetDescription() << std::endl;
+    std::cout << e.what()    << std::endl;
+    std::cout << "Test FAILED." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Follow affine with deformable registration " << std::endl;
+  // now add the displacement field to the composite transform
+  compositeTransform->AddTransform( affineTransform );
+  compositeTransform->AddTransform( displacementTransform );
+  compositeTransform->SetAllTransformsToOptimizeOn(); //Set back to optimize all.
+  compositeTransform->SetOnlyMostRecentTransformToOptimizeOn(); //set to optimize the displacement field
+  metric->SetMovingTransform( compositeTransform );
+  metric->SetUseFixedSampledPointSet( false );
+  metric->Initialize();
+
+  // Optimizer
+  RegistrationParameterScalesFromShiftType::ScalesType
+    displacementScales( displacementTransform->GetNumberOfLocalParameters() );
+  displacementScales.Fill(1);
+  optimizer->SetMetric( metric );
+  optimizer->SetNumberOfIterations( numberOfDisplacementIterations );
+  optimizer->SetScalesEstimator( shiftScaleEstimator );
+  try
+    {
+    optimizer->StartOptimization();
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cout << "Exception thrown ! " << std::endl;
+    std::cout << "An error ocurred during deformation Optimization:" << std::endl;
+    std::cout << e.GetLocation() << std::endl;
+    std::cout << e.GetDescription() << std::endl;
+    std::cout << e.what()    << std::endl;
+    std::cout << "Test FAILED." << std::endl;
+    return EXIT_FAILURE;
+    }
+  std::cout << "...finished. " << std::endl;
+
+
+  //warp the image with the displacement field
+  resample->SetTransform( compositeTransform );
+  resample->SetInput( movingImageReader->GetOutput() );
+  resample->SetSize(    fixedImage->GetLargestPossibleRegion().GetSize() );
+  resample->SetOutputOrigin(  fixedImage->GetOrigin() );
+  resample->SetOutputSpacing( fixedImage->GetSpacing() );
+  resample->SetOutputDirection( fixedImage->GetDirection() );
+  resample->SetDefaultPixelValue( 0 );
+  resample->Update();
+  //write out the displacement field
+  typedef itk::ImageFileWriter< DisplacementFieldType >  DisplacementWriterType;
+  DisplacementWriterType::Pointer      displacementwriter =  DisplacementWriterType::New();
+  std::string outfilename( argv[4] );
+  std::string ext = itksys::SystemTools::GetFilenameExtension( outfilename );
+  std::string name = itksys::SystemTools::GetFilenameWithoutExtension( outfilename );
+  std::string defout = name + std::string("_def") + ext;
+  displacementwriter->SetFileName( defout.c_str() );
+  displacementwriter->SetInput( displacementTransform->GetDisplacementField() );
+  displacementwriter->Update();
+
+  //write the warped image into a file
+  //typedef double                                    OutputPixelType;
+  typedef unsigned short                            OutputPixelType;
+  typedef itk::Image< OutputPixelType, Dimension >  OutputImageType;
+  typedef itk::CastImageFilter<
+                        MovingImageType,
+                        OutputImageType >           CastFilterType;
+  typedef itk::ImageFileWriter< OutputImageType >   WriterType;
+  WriterType::Pointer      writer =  WriterType::New();
+  CastFilterType::Pointer  caster =  CastFilterType::New();
+  writer->SetFileName( argv[4] );
+  caster->SetInput( resample->GetOutput() );
+  writer->SetInput( caster->GetOutput() );
+  writer->Update();
+
+  std::cout << "Test PASSED." << affineTransform->GetParameters() << std::endl;
+  return EXIT_SUCCESS;
+
+}
