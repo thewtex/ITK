7e731a74ed14dd1c5e268e4d1125bc7878d771be
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-11-03 17:18:26 -0400

    PERF: Implementing optimized image copy method
    Adding a distpatching of the ImageAlgorithm::Copy method to a
    standard image region iterator approach and to an optimized memcpy
    method.
    
    The dispatching is based on the templated image class, and the pixel
    type. Conservatively, only itk::Image, and itk::VectorImage classes may
    use the optimized method, additionally the internal pixel type must
    be POD (Plain Old Data) to use memcpy.
    
    POD detection is done with TR1's is_pod type trait. If the system
    does not have tr1 an optimized method will not be used.
    
    Change-Id: I1a64793dde7cdd95e5b2d3635f74c1af18a36a18

--- Modules/Core/Common/include/itkImageAlgorithm.hxx
+++ Modules/Core/Common/include/itkImageAlgorithm.hxx
@@ -26,9 +26,10 @@ namespace itk
 {
 
 template<typename InputImageType, typename OutputImageType >
-void ImageAlgorithm::Copy( const InputImageType *inImage, OutputImageType *outImage,
-                           const typename InputImageType::RegionType &inRegion,
-                           const typename OutputImageType::RegionType &outRegion )
+void ImageAlgorithm::DispatchedCopy( const InputImageType *inImage, OutputImageType *outImage,
+                                     const typename InputImageType::RegionType &inRegion,
+                                     const typename OutputImageType::RegionType &outRegion,
+                                     FalseType )
 {
   itk::ImageRegionConstIterator<InputImageType> it( inImage, inRegion );
   itk::ImageRegionIterator<OutputImageType> ot( outImage, outRegion );
@@ -40,6 +41,110 @@ void ImageAlgorithm::Copy( const InputImageType *inImage, OutputImageType *outIm
     ++it;
     }
 }
+
+
+template<typename ImageType >
+void ImageAlgorithm::DispatchedCopy( const ImageType *inImage, ImageType *outImage,
+                                     const typename ImageType::RegionType &inRegion,
+                                     const typename ImageType::RegionType &outRegion,
+                                     TrueType )
+{
+  typedef typename ImageType::RegionType RegionType;
+  typedef typename ImageType::IndexType  IndexType;
+
+  typedef typename ImageType::PixelType PixelType;
+
+  // We wish to copy whole lines, otherwise just use the basic implementation.
+  if ( inRegion.GetSize()[0] != outRegion.GetSize()[0] )
+    {
+    ImageAlgorithm::DispatchedCopy<ImageType, ImageType>( inImage, outImage, inRegion, outRegion );
+    }
+
+  // Get the number of bytes of each pixel in the buffer.
+  size_t PixelSize = ImageAlgorithm::PixelSize<ImageType>::Get( inImage );
+
+  const void *in = inImage->GetBufferPointer();
+  void *out = outImage->GetBufferPointer();
+
+  const RegionType &inBufferedRegion = inImage->GetBufferedRegion();
+  const RegionType &outBufferedRegion = outImage->GetBufferedRegion();
+
+  ImageRegionConstIterator<ImageType> it( inImage, inRegion );
+
+  // Compute the number of continuous pixel which can be copied.
+  size_t numberOfPixel = 1;
+  unsigned int    movingDirection = 0;
+  do
+    {
+    numberOfPixel *= inRegion.GetSize(movingDirection );
+    ++movingDirection;
+    }
+  // The copy regions must extend to the full buffered region, to
+  // ensure continuity of pixels between dimensions.
+  while ( movingDirection < RegionType::ImageDimension
+          && inRegion.GetSize( movingDirection - 1 ) == inBufferedRegion.GetSize( movingDirection - 1 )
+          && outRegion.GetSize( movingDirection - 1 ) == outBufferedRegion.GetSize( movingDirection - 1 )
+          && inBufferedRegion.GetSize(movingDirection - 1) == outBufferedRegion.GetSize(movingDirection - 1) );
+
+  const size_t sizeOfChunk = numberOfPixel*PixelSize;
+
+  IndexType inCurrentIndex = inRegion.GetIndex();
+  IndexType outCurrentIndex = outRegion.GetIndex();
+
+  while ( inRegion.IsInside( inCurrentIndex ) )
+    {
+    size_t inOffset = 0; // in bytes
+    size_t outOffset = 0;
+
+    size_t inSubDimensionQuantity = 1; // in pixels
+    size_t outSubDimensionQuantity = 1;
+
+    for (unsigned int i = 0; i < RegionType::ImageDimension; ++i )
+      {
+      inOffset += inSubDimensionQuantity*PixelSize*static_cast<size_t>( inCurrentIndex[i] - inBufferedRegion.GetIndex(i) );
+      inSubDimensionQuantity *= inBufferedRegion.GetSize(i);
+
+      outOffset += outSubDimensionQuantity*PixelSize*static_cast<size_t>( outCurrentIndex[i] - outBufferedRegion.GetIndex(i) );
+      outSubDimensionQuantity *= outBufferedRegion.GetSize(i);
+      }
+
+    const char *inBuffer = static_cast<const char*>(in) + inOffset;
+    char* outBuffer = static_cast<char*>(out) + outOffset;
+
+    memcpy( outBuffer, inBuffer, sizeOfChunk );
+
+    if ( movingDirection == RegionType::ImageDimension )
+      {
+      break;
+      }
+
+    // increment index to next chunk
+    ++inCurrentIndex[movingDirection];
+    for ( unsigned int i = movingDirection; i + 1 < RegionType::ImageDimension; ++i )
+      {
+      // When reaching the end of the moving index in the copy region
+      // dimension, carry to higher dimensions.
+      if ( static_cast<SizeValueType>(inCurrentIndex[i]  - inRegion.GetIndex(i)) >= inRegion.GetSize(i) )
+        {
+        inCurrentIndex[i] = inRegion.GetIndex(i);
+        ++inCurrentIndex[i + 1];
+        }
+      }
+
+    // increment index to next chunk
+    ++outCurrentIndex[movingDirection];
+    for ( unsigned int i = movingDirection; i + 1 < RegionType::ImageDimension; ++i )
+      {
+      if ( static_cast<SizeValueType>(outCurrentIndex[i]  - outRegion.GetIndex(i)) >= outRegion.GetSize(i) )
+        {
+        outCurrentIndex[i] = outRegion.GetIndex(i);
+        ++outCurrentIndex[i + 1];
+        }
+      }
+
+    }
+}
+
 
 } // end namespace itk
 
