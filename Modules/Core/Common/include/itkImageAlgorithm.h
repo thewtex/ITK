05fb1fa173398268cde1ce2c17c9fff2fdc92f43
Bradley Lowekamp <blowekamp@mail.nih.gov>
2011-11-03 17:18:26 -0400

    WIP: Implementing optimized image copy method
    Adding an distpatching of the ImageAlgorithm::Copy method to a
    standard image region iterator approach and to an optimized memcpy
    method.
    
    The dispatching is based on the templated image class, and the pixel
    type. Conservatively only itk::Image, and itk::VectorImage classes may
    used the optimized method, additionally the internal pixel type must
    be pod to use memcpy.
    
    POD detection was done with TR1's is_pod type trait. If the system
    does not have tr1 not optimized method will be used.
    
    TODO: Posting to gerrit to see if there are large objections to this
    dispatch approach and it's restrictions, while I am adding more testing.
    
    Change-Id: I1a64793dde7cdd95e5b2d3635f74c1af18a36a18

--- Modules/Core/Common/include/itkImageAlgorithm.h
+++ Modules/Core/Common/include/itkImageAlgorithm.h
@@ -18,8 +18,21 @@
 #ifndef __itkImageAlgorithm_h
 #define __itkImageAlgorithm_h
 
+#include "itkImageRegionIterator.h"
+
+#ifdef ITK_HAS_STLTR1_TR1_TYPE_TRAITS
+#  include <tr1/type_traits>
+#elif defined ITK_HAS_STLTR1_TYPE_TRAITS
+#  include <type_traits>
+#else
+#  include "itkIsSame.h"
+#endif
+
 namespace itk
 {
+
+template <typename TPixelType, unsigned int VImageDimension > class VectorImage;
+
 
 /** \class ImageAlgorithm
  * \brief A container of static functions which can operate on Images
@@ -33,6 +46,15 @@ namespace itk
  */
 struct ImageAlgorithm
 {
+
+#if defined(ITK_HAS_STLTR1_TR1_TYPE_TRAITS) || defined(ITK_HAS_STLTR1_TYPE_TRAITS)
+  typedef std::tr1::true_type  TrueType;
+  typedef std::tr1::false_type FalseType;
+#else
+  typedef itk::TrueType  TrueType;
+  typedef itk::FalseType FalseType;
+#endif
+
 /**
  * \brief This generic function copies a region from one image to
  * another. It may perform optimizations on the copy for efficiency.
@@ -53,7 +75,53 @@ struct ImageAlgorithm
   template<typename InputImageType, typename OutputImageType >
   static void Copy( const InputImageType *inImage, OutputImageType *outImage,
                     const typename InputImageType::RegionType &inRegion,
-                    const typename OutputImageType::RegionType &outRegion );
+                    const typename OutputImageType::RegionType &outRegion )
+  {
+    ImageAlgorithm::DispatchedCopy( inImage, outImage, inRegion, outRegion );
+  }
+
+
+  template< class TPixel, unsigned int VImageDimension >
+  static void Copy( const Image<TPixel, VImageDimension> * inImage,
+                               Image<TPixel, VImageDimension> * outImage,
+                               const typename Image<TPixel, VImageDimension>::RegionType &inRegion,
+                               const typename Image<TPixel, VImageDimension>::RegionType &outRegion )
+  {
+    typedef Image<TPixel, VImageDimension> ImageType;
+    ImageAlgorithm::DispatchedCopy( inImage, outImage, inRegion, outRegion
+#if defined(ITK_HAS_STLTR1_TR1_TYPE_TRAITS) || defined(ITK_HAS_STLTR1_TYPE_TRAITS)
+                                    , std::tr1::is_pod<typename ImageType::InternalPixelType>()
+#endif
+      );
+  }
+
+  template< class TPixel, unsigned int VImageDimension >
+  static void Copy( const VectorImage<TPixel, VImageDimension> * inImage,
+                               VectorImage<TPixel, VImageDimension> * outImage,
+                               const typename VectorImage<TPixel, VImageDimension>::RegionType &inRegion,
+                               const typename VectorImage<TPixel, VImageDimension>::RegionType &outRegion )
+  {
+    typedef Image<TPixel, VImageDimension> ImageType;
+    ImageAlgorithm::DispatchedCopy( inImage, outImage, inRegion, outRegion
+#if defined(ITK_HAS_STLTR1_TR1_TYPE_TRAITS) || defined(ITK_HAS_STLTR1_TYPE_TRAITS)
+                                    , std::tr1::is_pod<typename ImageType::InternalPixelType>()
+#endif
+      );
+  }
+
+
+private:
+
+
+  template<typename ImageType >
+  static void DispatchedCopy( const ImageType *inImage, ImageType *outImage,
+                              const typename ImageType::RegionType &inRegion,
+                              const typename ImageType::RegionType &outRegion, TrueType isPod );
+
+  template<typename InputImageType, typename OutputImageType >
+  static void DispatchedCopy( const InputImageType *inImage, OutputImageType *outImage,
+                              const typename InputImageType::RegionType &inRegion,
+                              const typename OutputImageType::RegionType &outRegion, FalseType isPod = FalseType() );
 };
 } // end namespace itk
 
--- Modules/Core/Common/include/itkImageAlgorithm.hxx
+++ Modules/Core/Common/include/itkImageAlgorithm.hxx
@@ -26,9 +26,10 @@ namespace itk
 {
 
 template<typename InputImageType, typename OutputImageType >
-void ImageAlgorithm::Copy( const InputImageType *inImage, OutputImageType *outImage,
-                           const typename InputImageType::RegionType &inRegion,
-                           const typename OutputImageType::RegionType &outRegion )
+void ImageAlgorithm::DispatchedCopy( const InputImageType *inImage, OutputImageType *outImage,
+                                     const typename InputImageType::RegionType &inRegion,
+                                     const typename OutputImageType::RegionType &outRegion,
+                                     FalseType )
 {
   itk::ImageRegionConstIterator<InputImageType> it( inImage, inRegion );
   itk::ImageRegionIterator<OutputImageType> ot( outImage, outRegion );
@@ -40,6 +41,119 @@ void ImageAlgorithm::Copy( const InputImageType *inImage, OutputImageType *outIm
     ++it;
     }
 }
+
+
+template<typename ImageType >
+void ImageAlgorithm::DispatchedCopy( const ImageType *inImage, ImageType *outImage,
+                                     const typename ImageType::RegionType &inRegion,
+                                     const typename ImageType::RegionType &outRegion,
+                                     TrueType )
+{
+  typedef typename ImageType::RegionType RegionType;
+  typedef typename ImageType::IndexType  IndexType;
+
+  typedef typename ImageType::PixelType PixelType;
+
+  if ( inRegion.GetSize()[0] != inRegion.GetSize()[0] )
+    {
+    ImageAlgorithm::DispatchedCopy<ImageType, ImageType>( inImage, outImage, inRegion, outRegion );
+    }
+
+
+  size_t PixelSize= 0;
+  if ( strcmp(inImage->GetNameOfClass(), "VectorImage") == 0 )
+    {
+    PixelSize = ImageType::AccessorFunctorType::GetVectorLength(inImage) * sizeof(typename NumericTraits<PixelType>::ValueType);
+    }
+  else
+    {
+    PixelSize = sizeof( PixelType );
+    }
+
+  const void *in = inImage->GetBufferPointer();
+  void *out = outImage->GetBufferPointer();
+
+  const RegionType &inBufferedRegion = inImage->GetBufferedRegion();
+  const RegionType &outBufferedRegion = outImage->GetBufferedRegion();
+
+  ImageRegionConstIterator<ImageType> it( inImage, inRegion );
+
+  // compute the number of continuous pixel which can be copied
+  size_t numberOfPixel = 1;
+  unsigned int    movingDirection = 0;
+  do
+    {
+    numberOfPixel *= inRegion.GetSize(movingDirection );
+    ++movingDirection;
+    }
+
+  // The copy regions must extend to the full buffered region, to
+  // ensure continuity of pixel between dimensions
+  while ( movingDirection < RegionType::ImageDimension
+          && inRegion.GetSize( movingDirection - 1 ) == inBufferedRegion.GetSize( movingDirection - 1 )
+          && outRegion.GetSize( movingDirection - 1 ) == outBufferedRegion.GetSize( movingDirection - 1 )
+          && inBufferedRegion.GetSize(movingDirection - 1) == outBufferedRegion.GetSize(movingDirection - 1) );
+
+  size_t sizeOfChunk = numberOfPixel*PixelSize;
+
+  IndexType inCurrentIndex = inRegion.GetIndex();
+  IndexType outCurrentIndex = outRegion.GetIndex();
+
+  while ( inRegion.IsInside( inCurrentIndex ) )
+    {
+    size_t inOffset = 0; // in bytes
+    size_t outOffset = 0;
+
+    size_t inSubDimensionQuantity = 1; // in pixels
+    size_t outSubDimensionQuantity = 1;
+
+    for (unsigned int i = 0; i < RegionType::ImageDimension; ++i )
+      {
+      inOffset += inSubDimensionQuantity*PixelSize*static_cast<size_t>( inCurrentIndex[i] - inBufferedRegion.GetIndex(i) );
+      inSubDimensionQuantity *= inBufferedRegion.GetSize(i);
+
+      outOffset += outSubDimensionQuantity*PixelSize*static_cast<size_t>( outCurrentIndex[i] - outBufferedRegion.GetIndex(i) );
+      outSubDimensionQuantity *= outBufferedRegion.GetSize(i);
+      }
+
+    const char *inBuffer = static_cast<const char*>(in) + inOffset;
+    char* outBuffer = static_cast<char*>(out) + outOffset;
+
+    memcpy( outBuffer, inBuffer, sizeOfChunk );
+
+
+    if ( movingDirection == RegionType::ImageDimension )
+      {
+      break;
+      }
+
+    // increment index to next chunk
+    ++inCurrentIndex[movingDirection];
+    for ( unsigned int i = movingDirection; i < RegionType::ImageDimension - 1; ++i )
+      {
+      // when reaching the end of the moving index in the copy region
+      // dimension carry to higher dimensions
+      if ( static_cast<SizeValueType>(inCurrentIndex[i]  - inRegion.GetIndex(i)) >= inRegion.GetSize(i) )
+        {
+        inCurrentIndex[i] = inRegion.GetIndex(i);
+        ++inCurrentIndex[i + 1];
+        }
+      }
+
+    // increment index to next chunk
+    ++outCurrentIndex[movingDirection];
+    for ( unsigned int i = movingDirection; i < RegionType::ImageDimension - 1; ++i )
+      {
+      if ( static_cast<SizeValueType>(outCurrentIndex[i]  - outRegion.GetIndex(i)) >= outRegion.GetSize(i) )
+        {
+        outCurrentIndex[i] = outRegion.GetIndex(i);
+        ++outCurrentIndex[i + 1];
+        }
+      }
+
+    }
+}
+
 
 } // end namespace itk
 
