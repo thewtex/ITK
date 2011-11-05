233352552e58b8f1709069fe35fb6ff89f7f9c44
Hans Johnson <hans-johnson@uiowa.edu>
2011-11-05 17:20:23 -0500

    COMP: Removed unreachable code.
    This also implements the "Prefer
    initialization to assignment" rule.
    
    warning #177: variable "" was declared but never referenced
    warning #593: variable "" was set but never used
    remark #185: dynamic initialization in unreachable code
    remark #111: statement is unreachable
    
    The intended behavior of the preprocessing
    code was not properly implemented, and the
    code was never executed.
    
    remark #193: zero used for undefined preprocessing identifier "TDimension"
    
    Change-Id: I8a4840e5ad248202b22b0d57fc44b5a3a9594dbb

--- Modules/ThirdParty/MetaIO/src/MetaIO/metaMesh.cxx
+++ Modules/ThirdParty/MetaIO/src/MetaIO/metaMesh.cxx
@@ -626,8 +626,8 @@ M_Read(void)
                             << METAIO_STREAM::endl;
         METAIO_STREAM::cout << "   ideal = " << readSize << " : actual = " << gc
                             << METAIO_STREAM::endl;
-        return false;
         delete [] _data;
+        return false;
         }
 
       int i=0;
