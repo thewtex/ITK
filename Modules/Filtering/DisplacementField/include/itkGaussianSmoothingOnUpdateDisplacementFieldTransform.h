a2b0c79c0594330f6353d4563c2261f87e931f21
Kent Williams <norman-k-williams@uiowa.edu>
2011-11-17 12:27:04 -0600

    WIP: Add Clone Method to transform hierarchy.
    As far as I can tell this is the minimal sufficient
    set of changes to allow for cloning.  If there are other
    transform classes besides DisplacementField and Composite
    that require more than calling SetParameters & SetFixedParameters
    to be fully copied, they'll also have to override the virtual Clone
    method.
    
    Change-Id: I80b7ea623a7bcca9be2163665dab7a5f15a78240

--- Modules/Filtering/DisplacementField/include/itkGaussianSmoothingOnUpdateDisplacementFieldTransform.h
+++ Modules/Filtering/DisplacementField/include/itkGaussianSmoothingOnUpdateDisplacementFieldTransform.h
@@ -58,7 +58,10 @@ public:
                                                 DisplacementFieldTransform );
 
   /** New macro for creation of through a Smart Pointer */
-  itkSimpleNewMacro( Self );
+  itkNewMacro( Self );
+
+  /** implement type-specific clone method*/
+  itkTransformCloneMacro();
 
   /** Types from superclass */
   typedef typename Superclass::ScalarType               ScalarType;
@@ -103,6 +106,9 @@ protected:
   GaussianSmoothingOnUpdateDisplacementFieldTransform();
   virtual ~GaussianSmoothingOnUpdateDisplacementFieldTransform();
   void PrintSelf( std::ostream& os, Indent indent ) const;
+
+  /** Clone the current transform */
+  virtual typename Transform<TScalar,NDimensions,NDimensions>::Pointer InternalClone() const;
 
   /** Used in GaussianSmoothDisplacementField as variance for the
    * GaussianOperator */
--- Modules/Filtering/DisplacementField/include/itkTimeVaryingVelocityFieldTransform.h
+++ Modules/Filtering/DisplacementField/include/itkTimeVaryingVelocityFieldTransform.h
@@ -67,7 +67,10 @@ public:
   itkTypeMacro( TimeVaryingVelocityFieldTransform, DisplacementFieldTransform );
 
   /** New macro for creation of through a Smart Pointer */
-  itkSimpleNewMacro( Self );
+  itkNewMacro( Self );
+
+  /** implement type-specific clone method */
+  itkTransformCloneMacro();
 
   /** InverseTransform type. */
   typedef typename Superclass:: InverseTransformBasePointer InverseTransformBasePointer;
@@ -106,6 +109,9 @@ public:
    */
   typedef Image<OutputVectorType, TimeVaryingVelocityFieldDimension>  TimeVaryingVelocityFieldType;
   typedef typename TimeVaryingVelocityFieldType::Pointer              TimeVaryingVelocityFieldPointer;
+  typedef typename TimeVaryingVelocityFieldType::PointType            TimeVaryingVelocityFieldPointType;
+  typedef typename TimeVaryingVelocityFieldType::SpacingType          TimeVaryingVelocityFieldSpacingType;
+  typedef typename TimeVaryingVelocityFieldType::DirectionType        TimeVaryingVelocityFieldDirectionType;
 
   typedef VectorInterpolateImageFunction<TimeVaryingVelocityFieldType, ScalarType>  TimeVaryingVelocityFieldInterpolatorType;
   typedef typename TimeVaryingVelocityFieldInterpolatorType::Pointer                TimeVaryingVelocityFieldInterpolatorPointer;
@@ -226,6 +232,17 @@ protected:
 
   ScalarType                                m_LowerTimeBound;
   ScalarType                                m_UpperTimeBound;
+
+  /** Clone the current transform */
+  virtual typename Transform<TScalar,NDimensions,NDimensions>::Pointer InternalClone() const;
+
+  typename DisplacementFieldType::Pointer
+    CopyDisplacementField(const DisplacementFieldType *toCopy) const;
+  /**
+   * Convenience method which reads the information from the current
+   * displacement field into m_FixedParameters.
+   */
+  virtual void SetFixedParametersFromTimeVaryingVelocityField();
 
   /** The deformation field and its inverse (if it exists). */
   typename TimeVaryingVelocityFieldType::Pointer    m_TimeVaryingVelocityField;
