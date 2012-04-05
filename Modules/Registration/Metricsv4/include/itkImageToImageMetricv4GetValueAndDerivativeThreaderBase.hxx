99182325b487ba3cda7f2f657c9aadbfe89ceef4
Michael Stauffer <mstauff@verizon.net>
2012-04-04 20:35:22 -0400

    BUG: Workaround for bad access when resizing array
    ImageToImageMetricv4GetValueAndDerivativeThreaderBase:
    Sometimes (details not known yet) when
    resizing the m_DerivativesPerThread array in
    BeforeThreadedExecution() to a larger size, EXC_BAD_ACCESS is thrown.
    
    This workaround around clears the arrays first and avoids the
    exception, at least in the test case.
    
    Change-Id: I6f947210c393fdc5b4c1380de9e89946a97e53c7

--- Modules/Registration/Metricsv4/include/itkImageToImageMetricv4GetValueAndDerivativeThreaderBase.hxx
+++ Modules/Registration/Metricsv4/include/itkImageToImageMetricv4GetValueAndDerivativeThreaderBase.hxx
@@ -38,6 +38,19 @@ ImageToImageMetricv4GetValueAndDerivativeThreaderBase< TDomainPartitioner, TImag
   //---------------------------------------------------------------
   // Resize the per thread memory objects.
 
+  // If the size changes, call clear() first to avoid a bug
+  // that sometimes happens when resizing to a larger number
+  // of elements.
+  if( this->m_DerivativesPerThread.size() != this->GetNumberOfThreadsUsed() )
+    {
+    this->m_MeasurePerThread.clear();
+    this->m_NumberOfValidPointsPerThread.clear();
+    this->m_DerivativesPerThread.clear();
+    this->m_CompensatedDerivativesPerThread.clear();
+    this->m_LocalDerivativesPerThread.clear();
+    this->m_MovingTransformJacobianPerThread.clear();
+    }
+
   /* Per-thread results */
   this->m_MeasurePerThread.resize( this->GetNumberOfThreadsUsed() );
   this->m_NumberOfValidPointsPerThread.resize( this->GetNumberOfThreadsUsed() );
@@ -67,8 +80,7 @@ ImageToImageMetricv4GetValueAndDerivativeThreaderBase< TDomainPartitioner, TImag
        * use a single derivative container that's updated by region
        * in multiple threads.
        * Initialization to zero is done in main class. */
-      itkDebugMacro(
-        "ImageToImageMetricv4::Initialize: transform HAS local support\n");
+      itkDebugMacro("ImageToImageMetricv4::Initialize: transform HAS local support\n");
         /* Set each per-thread object to point to m_DerivativeResult for efficiency. */
         this->m_DerivativesPerThread[i].SetData(
                                       this->m_Associate->m_DerivativeResult->data_block(),
@@ -77,8 +89,7 @@ ImageToImageMetricv4GetValueAndDerivativeThreaderBase< TDomainPartitioner, TImag
       }
     else
       {
-      itkDebugMacro(
-      "ImageToImageMetricv4::Initialize: transform does NOT have local support\n");
+      itkDebugMacro("ImageToImageMetricv4::Initialize: transform does NOT have local support\n");
       /* Global transforms get a separate derivatives container for each thread
        * that holds the result over a particular image region.
        * Use a CompensatedSummation value to provide for better consistency between
