b03b7f428819dcf5d7118cd8f31b972f58efb271
Ren-Hui Gong <rhgong@gmail.com>
2012-05-08 23:51:56 -0400

    ENH: XML DOM for ITK preview 4.
    ITK provided functions to read objects from or write objects to XML files.
    However, the included XML parser uses the SAX (Simple API for XML)
    technique, which makes implementing user XML-based readers/writers quite
    difficult and error-prone. This is because the information are pushed to users
    during parsing, and the users have to keep the incomplete intermediate states
    and instantly handle every piece of incoming information.
    
    This patch adds DOM (Document Object Model) based XML reading/writing
    functions to ITK, which greatly simplifies the processes of XML-based
    object reading/writing. In this I/O model, reading/writing is with respect
    to an intermediate DOM object that represents an XML file. The advantages are
    that pulling/pushing information from/to an ITK object is easy, and
    reading/writing a DOM object from/to an XML file is performed implicitly.
    Information are cached in the intermediate DOM object, so the user doesn't
    have to track the states during the reading/writing process.
    
    This patch includes DOM-based add-ons to ITK library ITKIOXML, testings
    for the implementation, testing data (XML files), and examples that
    demonstrate the use of this new feature.
    
    Change-Id: Ibfacbac653e32f20ff1784d4a4349bed3cd92a56
    Signed-off-by: Ren-Hui Gong <rhgong@gmail.com>

---
+++ Modules/IO/XML/test/itkDOMTest5.cxx
@@ -0,0 +1,105 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+/*
+This program tests the itk::DOMReader and itk::DOMWriter update functions.
+*/
+
+#include "itkDOMTestObjectDOMReader.h"
+#include "itkDOMTestObjectDOMWriter.h"
+
+#include <iostream>
+#include "itkMacro.h"
+
+int itkDOMTest5( int argc, char* argv[] )
+{
+  if ( argc < 2 )
+    {
+    std::cerr << "arguments expected: <output>.DOMTestObject.xml" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  try
+    {
+    std::cout << "input file for w/r: " << argv[1] << std::endl;
+    std::cout << std::endl;
+
+    // create the test object
+    itk::DOMTestObject::Pointer testobj1 = itk::DOMTestObject::New();
+    // settings for foo
+    testobj1->SetFooValue( "Hello!" );
+    testobj1->SetFooFileName( "itkDOMTest5-output_foo.txt" );
+    // settings for bar
+    testobj1->SetBarValue( 12345 );
+    testobj1->SetBarFileName( "itkDOMTest5-output_bar.txt" );
+
+    std::cout << "foo value: " << testobj1->GetFooValue() << std::endl;
+    std::cout << "foo file : " << testobj1->GetFooFileName() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "bar value: " << testobj1->GetBarValue() << std::endl;
+    std::cout << "bar file : " << testobj1->GetBarFileName() << std::endl;
+    std::cout << std::endl;
+
+    // write the test object to an XML file
+    itk::DOMTestObjectDOMWriter::Pointer writer = itk::DOMTestObjectDOMWriter::New();
+    writer->SetInput( testobj1 );
+    writer->SetFileName( argv[1] );
+    writer->Update();
+
+    itk::DOMTestObject::Pointer testobj2;
+
+    // read the object back to memory from the disk
+    itk::DOMTestObjectDOMReader::Pointer reader = itk::DOMTestObjectDOMReader::New();
+    reader->SetFileName( argv[1] );
+    reader->Update();
+    testobj2 = reader->GetOutput();
+
+    // check whether the two test objects have the same values
+    std::cout << "returned foo value: " << testobj2->GetFooValue() << std::endl;
+    std::cout << "returned bar value: " << testobj2->GetBarValue() << std::endl;
+    std::cout << std::endl;
+    if ( testobj1->GetFooValue() != testobj2->GetFooValue() )
+      {
+      throw "foo values mismatch";
+      }
+    if ( testobj1->GetBarValue() != testobj2->GetBarValue() )
+      {
+      throw "bar values mismatch";
+      }
+
+    // testing is successful if reached here
+    }
+  catch ( const itk::ExceptionObject& eo )
+    {
+    eo.Print( std::cerr );
+    return EXIT_FAILURE;
+    }
+  catch ( const char* e )
+    {
+    std::cerr << e << std::endl;
+    return EXIT_FAILURE;
+    }
+  catch ( ... )
+    {
+    std::cerr << "Unknown exception caught!" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
