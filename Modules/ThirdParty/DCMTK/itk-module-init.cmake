d4d26878d48dd8c2def886aa523833bb71c5cb24
Kent Williams <norman-k-williams@uiowa.edu>
2012-05-29 11:32:42 -0500

    WIP: Add DCMTKImageIO to ITK.
    1. DCMTKImageIO -- original work by Andrew Wassem,
    Bill Ryan, and Alexandre Gouaillard.
    2. Add in ExternalProject support for DCMTK
    
    Change-Id: If128f3489e5ec6e7b03c92ae3d5449cdead83937

---
+++ Modules/ThirdParty/DCMTK/itk-module-init.cmake
@@ -0,0 +1,22 @@
+
+
+if(NOT ITK_USE_SYSTEM_DCMTK)
+  if(NOT WIN32)
+    set(lib_prefix lib)
+    set(lib_suffix .a)
+  else(NOT WIN32)
+    set(lib_prefix "")
+    set(lib_suffix .lib)
+  endif()
+
+  set(DCMTK_LIB ${ITK_BINARY_DIR}/Modules/ThirdParty/DCMTK/DCMTK_Prereqs/lib)
+  set(_ITKDCMTK_LIBRARIES dcmdata dcmimage dcmimgle dcmjpeg
+    dcmnet dcmpstat dcmqrdb dcmsr dcmtls ijg12 ijg16 ijg8 ofstd
+    oflog)
+
+  foreach(lib ${_ITKDCMTK_LIBRARIES})
+    add_library(${lib} STATIC IMPORTED)
+    set(libname ${DCMTK_LIB}/${lib_prefix}${lib}${lib_suffix})
+    set_property(TARGET ${lib} PROPERTY IMPORTED_LOCATION ${libname})
+  endforeach()
+endif(NOT ITK_USE_SYSTEM_DCMTK)
