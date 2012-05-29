407c2772cdc86b863ea608856e53b9bf41b8601b
Kent Williams <norman-k-williams@uiowa.edu>
2012-05-29 11:32:42 -0500

    WIP: Add DCMTKImageIO to ITK.
    1. DCMTKImageIO -- original work by Andrew Wassem,
    Bill Ryan, and Alexandre Gouaillard.
    2. Make DCMTK an ITK ThirdParty Module, that gets
    built as a ExternalProject
    
    Change-Id: If128f3489e5ec6e7b03c92ae3d5449cdead83937

---
+++ Modules/ThirdParty/DCMTK/CMake/dcmtkPatchScript.cmake
@@ -0,0 +1,44 @@
+set(dcmtk3rdParty ${dcmtkSource}/CMake/3rdparty.cmake)
+
+#
+# Newer versions of TIFF depend on JPEG
+file(READ ${dcmtk3rdParty} code)
+string(REPLACE "SET(LIBTIFF_LIBS \${TIFF_LIBRARY})
+"
+"SET(LIBTIFF_LIBS \${TIFF_LIBRARY})
+    list(APPEND LIBTIFF_LIBS \${JPEG_LIBRARY})
+" code "${code}")
+#
+# we pass in the TIFF and ZLIB library stuff,
+# running find_package for those libraries
+# screws up using the ITK versions
+if(NOT ITK_USE_SYSTEM_TIFF)
+  string(REPLACE "FIND_PACKAGE(TIFF)
+" "" code "${code}")
+endif()
+
+if(NOT ITK_USE_SYSTEM_ZLIB)
+string(REPLACE "FIND_PACKAGE(ZLIB)
+" "" code "${code}")
+endif()
+
+file(WRITE ${dcmtk3rdParty} "${code}")
+
+#
+# in order to use the ITK versions of ZLIB and TIFF
+# it is necessary to use the ITK symbol-mangled versions
+file(GLOB_RECURSE dcmtk_src "*.cc" "*.h")
+
+foreach (filename ${dcmtk_src})
+  #  message("patching ${filename}")
+  file(READ ${filename} sourcecode)
+  if(NOT ITK_USE_SYSTEM_ZLIB)
+    string(REPLACE "<zlib.h>"
+      "\"itk_zlib.h\"" sourcecode "${sourcecode}")
+  endif()
+  if(NOT ITK_USE_SYSTEM_TIFF)
+    string(REPLACE "<tiffio.h>"
+      "\"itk_tiff.h\"" sourcecode "${sourcecode}")
+  endif()
+  file(WRITE ${filename} "${sourcecode}")
+endforeach(filename)
