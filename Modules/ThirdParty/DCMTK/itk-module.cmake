7daf174ad5b898247dd0c3d745b2340497731940
Kent Williams <norman-k-williams@uiowa.edu>
2012-05-29 11:32:42 -0500

    ENH: Add DCMTKImageIO to ITK
    1. DCMTKImageIO -- original work by Andrew Wassem,
    Bill Ryan, and Alexandre Gouaillard.
    
    2. Make DCMTK an ITK ThirdParty Module, that gets
    built as a ExternalProject
    
    Change-Id: I4e26c96756a2e72968f33c522eb9d81d5b098244

---
+++ Modules/ThirdParty/DCMTK/itk-module.cmake
@@ -0,0 +1,12 @@
+set(DOCUMENTATION "This module builds the DCMTK library <a
+href=\"http://dicom.offis.de/dcmtk.php.en\">DCMTK</a> DICOM
+library suite.")
+
+itk_module(ITKDCMTK
+  DEPENDS
+  ITKZLIB
+  ITKJPEG
+  ITKTIFF
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
