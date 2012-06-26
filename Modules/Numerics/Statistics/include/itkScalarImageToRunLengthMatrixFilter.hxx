da1d9367338d26e4bcbbbda9e0eee63932460ffc
Bradley Lowekamp <blowekamp@mail.nih.gov>
2012-06-25 23:48:11 -0400

    COMP: fix compilation error where IndexType is undefined
    I have not confirmed this patch on the failing systems, with DEBUG enabled.
    
    Change-Id: Icf48e79492825ad48997c115bf3bcb255daf3ddc

--- Modules/Numerics/Statistics/include/itkScalarImageToRunLengthMatrixFilter.hxx
+++ Modules/Numerics/Statistics/include/itkScalarImageToRunLengthMatrixFilter.hxx
@@ -270,7 +270,7 @@ ScalarImageToRunLengthMatrixFilter<TImageType, THistogramFrequencyContainer>
         {
         output->IncreaseFrequencyOfMeasurement( run, 1 );
 
-        itkDebugStatement(typename THistogramFrequencyContainer::IndexType tempMeasurementIndex;)
+        itkDebugStatement(typename HistorgramType::IndexType tempMeasurementIndex;)
         itkDebugStatement(output->GetIndex(run,tempMeasurementIndex);)
         itkDebugMacro( "centerIndex<->index: "
             << static_cast<int>( centerPixelIntensity )
