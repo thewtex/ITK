d4d4599684438cf637b36e2d2b83d677210a92a4
Nick Tustison <ntustison@gmail.com>
2012-07-18 21:28:30 -0400

    ENH:  Add GetTransformCategory() to translation transform.
    Change-Id: Icd273dfec70ef05dbd6064ca07857356a3ee739d

--- Modules/Core/Transform/include/itkTranslationTransform.h
+++ Modules/Core/Transform/include/itkTranslationTransform.h
@@ -191,6 +191,14 @@ public:
     return true;
   }
 
+  /** Indicates the category transform.
+   *  e.g. an affine transform, or a local one, e.g. a deformation field.
+   */
+  virtual TransformCategoryType GetTransformCategory() const
+  {
+    return Self::Linear;
+  }
+
   /** Set the fixed parameters and update internal transformation.
    * The Translation Transform does not require fixed parameters,
    * therefore the implementation of this method is a null operation. */
