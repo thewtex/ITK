c41dec827f6a329fc37dcf83ca0966b5fd03fe0d
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-12-20 19:29:19 -0500

    WIP: Adding libminc and updating MINC IO support.
    Added libminc library inside Modules/ThirdParty/MINC to compile withing ITK.
    Also rewrote MINC IO from Modules/Nonunit/Review/  to work correctly,
    also added extensive testing to the module.
    
    WIP: Modified UseITK.cmake to include automatic MINCIO initialization
    
    WIP: MINC2 Metadata reading and writing is now supported and compatible with ITK standard
    
    WIP: Dimension order and step sign is now preserved
    
    WIP: Run uncrustify on all source code to satisfy ITK style requirements
    
    WIP: Fixed error with wrong order of dimensions
    
    WIP: Reduced number of tests
    
    WIP: Fixed error in interslice normalization when reading floating point volumes
         in libminc, so updated GIT TAG
    
    WIP: Made libminc to compile statically always and with itk_ prefix to avoid
         potential conflict with already installed libminc
    
    WIP: Fixed a bug in underlying libminc library, updated GIT tag
    
    WIP: Fixed a bug while reading vector images or time-series with zero time step
    
    WIP: Updated to latest version of libminc
    
    WIP: Making more tests
    
    WIP: Fixed logical error when reading files with global normalization
    
    WIP: Added tests for 4D Dti data and Labels
    
    WIP: Forgot to change libminc git id
    
    WIP: Reduced number of tests
    
    WIP: Updated to latest version of libminc
    
    WIP: Fixed a bug in libminc which affected building MINC2 module
    
    Change-Id: Ie9ed4151dca83d5854cc713536a9594dc520c15a

---
+++ Modules/IO/MINC/itk-module.cmake
@@ -0,0 +1,13 @@
+set(DOCUMENTATION "This module contains classes for reading and writing image
+files in the MINC 2 file Format (mnc).")
+
+itk_module(ITKIOMINC
+  DEPENDS
+    ITKMINC
+    ITKIOImageBase
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKImageStatistics
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
