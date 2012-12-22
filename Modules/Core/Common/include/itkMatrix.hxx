da3e339dd9f86fb3a5ae45ba2b6256ec8c8898eb
Hans Johnson <hans-johnson@uiowa.edu>
2012-12-21 18:54:48 -0600

    PERF: Improve perf of ANTSNeighborhoodCorrelation
    This is a series of code changes and performance improvements
    that have been found to improve overall appication exectution
    time by 5% for a small test case that uses the ANTS
    NeighborhoodCorrelation.
    
    The major speed improvement is from the specialized computation of
    of matrix * vector computations for the 3D case by avoiding
    class initialization, and by using "prefer-initialization to
    assignment", wherever possible.
    
    Change-Id: Ibef853b6bccab233ec9747da4b3d3399e55b292b

--- Modules/Core/Common/include/itkMatrix.hxx
+++ Modules/Core/Common/include/itkMatrix.hxx
@@ -26,6 +26,34 @@ namespace itk
 /**
  *  Product by a Vector
  */
+//Paritally specialized versions for performance of common cases
+Vector< double, 2 >
+Matrix< double, 2, 2 >
+::operator*(const Vector< double, 2 > & vect) const
+{
+  Vector< double, 2 > result;
+  double * const rr=&result[0];
+  double const * const vv=&vect[0];
+  double const * const A=m_Matrix[0]; //Get a pointer to the first element of array
+  rr[0]  = A[0] * vv[0] + A[1] * vv[1];
+  rr[1]  = A[2] * vv[0] + A[3] * vv[1];
+  return result;
+}
+//Paritally specialized versions for performance of common cases
+Vector< double, 3 >
+Matrix< double, 3, 3 >
+::operator*(const Vector< double, 3 > & vect) const
+{
+  Vector< double, 3 > result;
+  double * const rr=&result[0];
+  double const * const vv=&vect[0];
+  double const * const A=m_Matrix[0]; //Get a pointer to the first element of array
+  rr[0]  = A[0] * vv[0] + A[1] * vv[1] + A[2] * vv[2];
+  rr[1]  = A[3] * vv[0] + A[4] * vv[1] + A[5] * vv[2];
+  rr[2]  = A[6] * vv[0] + A[7] * vv[1] + A[8] * vv[2];
+  return result;
+}
+
 template< class T, unsigned int NRows, unsigned int NColumns >
 Vector< T, NRows >
 Matrix< T, NRows, NColumns >
