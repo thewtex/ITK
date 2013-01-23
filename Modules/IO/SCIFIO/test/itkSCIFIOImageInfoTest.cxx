98364fc2b96248c2b1bd6f420186efeebe3bd72d
Mark Hiner <hiner@wisc.edu>
2013-01-23 18:56:54 -0500

    WIP: Add SCIFIOImageIO.
    This package provides an ITK ImageIO plugin that uses SCIFIO
    to read and write supported life sciences file formats.
    
    The SCIFIO ImageIO plugin was developed by Gaetan Lehmann, Mark Hiner,
    Curtis Rueden, Melissa Linkert and Matt McCormick. Development of this
    module was funded in part by the [FARSIGHT
    project](http://farsight-toolkit.org/), as well as the [Open Microscopy
    Environment](http://openmicroscopy.org/).
    
    Special thanks to Alex Gouaillard, Sebastien Barre, Luis Ibanez and
    Jim Miller for fixes and suggestions.
    
    For further details, including usage instructions, see the [Bio-Formats
    ITK page](http://openmicroscopy.org/site/support/bio-formats/users/itk).
    
    Please [direct any questions to the OME
    team](http://www.openmicroscopy.org/site/community).
    
    Change-Id: Idafa221438f1faec75f48e7ec04fc7b435e80f24

---
+++ Modules/IO/SCIFIO/test/itkSCIFIOImageInfoTest.cxx
@@ -0,0 +1,108 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImage.h"
+#include "itkMetaDataObject.h"
+#include "itkMetaDataDictionary.h"
+#include "itkImageIOBase.h"
+//#include "itkSCIFIOImageIO.h"
+
+#define METADATA_NOT_FOUND "No value for this key."
+
+int itkSCIFIOImageInfoTest( int argc, char * argv[] )
+{
+  std::cout << "--------- asdfasdfasdf ----------" << std::endl;
+  if( argc < 4)
+  {
+    std::cerr << "Usage: " << argv[0] << " sizeZ sizeC sizeT\n";
+    return EXIT_FAILURE;
+  }
+
+  char* sZ = argv[1];
+  char* sC = argv[2];
+  char* sT = argv[3];
+  std::string inputFile = "scifioImageInfo"
+                          "&sizeZ=" + std::string(sZ) +
+                          "&sizeC=" + std::string(sC) +
+                          "&sizeT=" + std::string(sT) +
+                          ".fake";
+  int sizeZ = atoi(sZ);
+  int sizeC = atoi(sC);
+  int sizeT = atoi(sT);
+  std::cout << "--------- itkSCIFIOImageInfoTest ----------" << std::endl;
+
+  typedef unsigned char     PixelType;
+  const unsigned int        Dimension = 3;
+
+  typedef itk::Image< PixelType, Dimension >  ImageType;
+
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  ImageType::Pointer img;
+
+  reader->SetFileName(inputFile);
+  reader->Update();
+  img = reader->GetOutput();
+
+  std::string metaString (METADATA_NOT_FOUND);
+  itk::MetaDataDictionary imgMetaDictionary = img->GetMetaDataDictionary();
+  std::vector<std::string> imgMetaKeys = imgMetaDictionary.GetKeys();
+  std::vector<std::string>::const_iterator itKey = imgMetaKeys.begin();
+
+  // Iterate through the keys and print their paired values
+  std::cout << "Metadata Key ---> Value pairs, from dictionary:" << std::endl;
+  for(; itKey != imgMetaKeys.end(); ++itKey)
+  {
+    std::string tmp;
+    itk::ExposeMetaData<std::string>( imgMetaDictionary, *itKey, tmp );
+    std::cout << *itKey << " ---> " << tmp << std::endl;
+    //std::cout << "Metadata: " << notes1 << std::endl;
+    metaString = METADATA_NOT_FOUND;
+  }
+
+  // Print out the metadata naturally contained within itkImageIOBase
+  itk::ImageIORegion region = reader->GetImageIO()->GetIORegion();
+  int regionDim = region.GetImageDimension();
+
+  std::cout << "Metadata Key ---> Value pairs, from ImageIOBase:" << std::endl;
+
+  for(int i = 0; i < regionDim; i++)
+  {
+    std::cout << "Dimension " << i + 1 << " Size: " << region.GetSize(i) << std::endl;
+  }
+  for(int i = 0; i < regionDim; i++)
+  {
+    std::cout << "Spacing " << i + 1 << ": " << reader->GetImageIO()->GetSpacing(i) << std::endl;
+  }
+  std::cout << "Byte Order: " << reader->GetImageIO()->GetByteOrderAsString(reader->GetImageIO()->GetByteOrder()) << std::endl;
+  std::cout << "Pixel Stride: " << reader->GetImageIO()->GetPixelStride() << std::endl;
+  std::cout << "Pixel Type: " << reader->GetImageIO()->GetPixelTypeAsString(reader->GetImageIO()->GetPixelType()) << std::endl;
+  std::cout << "Image Size (in pixels): " << reader->GetImageIO()->GetImageSizeInPixels() << std::endl;
+  std::cout << "Pixel Type: " << reader->GetImageIO()->GetComponentTypeAsString(reader->GetImageIO()->GetComponentType()) << std::endl;
+  std::cout << "RGB Channel Count: " << reader->GetImageIO()->GetNumberOfComponents() << std::endl;
+  std::cout << "Number of Dimensions: " << reader->GetImageIO()->GetNumberOfDimensions() << std::endl;
+
+  // TODO: Compare region attributes with input parameters.
+
+  return EXIT_SUCCESS;
+}
