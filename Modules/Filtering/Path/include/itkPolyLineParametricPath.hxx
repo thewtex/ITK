f91b5af78652ab6ff9d1069d26c37f9e94d416bc
Ho Cheung <hocheung20@gmail.com>
2013-02-07 10:52:48 -0600

    BUG: itkPolyLineParametricPath infinite loop for degenerate paths
    itkPolyLineParametricPath inherits EvalateDerivative and IncrementInput from its
    superclass: itkParametricPath. IncrementInput uses a default
    timestep and an derivative estimated from that default timestep to implement a
    tooBig/tooSmall search strategy to find the next pixel along the path.
    
    This strategy will result in oscillation ebtween tooBig and tooSmall for
    degenerate itkPolyLineParametricPath paths, such as those with a lot of
    verticies at the beginning less than one image index away from each other
    followed by a single vertex at the end that is multiple image indices away.
    
    This patch overrides the inherited IncrementInputs and EvaluateDerivative
    function and takes advantage of the fact that it is possible to calculate an
    instantaneous derivative which allows to calculate the exact timestep to effect
    a change in one pixel.
    
    Change-Id: Ibe2d5b8fb913e6311ee0f3b977fdadf1321f0653

--- Modules/Filtering/Path/include/itkPolyLineParametricPath.hxx
+++ Modules/Filtering/Path/include/itkPolyLineParametricPath.hxx
@@ -53,12 +53,108 @@ PolyLineParametricPath< VDimension >
   return output;
 }
 
-//template<unsigned int VDimension>
-//typename PolyLineParametricPath<VDimension>::VectorType
-//PolyLineParametricPath<VDimension>
-//::EvaluateDerivative(const InputType & input) const
-//{
-//}
+template<unsigned int VDimension>
+typename PolyLineParametricPath<VDimension>::VectorType
+PolyLineParametricPath<VDimension>
+::EvaluateDerivative(const InputType & input) const
+{
+  //Get next integral time-point
+  const InputType next_timepoint = std::min(std::floor(input + 1.0), this->EndOfInput());
+
+  //Get previous integral time-point
+  const InputType prev_timepoint = next_timepoint - 1.0;
+
+  //Calculate the continuous index for both points
+  const ContinuousIndexType next_index = this->Evaluate(next_timepoint);
+  const ContinuousIndexType prev_index = this->Evaluate(prev_timepoint);
+
+  //For some reason, there's no way to convert ContinuousIndexType to VectorType
+  VectorType partial_derivatives;
+  for (int i = 0; i < VDimension; ++i)
+    {
+    partial_derivatives[i] = next_index[i] - prev_index[i];
+    }
+
+  return partial_derivatives;
+}
+
+template<unsigned int VDimension>
+typename PolyLineParametricPath<VDimension>::OffsetType
+PolyLineParametricPath<VDimension>
+::IncrementInput(InputType & input) const
+{
+  //Save this input index since we will use it to calculate the offset
+  const OutputType original_index = this->EvaluateToIndex(input);
+
+  InputType potential_timestep = 0.0;
+  bool timeStepSmallEnough = false;
+  while (!timeStepSmallEnough)
+    {
+    if (input == this->EndOfInput())
+      return this->GetZeroOffset();
+
+    //Check to make sure we aren't already at a place with an offset of 1 pixel
+    const OutputType potential_index = this->EvaluateToIndex(input);
+    //For some reason, there's no way to convert OutputType to OffsetType
+    OffsetType offset;
+    for (int i = 0; i < VDimension; ++i)
+      {
+      offset[i] = potential_index[i] - original_index[i];
+      }
+
+    if (offset != this->GetZeroOffset())
+      {
+        return offset;
+      }
+    //Get the derivative at the current input
+    VectorType partial_derivatives = this->EvaluateDerivative(input);
+
+    //std::cout << "Partial derivatives: " << partial_derivatives << std::endl;
+
+    //Find the largest of all the partial derivatives
+    unsigned int max_partial_deriv_index = 0;
+    for (unsigned int i = 1; i < VDimension; ++i)
+      {
+      if (std::abs(partial_derivatives[i]) > std::abs(partial_derivatives[max_partial_deriv_index]))
+        {
+        max_partial_deriv_index = i;
+        }
+      }
+
+    //Calculate the timestep required to effect a 1 pixel change
+    potential_timestep = 1.0/std::abs(partial_derivatives[max_partial_deriv_index]);
+
+    //Check to make sure the timestep doesn't put the input past the next integral timestep
+    //(since the derivatives can change)
+    if (input + potential_timestep > std::floor(input + 1.0))
+      {
+      input = std::floor(input + 1.0); //Set the input to the next integral time-step
+      }
+    else
+      {
+        timeStepSmallEnough = true;
+      }
+    }
+
+  //Finalize the potential timestep into THE timestep
+  const InputType timestep = potential_timestep;
+
+  //Get the index at the next timestep so we can calculate the offset
+  const OutputType next_index = this->EvaluateToIndex(input + timestep);
+
+  //For some reason, there's no way to convert OutputType to OffsetType
+  OffsetType offset;
+  for (int i = 0; i < VDimension; ++i)
+    {
+    offset[i] = next_index[i] -  original_index[i];
+    }
+
+  //Update input timestep
+  input += timestep;
+
+  //Return the offset
+  return offset;
+}
 
 template< unsigned int VDimension >
 PolyLineParametricPath< VDimension >
