a73a69a32cf5f543540809a466223f9456a9af06
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-02-19 13:48:17 -0500

    ENH: Creating templateless region splitter class
    The end goal of this topic is to consolidate code enabling re-use,
    and modularization to facilitate customization and experimentation
    with different splitting algorithms for multi-threading.
    
    Creating a new ImageRegionSplitter class which does not have a class
    template. This enables the same splitter to be used for the
    multi-threading, the streaming image filter, and the IO
    classes. Additionally, it allows for a global default splitter to use
    used and shared.
    
    There are some novel to ITK design patterns used here which should be
    considered.
    
    1) Using a templatedless class with templated function which
    dispatches to a function which takes the dimension and arrays. This
    allow for compiled functions to be reused.
    2) Multiple inheritance is used, to add common static members which
    access common data between all implemented instances of the class. That
    is it can provide common data and methods for all instantiations
    across template instantiations.
    
    Add GetModified IVAR accessors to ImageRegion class.
    
    Change-Id: I7e67cf27ae80803499d75b8af589bee9762b8ec0
    
    Change-Id: I84f88ed9c40415f4ed50282a857e0dd0913c1797
    
     LocalWords:  instantiations

---
+++ Modules/Core/Common/include/itkImageRegionSplitterBase.h
@@ -0,0 +1,140 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkImageRegionSplitterBase_h
+#define __itkImageRegionSplitterBase_h
+
+#include "itkImageRegion.h"
+#include "itkObjectFactory.h"
+
+namespace itk
+{
+
+/** \class ImageRegionSplitterBase
+ * \brief Divide an image region into several pieces.
+ *
+ * ImageRegionSplitterBase divides an ImageRegion into smaller regions.
+ * ImageRegionSplitterBase is used by the ImageSource,
+ * StreamingImageFilter, streaming ImageIO classes to divide a region
+ * into a series of smaller subregions.
+ *
+ * This object has two basic methods: GetNumberOfSplits() and
+ * GetSplit().
+ *
+ * GetNumberOfSplits() is used to determine how may subregions a given
+ * region can be divided.  You call GetNumberOfSplits with an argument
+ * that is the number of subregions you want.  If the image region can
+ * support that number of subregions, that number is returned.
+ * Otherwise, the maximum number of splits less then or equal to the
+ * argumen  be returned.  For example, if a region splitter class only divides
+ * a region into horizontal slabs, then the maximum number of splits
+ * will be the number of rows in the region.
+ *
+ * GetSplit() returns the ith of N subregions (as an ImageRegion object).
+ *
+ * This ImageRegionSplitterBase class divides a region along the
+ * outermost dimension. If the outermost dimension has size 1 (i.e. a
+ * volume with a single slice), the ImageRegionSplitter will divide
+ * the region along the next outermost dimension. If that dimension
+ * has size 1, the process continues with the next outermost
+ * dimension.
+ *
+ * \sa ImageRegionSplitterDirection
+ *
+ * \ingroup ITKSystemObjects
+ * \ingroup DataProcessing
+ * \ingroup ITKCommon
+ */
+
+class ITKCommon_EXPORT ImageRegionSplitterBase
+  :public Object
+{
+public:
+  /** Standard class typedefs. */
+  typedef ImageRegionSplitterBase    Self;
+  typedef Object                     Superclass;
+  typedef SmartPointer< Self >       Pointer;
+  typedef SmartPointer< const Self > ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ImageRegionSplitterBase, Object);
+
+  /** How many pieces can the specified region be split? A given region
+   * cannot always be divided into the requested number of pieces. For
+   * instance, if the numberOfPieces exceeds the number of pixels along
+   * a certain dimensions, then some splits will not be possible. This
+   * method returns a number less than or equal to the requested number
+   * of pieces. */
+  template <unsigned int VImageDimension>
+    unsigned int GetNumberOfSplits(const ImageRegion<VImageDimension> & region,
+                                   unsigned int requestedNumber) const
+  {
+    typedef ImageRegion<VImageDimension> ImageRegionType;
+
+    return this->GetNumberOfSplits( VImageDimension,
+                                    region.GetIndex().m_Index,
+                                    region.GetSize().m_Size,
+                                    requestedNumber);
+  }
+
+  /** Get a region definition that represents the ith piece a specified region.
+   * The "numberOfPieces" must be equal to what
+   * GetNumberOfSplits() returns. */
+  // The return value is the maximum number of splits, available, if
+  // "i" is greater than or equal to the return value the value of the
+  // region is undefined.
+  template <unsigned int VImageDimension>
+    unsigned int GetSplit( unsigned int i,
+                           unsigned int numberOfPieces,
+                           ImageRegion<VImageDimension> & region ) const
+  {
+    typedef ImageRegion<VImageDimension> ImageRegionType;
+
+    return this->GetSplit( VImageDimension,
+                           i,
+                           numberOfPieces,
+                           region.GetModifiableIndex().m_Index,
+                           region.GetModifiableSize().m_Size );
+  }
+
+protected:
+  ImageRegionSplitterBase();
+
+
+  virtual unsigned int GetNumberOfSplits( unsigned int dim,
+                                          const IndexValueType regionIndex[],
+                                          const SizeValueType regionSize[],
+                                          unsigned int requestedNumber ) const;
+
+  virtual unsigned int GetSplit( unsigned int dim,
+                                 unsigned int i,
+                                 unsigned int numberOfPieces,
+                                 IndexValueType regionIndex[],
+                                 SizeValueType regionSize[] ) const;
+
+  void PrintSelf(std::ostream & os, Indent indent) const;
+
+private:
+  ImageRegionSplitterBase(const ImageRegionSplitterBase &); //purposely not implemented
+  void operator=(const ImageRegionSplitterBase &);      //purposely not implemented
+};
+} // end namespace itk
+
+#endif
