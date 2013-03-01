0691922e7a9cf3979e3585a7be1aa8413d5f0588
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-03-01 09:52:48 -0500

    ENH: ImageIO's now use ImageRegionSplitter base
    Moved ImageIORegion to Common module. Added GetModifiable methods.
    
    Added methods to ImageRegionSplitterBase for ImageIORegion objects.
    
    Refactored ImageIOBase to use the default ImageRegionSplitterBase
    method for streaming IO.
    
    Change-Id: I0c07db23824d65086a64b76dbecbe01137fa78a2

--- Modules/Core/Common/include/itkImageIORegion.h
+++ Modules/Core/Common/include/itkImageIORegion.h
@@ -46,7 +46,7 @@ namespace itk
  * \sa Index
  * \sa Size
  * \sa MeshRegion
- * \ingroup ITKIOImageBase
+ * \ingroup ITKCommon
  */
 
 class ITK_EXPORT ImageIORegion:public Region
@@ -110,6 +110,7 @@ public:
 
   /** Get index defining the corner of the region. */
   const IndexType & GetIndex() const;
+  IndexType & GetModifiableIndex();
 
   /** Set the size of the region. This plus the index determines the
    * rectangular shape, or extent, of the region. */
@@ -117,6 +118,7 @@ public:
 
   /** Get the size of the region. */
   const SizeType & GetSize() const;
+  SizeType & GetModifiableSize();
 
   /** Convenience methods to get the size of the image in a particular
    * coordinate direction i. Do not try to access image sizes beyond the
@@ -165,7 +167,7 @@ extern ITK_EXPORT std::ostream & operator<<(std::ostream & os, const ImageIORegi
  * \brief Helper class for converting ImageRegions into ImageIORegions and back.
  *
  *
- * \ingroup ITKIOImageBase
+ * \ingroup ITKCommon
  */
 template< unsigned int VDimension >
 class ImageIORegionAdaptor
