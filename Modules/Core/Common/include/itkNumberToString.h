ebef723063620abcd2697da8ad4f0c8a90664cce
Bill Lorensen <bill.lorensen@gmail.com>
2013-03-16 12:34:55 -0700

    WIP: Refactor DoubleToString into NumberToString
    NumberToString is now templated on value type. It resides in
    Core/Common rather than in the ThirdParty module. This removes ITK
    dependencies on ThirdParty.
    
    Change-Id: I38780ea1cca70965733e47bff6ac352a69522c97

---
+++ Modules/Core/Common/include/itkNumberToString.h
@@ -0,0 +1,51 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkNumberToString_h
+#define __itkNumberToString_h
+
+#include "double-conversion.h"
+#include "itkMacro.h"
+#include <string>
+
+namespace itk
+{
+template< typename TValueType>
+class NumberToString
+{
+public:
+  NumberToString() :
+    m_DoubleToStringConverter(double_conversion::DoubleToStringConverter::EcmaScriptConverter())
+    {
+    }
+  std::string operator() (TValueType val);
+
+private:
+  NumberToString & operator=(const NumberToString &); // not defined
+  const double_conversion::DoubleToStringConverter &m_DoubleToStringConverter;
+};
+
+// declaration of specialization
+template<> ITKCommon_EXPORT std::string NumberToString<double>::operator()(double val);
+template<> ITKCommon_EXPORT std::string NumberToString<float>::operator()(float val);
+
+} // namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkNumberToString.hxx"
+#endif
+#endif
