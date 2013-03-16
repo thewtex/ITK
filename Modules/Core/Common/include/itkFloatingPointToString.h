8fb1ba0febce548c288912ab4379ed7bf524e22f
Bill Lorensen <bill.lorensen@gmail.com>
2013-03-16 12:34:55 -0700

    WIP: Refactor DoubleToString into FloatingPointToString
    FloatingPointToString is now templated on value type: float or
    double. It resides in Core/Common rather than in the ThirdParty
    module. This removes ITK dependencies on ThirdParty.
    
    Change-Id: I38780ea1cca70965733e47bff6ac352a69522c97

---
+++ Modules/Core/Common/include/itkFloatingPointToString.h
@@ -0,0 +1,61 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkFloatingPointToString_h
+#define __itkFloatingPointToString_h
+
+#include "double-conversion.h"
+#include "itkMacro.h"
+
+namespace itk
+{
+template< typename TValueType>
+class FloatingPointToString
+{
+public:
+  FloatingPointToString() :
+    m_DoubleToStringConverter(double_conversion::DoubleToStringConverter::EcmaScriptConverter())
+    {
+    }
+  std::string operator()(TValueType val)
+    {
+    char buf[256];
+    double_conversion::StringBuilder builder(buf,sizeof(buf));
+    builder.Reset();
+    if (typeid(TValueType) == typeid(float))
+      {
+      if(!m_DoubleToStringConverter.ToShortestSingle(val,&builder))
+        {
+        itkGenericExceptionMacro(<< "Conversion failed for " << val);
+        }
+      }
+    else
+      {
+      if(!m_DoubleToStringConverter.ToShortest(val,&builder))
+        {
+        itkGenericExceptionMacro(<< "Conversion failed for " << val);
+        }
+      }
+    return std::string(builder.Finalize());
+    }
+private:
+  FloatingPointToString & operator=(const FloatingPointToString &); // not defined
+  const double_conversion::DoubleToStringConverter &m_DoubleToStringConverter;
+};
+
+} // namespace itk
+#endif
