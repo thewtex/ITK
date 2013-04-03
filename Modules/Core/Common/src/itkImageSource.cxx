61412f7e1af5fd4884f0fecc9910468656eef57d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-04-03 11:49:37 -0400

    BUG: Do not create static initialized ITK objects
    Allocating an ITK object with a factory new method during static
    initialization, caused the ITK factories to be registered. Which
    triggers other libraries to be loaded. This can not occur during
    static initialization.
    
    Thread safe lazy initialization is required for these cases.
    
    Change-Id: I49155b9e2083d4c5e8daaac44a5780d849345d9b

--- Modules/Core/Common/src/itkImageSource.cxx
+++ Modules/Core/Common/src/itkImageSource.cxx
@@ -18,17 +18,29 @@
 
 #include "itkImageRegionSplitterSlowDimension.h"
 #include "itkImageSource.h"
+#include "itkSimpleFastMutexLock.h"
+#include "itkMutexLockHolder.h"
 
 namespace itk
 {
 
 namespace
 {
-ImageRegionSplitterBase::Pointer globalDefaultSplitter = ImageRegionSplitterSlowDimension::New().GetPointer();
+SimpleFastMutexLock globalDefaultSplitterLock;
+ImageRegionSplitterBase::Pointer globalDefaultSplitter;
 }
 
 const ImageRegionSplitterBase*  ImageSourceCommon::GetGlobalDefaultSplitter(void)
 {
+  if ( globalDefaultSplitter.IsNull() )
+    {
+    // thread safe lazy initialization
+    MutexLockHolder< SimpleFastMutexLock > lock(globalDefaultSplitterLock);
+    if ( globalDefaultSplitter.IsNull() )
+      {
+      globalDefaultSplitter = ImageRegionSplitterSlowDimension::New().GetPointer();
+      }
+    }
   return globalDefaultSplitter;
 }
 
