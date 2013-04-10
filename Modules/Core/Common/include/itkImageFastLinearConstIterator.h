cef767e99e801ec2a11c0fb1f2d8be12a1e91745
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-04-10 16:17:26 -0400

    WIP: PERF: adding new fast linear iterator
    Adding a new iterator without a conditional in the operator++, to
    create an inner loop without branching to enable compiler
    vectorization.
    
    Using this new iterator with an inner loop for an individual line and
    an outer loop to advance through the lines, in the Functor based
    filters.
    
    This has resulted in a 2x-3x speed up for single threaded execution of
    128^3 images in filters such as CastImageFilter and
    AddImageFilter. Close to 100 functor based image filters exist in ITK
    all should benefit from this change.
    
    Since the ImageRegionIterator was removed from the UnaryImageFilter,
    a missing header needed to be added in a couple places.
    
    TODO: much work still needs to be done on the iterator...
    
    Change-Id: I83f7f0753a271389d8bfae2eda8d6ad1848510c4

---
+++ Modules/Core/Common/include/itkImageFastLinearConstIterator.h
@@ -0,0 +1,254 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkImageFastLinearConstIterator_h
+#define __itkImageFastLinearConstIterator_h
+
+#include "itkImageIterator.h"
+
+namespace itk
+{
+/** \class ImageFastLinearConstIterator
+ * \brief A multi-dimensional iterator templated over image type that walks a
+ * region of pixels.
+ *
+ * The itk::ImageFastLinearConstIterator is optimized...
+ * \code
+ *
+ *      it = ImageFastLinearConstIterator()
+ *      while( !it.IsAtEnd() )
+ *        {
+ *        while( !it.IsAtEndOfLine() )
+ *          {
+ *          *it += 100.0;
+ *          ++it;
+ *          }
+ *        it.NextLine();
+ *        }
+ *
+ * \endcode
+ *
+ * \ingroup ITKCommon
+ *
+ */
+template< typename TImage >
+class ITK_EXPORT ImageFastLinearConstIterator:public ImageConstIterator< TImage >
+{
+public:
+  /** Standard class typedef. */
+  typedef ImageFastLinearConstIterator Self;
+  typedef ImageConstIterator< TImage > Superclass;
+
+  /** Dimension of the image that the iterator walks.  This constant is needed so
+   * functions that are templated over image iterator type (as opposed to
+   * being templated over pixel type and dimension) can have compile time
+   * access to the dimension of the image that the iterator walks. */
+  itkStaticConstMacro(ImageIteratorDimension, unsigned int,
+                      Superclass::ImageIteratorDimension);
+
+  /**
+   * Index typedef support. While these were already typdef'ed in the superclass,
+   * they need to be redone here for this subclass to compile properly with gcc.
+   */
+  /** Types inherited from the Superclass */
+  typedef typename Superclass::IndexType             IndexType;
+  typedef typename Superclass::SizeType              SizeType;
+  typedef typename Superclass::OffsetType            OffsetType;
+  typedef typename Superclass::RegionType            RegionType;
+  typedef typename Superclass::ImageType             ImageType;
+  typedef typename Superclass::PixelContainer        PixelContainer;
+  typedef typename Superclass::PixelContainerPointer PixelContainerPointer;
+  typedef typename Superclass::InternalPixelType     InternalPixelType;
+  typedef typename Superclass::PixelType             PixelType;
+  typedef typename Superclass::AccessorType          AccessorType;
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ImageFastLinearConstIterator, ImageConstIterator);
+
+  /** Default constructor. Needed since we provide a cast constructor. */
+  ImageFastLinearConstIterator():ImageConstIterator< TImage >()
+  {
+    m_SpanBeginOffset = 0;
+    m_SpanEndOffset = 0;
+  }
+
+  /** Constructor establishes an iterator to walk a particular image and a
+   * particular region of that image. */
+  ImageFastLinearConstIterator(const ImageType *ptr,
+                           const RegionType & region):
+    ImageConstIterator< TImage >(ptr, region)
+  {
+    m_SpanBeginOffset = this->m_BeginOffset;
+    m_SpanEndOffset   = this->m_BeginOffset + static_cast< OffsetValueType >( this->m_Region.GetSize()[0] );
+  }
+
+  /** Constructor that can be used to cast from an ImageIterator to an
+   * ImageFastLinearConstIterator. Many routines return an ImageIterator, but for a
+   * particular task, you may want an ImageFastLinearConstIterator.  Rather than
+   * provide overloaded APIs that return different types of Iterators, itk
+   * returns ImageIterators and uses constructors to cast from an
+   * ImageIterator to a ImageFastLinearConstIterator. */
+  ImageFastLinearConstIterator(const ImageIterator< TImage > & it)
+  {
+    this->ImageConstIterator< TImage >::operator=(it);
+
+    IndexType ind = this->GetIndex();
+    m_SpanEndOffset = this->m_Offset + static_cast< OffsetValueType >( this->m_Region.GetSize()[0] )
+                      - ( ind[0] - this->m_Region.GetIndex()[0] );
+    m_SpanBeginOffset = m_SpanEndOffset
+                        - static_cast< OffsetValueType >( this->m_Region.GetSize()[0] );
+  }
+
+  /** Constructor that can be used to cast from an ImageConstIterator to an
+   * ImageFastLinearConstIterator. Many routines return an ImageIterator, but for a
+   * particular task, you may want an ImageFastLinearConstIterator.  Rather than
+   * provide overloaded APIs that return different types of Iterators, itk
+   * returns ImageIterators and uses constructors to cast from an
+   * ImageIterator to a ImageFastLinearConstIterator. */
+  ImageFastLinearConstIterator(const ImageConstIterator< TImage > & it)
+  {
+    this->ImageConstIterator< TImage >::operator=(it);
+
+    IndexType ind = this->GetIndex();
+    m_SpanEndOffset = this->m_Offset + static_cast< OffsetValueType >( this->m_Region.GetSize()[0] )
+                      - ( ind[0] - this->m_Region.GetIndex()[0] );
+    m_SpanBeginOffset = m_SpanEndOffset
+                        - static_cast< OffsetValueType >( this->m_Region.GetSize()[0] );
+  }
+
+  /** Move an iterator to the beginning of the region. "Begin" is
+   * defined as the first pixel in the region. */
+  void GoToBegin()
+  {
+    Superclass::GoToBegin();
+
+    // reset the span offsets
+    m_SpanBeginOffset = this->m_BeginOffset;
+    m_SpanEndOffset   = this->m_BeginOffset + static_cast< OffsetValueType >( this->m_Region.GetSize()[0] );
+  }
+
+  /** Move an iterator to the end of the region. "End" is defined as
+   * one pixel past the last pixel of the region. */
+  void GoToEnd()
+  {
+    Superclass::GoToEnd();
+
+    // reset the span offsets
+    m_SpanEndOffset = this->m_EndOffset;
+    m_SpanBeginOffset = m_SpanEndOffset - static_cast< OffsetValueType >( this->m_Region.GetSize()[0] );
+  }
+
+  /** Set the index. No bounds checking is performed. This is overridden
+   * from the parent because we have an extra ivar.
+   * \sa GetIndex */
+  void SetIndex(const IndexType & ind)
+  {
+    Superclass::SetIndex(ind);
+    m_SpanEndOffset = this->m_Offset + static_cast< OffsetValueType >( this->m_Region.GetSize()[0] )
+                      - ( ind[0] - this->m_Region.GetIndex()[0] );
+    m_SpanBeginOffset = m_SpanEndOffset - static_cast< OffsetValueType >( this->m_Region.GetSize()[0] );
+  }
+
+  /** Go to the next line.
+   * \sa operator++  \sa operator-- \sa IsAtEndOfLine \sa PreviousLine \sa End */
+  inline void NextLine(void)
+  {
+    this->Increment();
+  };
+
+  /** Go to the previous line.
+   * \sa operator++ \sa operator-- \sa IsAtEndOfLine \sa NextLine \sa End */
+  inline void PreviousLine(void)
+  {
+    this->Decrement();
+  }
+
+  /** Go to the beginning pixel of the current line.
+   * \sa GoToReverseBeginOfLine \sa operator++ \sa operator-- \sa NextLine \sa IsAtEndOfLine */
+  void GoToBeginOfLine(void)
+  {
+    this->Offset = m_SpanBeginOffset;
+  }
+
+  /** Go to the beginning pixel of the current line.
+   * \sa GoToBeginOfLine \sa operator++ \sa operator-- \sa NextLine \sa IsAtEndOfLine */
+  void GoToReverseBeginOfLine(void)
+  {
+    this->Offset = m_SpanEndOffset;
+    --this->Offset;
+  }
+
+  /** Go to the past end pixel of the current line.
+   * \sa GoToBeginOfLine \sa operator++ \sa operator-- \sa NextLine \sa IsAtEndOfLine */
+  void GoToEndOfLine(void);
+
+  /** Test if the index is at the begin of line */
+  inline bool IsAtReverseEndOfLine(void)
+  {
+    return this->m_Offset < m_SpanBeginOffset;
+  }
+
+  /** Test if the index is at the end of line */
+  inline bool IsAtEndOfLine(void)
+  {
+    return this->m_Offset >= m_SpanEndOffset;
+  }
+
+  /** Increment (prefix) the fastest moving dimension of the iterator's index.
+   * This operator will constrain the iterator within the region (i.e. the
+   * iterator will automatically wrap from the end of the row of the region
+   * to the beginning of the next row of the region) up until the iterator
+   * tries to moves past the last pixel of the region.  Here, the iterator
+   * will be set to be one pixel past the end of the region.
+   * \sa operator++(int) */
+  Self &
+  operator++()
+  {
+    ++this->m_Offset;
+    return *this;
+  }
+
+  /** Decrement (prefix) the fastest moving dimension of the iterator's index.
+   * This operator will constrain the iterator within the region (i.e. the
+   * iterator will automatically wrap from the beginning of the row of the region
+   * to the end of the next row of the region) up until the iterator
+   * tries to moves past the first pixel of the region.  Here, the iterator
+   * will be set to be one pixel past the beginning of the region.
+   * \sa operator--(int) */
+  Self & operator--()
+  {
+    --this->m_Offset;
+    return *this;
+  }
+
+protected:
+  OffsetValueType m_SpanBeginOffset; // one pixel before the beginning of the span
+                                   // (row)
+  OffsetValueType m_SpanEndOffset;   // one pixel past the end of the span (row)
+
+private:
+  void Increment(); // advance in a direction other than the fastest moving
+
+  void Decrement(); // go back in a direction other than the fastest moving
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageFastLinearConstIterator.hxx"
+#endif
+
+#endif
---
+++ Modules/Core/Common/include/itkImageFastLinearIterator.h
@@ -0,0 +1,97 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkImageFastLinearIterator_h
+#define __itkImageFastLinearIterator_h
+
+#include "itkImageFastLinearConstIterator.h"
+
+namespace itk
+{
+/** \class ImageFastLinearIterator
+ * \brief A multi-dimensional iterator templated over image type that walks a
+ * region of pixels.
+ *
+ * The itk::ImageFastLinearIterator is optimized for iteration speed and is the
+ * first choice for iterative, pixel-wise operations on an image.
+ *
+ * \ingroup ImageIterators
+ *
+ * \ingroup ITKCommon
+ *
+ */
+template< typename TImage >
+class ITK_EXPORT ImageFastLinearIterator:public ImageFastLinearConstIterator< TImage >
+{
+public:
+  /** Standard class typedefs. */
+  typedef ImageFastLinearIterator                Self;
+  typedef ImageFastLinearConstIterator< TImage > Superclass;
+
+  /** Types inherited from the Superclass */
+  typedef typename Superclass::IndexType             IndexType;
+  typedef typename Superclass::SizeType              SizeType;
+  typedef typename Superclass::OffsetType            OffsetType;
+  typedef typename Superclass::RegionType            RegionType;
+  typedef typename Superclass::ImageType             ImageType;
+  typedef typename Superclass::PixelContainer        PixelContainer;
+  typedef typename Superclass::PixelContainerPointer PixelContainerPointer;
+  typedef typename Superclass::InternalPixelType     InternalPixelType;
+  typedef typename Superclass::PixelType             PixelType;
+  typedef typename Superclass::AccessorType          AccessorType;
+
+  /** Default constructor. Needed since we provide a cast constructor. */
+  ImageFastLinearIterator();
+
+  /** Constructor establishes an iterator to walk a particular image and a
+   * particular region of that image. */
+  ImageFastLinearIterator(ImageType *ptr, const RegionType & region);
+
+  /** Constructor that can be used to cast from an ImageIterator to an
+   * ImageFastLinearIterator. Many routines return an ImageIterator but for a
+   * particular task, you may want an ImageFastLinearIterator.  Rather than
+   * provide overloaded APIs that return different types of Iterators, itk
+   * returns ImageIterators and uses constructors to cast from an
+   * ImageIterator to a ImageFastLinearIterator. */
+  ImageFastLinearIterator(const ImageIterator< TImage > & it);
+
+  /** Set the pixel value */
+  void Set(const PixelType & value) const
+  {
+    this->m_PixelAccessorFunctor.Set(*( const_cast< InternalPixelType * >(
+                                          this->m_Buffer + this->m_Offset ) ), value);
+  }
+
+  /** Return a reference to the pixel
+   * This method will provide the fastest access to pixel
+   * data, but it will NOT support ImageAdaptors. */
+  PixelType & Value(void)
+  { return *( const_cast< InternalPixelType * >( this->m_Buffer + this->m_Offset ) ); }
+
+protected:
+  /** the construction from a const iterator is declared protected
+      in order to enforce const correctness. */
+  ImageFastLinearIterator(const ImageFastLinearConstIterator< TImage > & it);
+  Self & operator=(const ImageFastLinearConstIterator< TImage > & it);
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageFastLinearIterator.hxx"
+#endif
+
+#endif
