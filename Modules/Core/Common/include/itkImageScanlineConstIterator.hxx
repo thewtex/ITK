90e8b52f7a5b2776b221b6d9ef4cec1a271c5d5a
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-04-10 16:17:26 -0400

    PERF: Adding performance oriented ScanlineIterator
    Adding a new iterator without a conditional in the operator++, to
    create an inner loop without branching to improve performance.
    
    Using the new iterator with an inner loop for an individual line and
    an outer loop to advance through the lines, in the Functor based
    filters.
    
    This has resulted in a 2x-3x speed up for single threaded execution of
    128^3 images in filters such as CastImageFilter and
    AddImageFilter. Close to 100 functor based image filters exist in ITK
    all should benefit from this change.
    
    Since the ImageRegionIterator was removed from the UnaryImageFilter,
    a missing header needed to be added in a couple places.
    
    Change-Id: I83f7f0753a271389d8bfae2eda8d6ad1848510c4

---
+++ Modules/Core/Common/include/itkImageScanlineConstIterator.hxx
@@ -0,0 +1,66 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkImageScanlineConstIterator_hxx
+#define __itkImageScanlineConstIterator_hxx
+
+#include "itkImageScanlineConstIterator.h"
+
+namespace itk
+{
+
+template< class TImage >
+void
+ImageScanlineConstIterator< TImage >
+::Increment()
+{
+  // increment to the next scanline
+
+  // Get the index of the last pixel on the span (row)
+  IndexType ind = this->m_Image->ComputeIndex( static_cast< OffsetValueType >( m_SpanEndOffset -1 ) );
+
+  const IndexType &startIndex = this->m_Region.GetIndex();
+  const SizeType &size = this->m_Region.GetSize();
+
+  // Check to see if this is the last row
+  if (ind == this->m_Region.GetUpperIndex())
+    {
+    return;
+    }
+
+  // Note that ++ind[0] moves to the next pixel past current scanline
+  ++ind[0];
+
+  // if the iterator is outside the region (but not past region end) then
+  // we need to wrap around the region
+  for( unsigned int d = 0; d+1 < ImageIteratorDimension; ++d)
+    {
+    if ( static_cast< SizeValueType >( ind[d] - startIndex[d] ) >= size[d] )
+      {
+      ind[d] = startIndex[d];
+      ++ind[d+1];
+      }
+    }
+
+  this->m_Offset = this->m_Image->ComputeOffset(ind);
+  m_SpanEndOffset = this->m_Offset + static_cast< OffsetValueType >( size[0] );
+  m_SpanBeginOffset = this->m_Offset;
+}
+
+} // end namespace itk
+
+#endif
