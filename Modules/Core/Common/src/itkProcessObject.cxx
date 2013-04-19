6b3f5f8624c656ddf319f34a7d767193bd23d384
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-04-19 14:21:25 -0400

    WIP: PERF: Add global variable of index names in ProcessObject
    COMPATIBILITY: Renamed default index ios' prefix from
    "IndexedDataObject" to just "_" followed by the index number.
    
    There are two motivation for this: 1) The prior approach required each
    comparison done in the named map to compare 17 chars to get to the
    number, this is just a waist of computational resources. 2) With the
    introduction on the global variable of these name, shortening the name
    reduces the ( by a small amount ) the foot print of the variable.
    
    Many calls to ProcessObject's Set/Get Inputs/Outputs methods need to
    construct a string with of the named index. This patch convert from
    the procedural approach to used a constant global variable for the
    first 100 names indexed inputs.
    
    Change-Id: I0daa7afe431b1254e07ae1e0542325b5bd33699d

--- Modules/Core/Common/src/itkProcessObject.cxx
+++ Modules/Core/Common/src/itkProcessObject.cxx
@@ -26,11 +26,34 @@
  *
  *=========================================================================*/
 #include "itkProcessObject.h"
+#include "itkMutexLockHolder.h"
 
 #include <stdio.h>
 
 namespace itk
 {
+
+
+namespace
+{ // local namespace for managing globals
+const size_t ITK_GLOBAL_INDEX_NAMES_NUMBER = 100;
+const size_t ITK_GLOBAL_INDEX_NAMES_LENGTH = 4;
+const char globalIndexNames[ITK_GLOBAL_INDEX_NAMES_NUMBER][ITK_GLOBAL_INDEX_NAMES_LENGTH] =
+{ "_0", "_1", "_2", "_3", "_4", "_5", "_6", "_7", "_8", "_9",
+  "_10", "_11", "_12", "_13", "_14", "_15", "_16", "_17", "_18", "_19",
+  "_20", "_21", "_22", "_23", "_24", "_25", "_26", "_27", "_28", "_29",
+  "_30", "_31", "_32", "_33", "_34", "_35", "_36", "_37", "_38", "_39",
+  "_40", "_41", "_42", "_43", "_44", "_45", "_46", "_47", "_48", "_49",
+  "_50", "_51", "_52", "_53", "_54", "_55", "_56", "_57", "_58", "_59",
+  "_60", "_61", "_62", "_63", "_64", "_65", "_66", "_67", "_68", "_69",
+  "_70", "_77", "_72", "_73", "_74", "_75", "_76", "_77", "_78", "_79",
+  "_80", "_88", "_82", "_83", "_84", "_85", "_86", "_87", "_88", "_89",
+  "_90", "_99", "_92", "_93", "_94", "_95", "_96", "_97", "_98", "_99"
+};
+
+}
+
+
 /**
  * Instantiate object with no start, end, or progress methods.
  */
@@ -887,18 +910,15 @@ ProcessObject::DataObjectIdentifierType
 ProcessObject
 ::MakeNameFromIndex(DataObjectPointerArraySizeType idx) const
 {
-  if ( idx < 999 )
+  if ( idx < ITK_GLOBAL_INDEX_NAMES_NUMBER )
     {
-    char buf[17+4];
-    sprintf(buf, "IndexedDataObject%u", static_cast<unsigned int>(idx));
-    return buf;
+    return ProcessObject::DataObjectIdentifierType( globalIndexNames[idx] );
     }
   else
     {
-    const char baseName[] = "IndexedDataObject";
-    std::ostringstream oss;
-    oss << baseName << idx;
-    return oss.str();
+    char buf[2+21]; // a 64-bit integer is ~20 decimal places max
+    sprintf(buf, "_%u", static_cast<unsigned int>(idx));
+    return buf;
     }
 }
 
@@ -930,7 +950,7 @@ ProcessObject::DataObjectPointerArraySizeType
 ProcessObject
 ::MakeIndexFromName(const DataObjectIdentifierType & name) const
 {
-  DataObjectIdentifierType baseName = "IndexedDataObject";
+  DataObjectIdentifierType baseName = "_";
   DataObjectPointerArraySizeType baseSize = baseName.size();
   if( name.size() <= baseSize || name.substr(0, baseSize) != baseName )
     {
@@ -974,7 +994,7 @@ bool
 ProcessObject
 ::IsIndexedName(const DataObjectIdentifierType & name) const
 {
-  DataObjectIdentifierType baseName = "IndexedDataObject";
+  DataObjectIdentifierType baseName = "_";
   DataObjectPointerArraySizeType baseSize = baseName.size();
   if( name.size() <= baseSize || name.substr(0, baseSize) != baseName )
     {
