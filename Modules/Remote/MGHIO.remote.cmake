2a1324cf6d2ae784f79f66c9f7edafc6e89f0e39
Hans Johnson <hans-johnson@uiowa.edu>
2013-05-11 12:15:03 -0500

    ENH: Add Remote Module for r/w of MGH files
    The MGH file format is prevelant in many
    neuroimaging environments and it would be
    convenient if ITK were able to do at least
    basic manipulations of these image file
    formats.
    
    This patch is intended to develop a solution
    including MGHIO as a Remote Module.
    
    The Remote Module has more extensive testing than the original.
    
    Change-Id: I8d3e3d62062c2236e8aca5760551348c5eaf7494

---
+++ Modules/Remote/MGHIO.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(MGHIO
+  "MGHIO ImageIO plugin for ITK"
+  GIT_REPOSITORY https://github.com/Slicer/itkMGHImageIO.git
+  GIT_TAG master ## Will be changed to a hash once all the build details are worked out.
+  )
