ac2d9fb56a088dca66fa78f55e2aa1f50f60e590
Kent Williams <norman-k-williams@uiowa.edu>
2013-06-20 16:34:03 -0500

    COMP: Add remote for BRAINSFilterPack
    BRAINSFilterPack is intended to hold potentially useful
    filter provided for evaluation, possibly to be included
    in a future ITK release.

---
+++ Modules/Remote/BRAINSFilterPack.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(BRAINSFilterPack
+  "BRAINSFilterPack for ITK"
+  GIT_REPOSITORY git@github.com:BRAINSia/BRAINSFilterPack.git
+  GIT_TAG 13c9a5bf0a2a238e6c117900688a5fec1ed3727c
+  )
