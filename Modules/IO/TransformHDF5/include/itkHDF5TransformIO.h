658baabcd2a18b4b950778deff1a21855306ad3b
Matt McCormick <matt.mccormick@kitware.com>
2013-07-05 01:03:46 +0000

    COMP: Update MINCTransformIO for templated transforms.
    ENH: Added test for float displacement field
    
    ENH: Updated to latest libminc
    
    Change-Id: Ifbfc4c80602b467c6f884a67dec0891b84d71b67

--- Modules/IO/TransformHDF5/include/itkHDF5TransformIO.h
+++ Modules/IO/TransformHDF5/include/itkHDF5TransformIO.h
@@ -66,7 +66,6 @@ struct HDF5CommonPathNames
   };
 
 
-
 /** \class HDF5TransformIOTemplate
  *  \brief Read&Write transforms in HDF5 Format
  *
@@ -81,13 +80,13 @@ class HDF5TransformIOTemplate:public TransformIOBaseTemplate< TInternalComputati
 private HDF5CommonPathNames
 {
 public:
-  typedef HDF5TransformIOTemplate                               Self;
-  typedef TransformIOBaseTemplate< TInternalComputationValueType >    Superclass;
-  typedef SmartPointer< Self >                                  Pointer;
-  typedef typename Superclass::TransformType                    TransformType;
-  typedef typename Superclass::TransformPointer                 TransformPointer;
-  typedef typename Superclass::TransformListType                TransformListType;
-  typedef typename TransformType::ParametersType                ParametersType;
+  typedef HDF5TransformIOTemplate                                  Self;
+  typedef TransformIOBaseTemplate< TInternalComputationValueType > Superclass;
+  typedef SmartPointer< Self >                                     Pointer;
+  typedef typename Superclass::TransformType                       TransformType;
+  typedef typename Superclass::TransformPointer                    TransformPointer;
+  typedef typename Superclass::TransformListType                   TransformListType;
+  typedef typename TransformType::ParametersType                   ParametersType;
 
   typedef typename TransformIOBaseTemplate
                       <TInternalComputationValueType>::ConstTransformListType
@@ -136,7 +135,7 @@ private:
 extern const std::string  GetTransformName(int);
 
 /** This helps to meet backward compatibility */
-typedef HDF5TransformIOTemplate<double> HDF5TransformIO;
+typedef HDF5TransformIOTemplate< double > HDF5TransformIO;
 
 } // end namespace itk
 
