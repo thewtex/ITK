bb38c6ea9970843224ba8ba2f8c1cb5cf04463f5
Mark Hiner <hinerm@gmail.com>
2013-07-11 12:58:44 -0500

    ENH: Updated SCIFIO remote reference
    Updated the SCIFIO remote reference. The SCIFIOImageIO module features
    the following improvements and bug fixes:
    
    * Takes advantage of the latest scifio-itk-bridge (bef14215)
    * Enhanced error handling (stack trace on exceptions + exit on command failure)
    * SetSeries(int) method for multi-series datasets
    * GetSeriesCount() method to determine total number of series
    * Fixed RGB image reading (was only reading 1/3 of image)
    * Fixed handshaking issues (leaving bytes on the buffer)
    * Improved SCIFIOImageIO->Write performance
    * Completely refactored the itkSCIFIOImageIOTest
    ** Allows argument flags to open RGB images, series ranges,
       complete multi-series datasets, select number of dimensions,
       select pixel type, streaming divisions, etc..
    
    Change-Id: I496e09f72322623416aaf037c821af0c5087d666

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG d096456994e0ac0e0cde7f3356e5fdbfac55c95f
+  GIT_TAG 2a060e9dbfc7a5422b842f1eb5f9e223ea09571e
   )
