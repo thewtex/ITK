ba3671fc589c40d4aeb7b9128208842a8be2f481
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2013-07-11 23:42:53 -0400

    BUG: Disable aligned attribute to fix Apple GCC 4.2.1 compiler seg fault
    Commit 2f96c578 fixed the configure test checking if aligned
    attributes were supported by the compiler. Then commit
    disabled the test for Gcc 4.1 to prevent a similar error.
    
    This commit ensures that the configure test also excludes Apple
    Gcc 4.2.1 by using __APPLE__, __GNUC_MINOR__ and __GNUC_PATCHLEVEL__
    as detailed here [1] and here [2]
    
    Fixes #3172 - See https://itk.icts.uiowa.edu/jira/browse/ITK-3172
    
    [1] http://gcc.gnu.org/onlinedocs/cpp/Common-Predefined-Macros.html
    
    [2] http://stackoverflow.com/a/2166491
    
    Change-Id: I67c870d8f68b0fd2fafaf470738bf125887165d7

--- Modules/Core/Common/CMake/itkCheckHasGNUAttributeAligned.cxx
+++ Modules/Core/Common/CMake/itkCheckHasGNUAttributeAligned.cxx
@@ -14,18 +14,19 @@ struct B
   char b;
 } __attribute__ ((aligned (64)));
 
-// fail for gcc 4.1
-#if __GNUC__ == 4 && __GNUC_MINOR__ == 1
-#error This version of GCC is know to have a internal compilation error with this feature in ITK.
+// fail for gcc 4.1 and 4.2.1
+#if __GNUC__ == 4 && ( __GNUC_MINOR__ == 1 || \
+  ( __GNUC_MINOR__ == 2 && __GNUC_PATCHLEVEL__ == 1 && __APPLE__ ) )
+#error This version of GCC is known to have a internal compilation error with this feature in ITK.
 #endif
 
 // BUG DETECTION: This following usage may generate a segfault during
 // compilation.
 //
 // The following block of code causes an internal compiler error with
-// Apple's (GCC) 4.2.1 (Apple Inc. build5666) (dot 3) compiler when
-// compiled in debug mode with the -g flag.
+// Apple's (GCC) 4.2.1 (Apple Inc. build5666) (dot 3) compiler.
 //
+// See https://itk.icts.uiowa.edu/jira/browse/ITK-3172
 template <typename T>
 class foo
 {
