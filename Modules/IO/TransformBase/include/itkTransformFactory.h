76184bbd17952e40c7f6ecbbdbc89ceba507c438
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-07-19 11:34:21 -0400

    ENH: adding shared for transform libraries
    Change-Id: Icb0f16fb24252481f5af5c583a87c1227bf5fc84

--- Modules/IO/TransformBase/include/itkTransformFactory.h
+++ Modules/IO/TransformBase/include/itkTransformFactory.h
@@ -27,6 +27,7 @@
  *=========================================================================*/
 #ifndef __itkTransformFactory_h
 #define __itkTransformFactory_h
+#include "ITKIOTransformBaseExport.h"
 
 #include "itkTransformFactoryBase.h"
 
