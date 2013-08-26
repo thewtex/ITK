74cc88964ee0d28d04cfc0c4bd082da4ec07c88c
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2013-08-26 11:51:40 -0400

    ENH: Remove "ITK" prefix from remote modules.
    Remove "ITK" prefix from the remote module names. To better
    distinguish remote modules from internal ITK modules.
    
    This requires the corresponding cmake code changes in each
    of the remote modules.
    
    DOC: Document the naming rules for remote modules.
    
    The name of each remote module appear in itk_fetch_module()
    function should be the same as the one defined in itk-module.cmake.
    Also, the remote module names should not contain the "ITK" string
    prefix.
    
    MGHIO updates included in 841832fd94889d0793b606c0a5b9be981b98cec3
    SCIFIO updates included in 67be5138c10769e2d9b09e96e2656ccf1d8c7a79
    
    Change-Id: If7977d94b6ffd8ad690eef1f4558decbb851e425

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,4 +1,4 @@
-itk_fetch_module(ITKIOSCIFIO
+itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
   GIT_TAG 9cb93a368e2d431ee5b03e061ed210a07adb98a7
