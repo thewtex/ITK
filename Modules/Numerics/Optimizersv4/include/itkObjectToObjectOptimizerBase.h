63918a710c5e39e33e11d6ea9428961a9ed607d5
Hans Johnson <hans-johnson@uiowa.edu>
2013-09-10 08:24:46 -0500

    BUG: Function should have const signature
    The function GetCurrentPosition() returns a const reference
    to the parameters and does not change any ivars, so it should
    be a const function.  This change facilitates accessing a const
    version of the optimizers for the purpose of printing
    diagnostics.
    Also this function is made virtual to have a similar singnature
    with the one in itk::Optimizer class where this function is
    defined by a macro:
    
      itkGetConstReferenceMacro(CurrentPosition,ParametersType);
    
    This definition is equvalent to:
    
      virtual const ParametersType&
          GetCurrentPosition() const;
      {
      return m_CurrentPosition;
      }
    
    In itk::ObjectToObjectOptimizerBaseTemplate the GetCurrentPosition()
    function is defined again for Optimizerv4 set of classes with a new
    implementation and the following signature:
    
      const ParametersType& GetCurrentPosition();
    
    As the implementation is different, the macro definition cannot be
    used anymore, but the above signature of function should be changed
    to be consistent with the aforementioned one in itk::Optimizer class.
    Also, defined as a virtual, helps possible overwriting of this function
    by derived optimizer classes in future.
    
    Change-Id: Id26c6751c6015bc0a0ee3dabb72c1ed0d3b13b66

--- Modules/Numerics/Optimizersv4/include/itkObjectToObjectOptimizerBase.h
+++ Modules/Numerics/Optimizersv4/include/itkObjectToObjectOptimizerBase.h
@@ -138,7 +138,7 @@ public:
   /** Get a reference to the current position of the optimization.
    * This returns the parameters from the assigned metric, since the optimizer
    * itself does not store a position. */
-  const ParametersType & GetCurrentPosition();
+  virtual const ParametersType & GetCurrentPosition() const;
 
   /** Run the optimization.
    * \param doOnlyInitialization This is false by default. It should only be
