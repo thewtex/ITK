77dfc0ac0c8705721c7b3e08ba2d955b7fa7471f
Hans Johnson <hans-johnson@uiowa.edu>
2013-09-10 08:24:46 -0500

    BUG: Function should have const signature
    The function GetCurrentPosition() returns a const reference
    to the parameters and does not change any ivars, so it should
    be a const function.  This change facilitates accessing a const
    version of the optimizers for the purpose of printing
    diagnostics.
    
    Change-Id: Id26c6751c6015bc0a0ee3dabb72c1ed0d3b13b66

--- Modules/Numerics/Optimizersv4/include/itkObjectToObjectOptimizerBase.hxx
+++ Modules/Numerics/Optimizersv4/include/itkObjectToObjectOptimizerBase.hxx
@@ -181,7 +181,7 @@ ObjectToObjectOptimizerBaseTemplate<TInternalComputationValueType>
 template<class TInternalComputationValueType>
 const typename ObjectToObjectOptimizerBaseTemplate<TInternalComputationValueType>::ParametersType &
 ObjectToObjectOptimizerBaseTemplate<TInternalComputationValueType>
-::GetCurrentPosition()
+::GetCurrentPosition() const
 {
   if( this->m_Metric.IsNull() )
     {
