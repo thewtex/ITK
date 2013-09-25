9a1c5557add7dd8cc687ef4f34e6de39f28a01e5
Kent Williams <norman-k-williams@uiowa.edu>
2013-09-25 11:16:06 -0500

    BUG: Bug fixed in remote MGHImageIO repo, update git tag
    Change-Id: I59d792f4b1efa45a7bf86368d3f841cc9de96ef3

--- Modules/Remote/MGHIO.remote.cmake
+++ Modules/Remote/MGHIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(MGHIO
   "MGHIO ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/Slicer/itkMGHImageIO.git
-  GIT_TAG 3566fcdd053249a59ba958586d8c32e5042f285b
+  GIT_TAG 63abf2315fbcd13c922079931ecbed6168f590c4
   )
