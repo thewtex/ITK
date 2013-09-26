0a95cb588ee496981a9f850a3b923d03b2517850
Kent Williams <norman-k-williams@uiowa.edu>
2013-09-26 11:20:29 -0500

    WIP: demo of how to fix problems with TransformFileWriter specializations
    Change-Id: I0aaffd8c0cce11f0ea32aa7043e944d4bac43bf2

---
+++ Modules/IO/TransformBase/src/itkTransformFileWriterSpecializations.cxx
@@ -0,0 +1,87 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#include "itkTransformFileWriter.h"
+namespace itk
+{
+
+namespace TransformFileWriterHelper
+{
+template<>
+void AddToTransformList<double,double>(TransformBaseTemplate<double>::ConstPointer &transform,
+                                        std::list< TransformBaseTemplate<double>::ConstPointer > & transformList)
+{
+  transformList.push_back( TransformBaseTemplate<double>::ConstPointer(transform) );
+}
+
+template<>
+void AddToTransformList<float,float>(TransformBaseTemplate<float>::ConstPointer &transform,
+                                      std::list< TransformBaseTemplate<float>::ConstPointer > & transformList)
+{
+  transformList.push_back( TransformBaseTemplate<float>::ConstPointer(transform) );
+}
+}
+
+template<>
+void TransformFileWriterTemplate<double>
+::PushBackTransformList(const Object *transObj)
+{
+  TransformBaseTemplate<double>::ConstPointer dblptr = dynamic_cast<const TransformBaseTemplate<double> *>( transObj );
+  if( dblptr.IsNotNull() )
+    {
+    TransformFileWriterHelper::AddToTransformList<double, double>( dblptr, m_TransformList );
+    }
+  else
+    {
+    TransformBaseTemplate<float>::ConstPointer fltptr = dynamic_cast<const TransformBaseTemplate<float> *>( transObj );
+    if( fltptr.IsNotNull() )
+      {
+      TransformFileWriterHelper::AddToTransformList<double, float>( fltptr, m_TransformList );
+      }
+    else
+      {
+      itkExceptionMacro("The input of writer should be whether a double precision"
+                        "or a single precision transform type.");
+      }
+    }
+}
+
+template<>
+void TransformFileWriterTemplate<float>
+::PushBackTransformList(const Object *transObj)
+{
+  TransformBaseTemplate<double>::ConstPointer dblptr = dynamic_cast<const TransformBaseTemplate<double> *>( transObj );
+  if( dblptr.IsNotNull() )
+    {
+    TransformFileWriterHelper::AddToTransformList<float, double>( dblptr, m_TransformList );
+    }
+  else
+    {
+    TransformBaseTemplate<float>::ConstPointer fltptr = dynamic_cast<const TransformBaseTemplate<float> *>( transObj );
+    if( fltptr.IsNotNull() )
+      {
+      TransformFileWriterHelper::AddToTransformList<float, float>( fltptr, m_TransformList );
+      }
+    else
+      {
+      itkExceptionMacro("The input of writer should be whether a double precision"
+                        "or a single precision transform type.");
+      }
+    }
+}
+
+}
