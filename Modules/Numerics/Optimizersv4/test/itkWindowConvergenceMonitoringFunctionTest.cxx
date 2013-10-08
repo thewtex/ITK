0db4faa83dbcc023c8f1fd5ca972b147c3ccad17
Hans Johnson <hans-johnson@uiowa.edu>
2013-10-08 11:49:41 -0500

    BUG: Match Convergence Window Precision
    When computing registrations, the convergence windows computations
    should be done in the same precision as the registration framework
    in order to keep the computation precision consistent.
    
    Change-Id: Id9d8724bc4fdc573b2d9b792c5dbc1d3f6fb36a2

--- Modules/Numerics/Optimizersv4/test/itkWindowConvergenceMonitoringFunctionTest.cxx
+++ Modules/Numerics/Optimizersv4/test/itkWindowConvergenceMonitoringFunctionTest.cxx
@@ -21,12 +21,13 @@
 
 int itkWindowConvergenceMonitoringFunctionTest( int itkNotUsed( argc ), char * [] )
 {
-  typedef itk::Function::WindowConvergenceMonitoringFunction<double> ConvergenceMonitoringType;
+  typedef float RealType;
+  typedef itk::Function::WindowConvergenceMonitoringFunction<RealType> ConvergenceMonitoringType;
   ConvergenceMonitoringType::Pointer convergenceMonitoring = ConvergenceMonitoringType::New();
 
   convergenceMonitoring->SetWindowSize( 10 );
 
-  for( double x = 0.0; x < 20; x += 1.0 )
+  for( RealType x = 0.0; x < 20; x += 1.0 )
     {
     convergenceMonitoring->AddEnergyValue( vcl_pow( 2.0, -x ) );
     try
