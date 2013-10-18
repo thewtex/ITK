58034457890cbc9166c721e5c623751227dbcf18
Matt McCormick <matt.mccormick@kitware.com>
2013-10-18 11:09:32 +0000

    DOC: Flat refers to binary in FlatStructuringElement.
    Change-Id: Iafc61993864b2a668f7678580dcce61884539713

--- Modules/Filtering/MathematicalMorphology/include/itkFlatStructuringElement.h
+++ Modules/Filtering/MathematicalMorphology/include/itkFlatStructuringElement.h
@@ -30,9 +30,10 @@ namespace itk
  *
  * FlatStructuringElement provides several static methods, which can
  * be used to create a structuring element with a particular shape,
- * size, etc. Currently, those methods allow to create a ball, a box,
- * a cross structuring element, and let create a structuring element
- * based on an image.
+ * size, etc. Currently, those methods enable the creation of a ball, a box,
+ * a cross structuring element, and enable the creation of a structuring
+ * element based on an image. "Flat" refers to binary as opposed to
+ * grayscale structuring elements.
  *
  * \ingroup ITKMathematicalMorphology
  *
