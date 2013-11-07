31af4509899813f7167a9f648ee8001449ee5baa
Brian Helba <brian.helba@kitware.com>
2013-11-07 11:57:26 -0500

    BUG: Update SCIFIO with upstream fixes
    Upstream contains a commit to fix an infinite loop.
    Change-Id: I3248ef540712d35ac2cbf6365d8f10e30d89d7d0

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG 3e76055918e70c8e97730d1e321ec59f15007f4c
+  GIT_TAG 214a78b8f9c4406a5510d2719f9bb9a2c96b57ee
   )
