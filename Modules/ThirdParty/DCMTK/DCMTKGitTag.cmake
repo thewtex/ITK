53790971d2d7cf44d0be0545b25315cb1485057a
Hans Johnson <hans-johnson@uiowa.edu>
2013-11-23 07:53:05 -0600

    ENH: Update DCMTK to include improvements to CMake
    A warning was issues when building DCMTK
    
    ITK-build/Modules/ThirdParty/DCMTK/ITKDCMTK_ExtProject/CMake/3rdparty.cmake:9:70
    Argument not separated from preceding token by whitespace.
    
    That was fixed during CMake cleanups of the parent project.
    Resyncronized the ITK specific minor patches on top of
    the main project to incorporate these changes directly.
    
    Change-Id: Ic6eee53c1a0566887c46b2fda88c868534a41017

--- Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
+++ Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
@@ -4,6 +4,6 @@
 # compiler fixes and bug fixes
 set(DCMTK_GIT_REPOSITORY "http://github.com/InsightSoftwareConsortium/DCMTK.git")
 # this is a specific tag.  It corresponds to the symbolic tag
-# 20130129_DCMTK_PATCHES_FOR_ITK which incorporates changes needed to build and deploy
-# with ITK
-set(DCMTK_GIT_TAG "257e800c42f40ff3207a9242fa225c850c56dd07")
+# 20131123_DCMTKUpstreamMerge which incorporates changes needed to build and deploy
+# with ITK.  DCMTK has made recent improvements to it's CMake compatibility as well.
+set(DCMTK_GIT_TAG "19eea0a1eb50bc299220abacc696c3e68559aa2c")
