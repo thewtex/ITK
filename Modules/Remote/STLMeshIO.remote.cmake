8f63da26efd29fa3e053dd4c9c964c1c4c5d39f6
Luis Ibanez <luis.ibanez@kitware.com>
2014-01-12 08:48:02 -0500

    ENH: Add STLMeshIO remote module.
    This module provides a MeshIO class for reading and writing STL files.
    
    Typically, STL files contain 2D manifolds in a 3D space. In other words,
    2D surfaces in 3D space. They are usually represented with triangle cells.
    
            http://en.wikipedia.org/wiki/STL_(file_format)
    
    The STLMeshIO can read and write STL files, and ideally it should be
    used in conjunction with a QuadEdgeMesh, or at least with a Mesh with
    3D points and 2D triangle cells.
    
    The STLMeshIO module manages STL files in both ASCII and BINARY formats.
    
    It also manages Endiannes when using BINARY format.
    
    More details in Insight Journal article:
    
                   http://hdl.handle.net/10380/3452
         http://www.insight-journal.org/browse/publication/913
    
    Change-Id: Id2dbe8bc5754d448da4214c28dac36972028231f

---
+++ Modules/Remote/STLMeshIO.remote.cmake
@@ -0,0 +1,6 @@
+# Contact: Luis Ibanez <luis.ibanez@kitware.com>
+itk_fetch_module(STLMeshIO
+  "STL (STereoLithography) MeshIO plugin for ITK   http://hdl.handle.net/10380/3452"
+  GIT_REPOSITORY https://github.com/luisibanez/ITKSTLMeshIO.git
+  GIT_TAG 4d2ba67c64f7a28e4faf7ec3e85b0657d7cb1278
+  )
