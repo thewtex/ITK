41098ec59890aa6a317c442d82b9d99c461979f4
Matt McCormick <matt.mccormick@kitware.com>
2014-01-31 15:05:05 -0500

    COMP: Bump DCMTK to fix warning.
    The previous CMake related patch not integrated upstream was rebased on the
    upstream DCMTK-3.6.1_20131114 tag, d60870a5b9bbb9792f63d9c929358446f50f1b88.
    Upstream has fixed a warning in CMake/3rdparty.cmake:9:70:
    
    CMake Warning (dev) at CMake/dcmtkPrepare.cmake:288 (INCLUDE):
    Syntax Warning in cmake code at
    
       CMake/3rdparty.cmake:9:70
    
    Argument not separated from preceding token by whitespace.
    
    Co-authored by Brian Helba <brian.helba@kitware.com>
    
    Change-Id: Ie21356974bbf58c9bac5421789bfbdee7427be80

--- Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
+++ Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
@@ -4,6 +4,6 @@
 # compiler fixes and bug fixes
 set(DCMTK_GIT_REPOSITORY "http://github.com/InsightSoftwareConsortium/DCMTK.git")
 # this is a specific tag.  It corresponds to the symbolic tag
-# 20130129_DCMTK_PATCHES_FOR_ITK which incorporates changes needed to build and deploy
+# 20131114_DCMTK_PATCHES_FOR_ITK which incorporates changes needed to build and deploy
 # with ITK
-set(DCMTK_GIT_TAG "257e800c42f40ff3207a9242fa225c850c56dd07")
+set(DCMTK_GIT_TAG "840cf5f158ca837ec398df3d6287ae953ff3bfce")
