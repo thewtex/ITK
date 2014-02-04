thewtex/ITK

commit 2a3100ceea5bb3afe407946f6a88df8cc2acd4cb

COMP: ImageRegistrationHistogramPlotter HistogramWriter visibility.
This fixes:

ImageRegistrationHistogramPlotter.cxx:171:7 warning: 'HistogramWriter'
declared with greater visibility than the type of its fild
'HistogramWriter::m_HistogramFileWriter'

Change-Id: I52936d5b35bfc2b30a2c7b21bb9b862406f3bba7

Examples/Registration/ImageRegistrationHistogramPlotter.cxx
@@ -168,6 +168,8 @@ public:
 // Software Guide : EndLatex
 
 // Software Guide : BeginCodeSnippet
+namespace
+{
 class HistogramWriter
 {
 public:
@@ -381,6 +383,7 @@ private:
   // Software Guide : EndCodeSnippet
   std::string   m_OutputFile;
 };
+} // end anonymous namespace
 
 
 // Command - observer invoked after every iteration of the optimizer

commit 6f7b34f255fd2b1cbde7f33273720eb91e4f6875

WIP: COMP: Fix a namespace warning
This was caused by 2a3100ceea5bb3afe407946f6a88df8cc2acd4cb

The warning is:

ImageRegistrationHistogramPlotter.cxx:391:
warning: 'CommandIterationUpdate' has a field
'CommandIterationUpdate::m_JointHistogramWriter' whose type uses the
anonymous namespace

Change-Id: I8afff3bf272826435c8499971adcebf96b49846e

Examples/Registration/ImageRegistrationHistogramPlotter.cxx
@@ -168,8 +168,6 @@ public:
 // Software Guide : EndLatex
 
 // Software Guide : BeginCodeSnippet
-namespace
-{
 class HistogramWriter
 {
 public:
@@ -383,7 +381,6 @@ private:
   // Software Guide : EndCodeSnippet
   std::string   m_OutputFile;
 };
-} // end anonymous namespace
 
 
 // Command - observer invoked after every iteration of the optimizer