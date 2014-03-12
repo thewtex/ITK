99aadf87398c0ab02a91112ebb9b60b722324b62
Kent Williams <norman-k-williams@uiowa.edu>
2014-03-12 16:23:59 -0500

    ENH: Add remote module for AnalyzeObjectMapIO
    This implements an IO class for reading and
    writing Analyze Object Map files.
    
    Based on this Insight Journal article:
    http://www.insight-journal.org/browse/publication/178
    
    Change-Id: I3a93be66409d627d96f196ff167ce889e7165431

---
+++ Modules/Remote/AnalyzeObjectMapIO.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(AnalyzeObjectMapIO
+  "AnalyzeObjectMapIO plugin for ITK. From Insight Journal http://www.insight-journal.org/browse/publication/178"
+  GIT_REPOSITORY https://github.com/BRAINSia/itkAnalyzeObjectMap.git
+  GIT_TAG 37c3685406927dd771edbc3af020e033c3713a43
+  )
