cb7eb2fe5daea6198e741f9d519ba02c27215721
Kent Williams <norman-k-williams@uiowa.edu>
2014-03-12 16:23:59 -0500

    ENH: Add remote module for AnalyzeObjectMapIO
    Change-Id: I3a93be66409d627d96f196ff167ce889e7165431

---
+++ Modules/Remote/AnalyzeObjectMapIO.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(AnalyzeObjectMapIO
+  "AnalyzeObjectMapIO plugin for ITK"
+  GIT_REPOSITORY git@github.com:BRAINSia/itkAnalyzeObjectMap.git
+  GIT_TAG 2c9e9a2cf716b92f241f0f8763d4ce4f30c98829
+  )
