8d32b8628e1e9475f88698713d107d0a9ab13bad
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    ENH: Thread pool
    Change-Id: Ia5d12605dc85de39822364294cc6b083d806d6d9

---
+++ Modules/Core/Common/include/itkWinThreadPool.h
@@ -0,0 +1,107 @@
+
+
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __itkWinThreadPool_h
+#define __itkWinThreadPool_h
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include "itkObject.h"
+#include "itkObjectFactory.h"
+#include "itkWinJob.h"
+
+namespace itk
+{
+
+class ITKCommon_EXPORT ThreadPool : public Object
+{
+public:
+
+  /** Standard class typedefs. */
+  typedef ThreadPool               Self;
+  typedef Object                   Superclass;
+  typedef SmartPointer<Self>       Pointer;
+  typedef SmartPointer<const Self> ConstPointer;
+
+/** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ThreadPool, Object);
+
+  static ThreadPool & GetThreadPool();
+
+  static ThreadPool & GetThreadPool(int maxThreads);
+
+  static SmartPointer<ThreadPool> GetNewThreadPool(int maxThreads);
+
+  static void DeleteSingletonInstance();
+
+  void DestroyPool(int maxPollSecs);
+
+  int AssignWork(WinJob worker);
+
+  void InitializeThreads(int maxThreads);
+
+  bool WaitForThread(int id);
+
+  int GetCompletedJobs() const;
+
+protected:
+
+  ThreadPool();
+  ~ThreadPool();
+private:
+  int m_CompletedJobs;
+  bool m_ScheduleForDestruction;
+  static ThreadPool * m_ThreadPoolInstance;
+  static SmartPointer<ThreadPool> m_SThreadPoolInstance;
+  static bool m_InstanceFlag;
+  ThreadPool(ThreadPool const &);              // copy constructor is private
+  ThreadPool & operator=(ThreadPool const &);  // assignment operator is
+
+  // private
+
+  void RemoveActiveId(int id);
+
+  int                 m_MaxThreads;
+  HANDLE              m_WorkAvailable;     // semaphore
+  std::vector<WinJob> m_WorkerQueue;
+  std::vector<int>    m_ActiveThreadIds;
+  int                 m_IncompleteWork;
+  int                 m_QueueSize;
+  unsigned int        m_IdCounter;
+  bool                m_ExceptionOccured;
+  HANDLE*             m_ThreadHandles;
+  static HANDLE       m_MutexSync;           // mutex
+  static HANDLE       m_ActiveThreadMutex;   // mutex
+  static HANDLE       m_MutexWorkCompletion; // mutex
+
+  WinJob FetchWork();
+
+  static void * ThreadExecute(void *param);
+
+};
+
+}
+#endif
+#endif
