973e475bbdc28ad2ffe6fd5c15342471277a12ef
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    WIP: Added a windows and linux based Thread pool.
    1)	Initialize the thread pool with 0 threads and grow
    2)	Allow to add threads to the thread pool
    3)	Always execute a job - no waiting for a thread to be available
    4)      Multithreader needs to access threadpool as singleton
    
    Pending Changes
    1)      Mutex locking and unlocking - verify
    2)      Private copy constructor for thread job
    3)      Format code
    
    Change-Id: I0eb6abb64a9054373968c226562633cc153db8c2

---
+++ Modules/Core/Common/include/itkPThreadPool.h
@@ -0,0 +1,103 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __itkPthreadPool_h
+#define __itkPthreadPool_h
+
+#ifdef __unix__
+#include <pthread.h>
+#include <semaphore.h>
+#include <iostream>
+#include <vector>
+#include "itkPThreadJob.h"
+#include "itkObject.h"
+#include "itkObjectFactory.h"
+
+
+namespace itk
+{
+
+class ITKCommon_EXPORT ThreadPool : public Object
+{
+public:
+
+  /** Standard class typedefs. */
+  typedef ThreadPool               Self;
+  typedef Object                   Superclass;
+  typedef SmartPointer<Self>       Pointer;
+  typedef SmartPointer<const Self> ConstPointer;
+  typedef std::vector<ThreadJob>   Queue;
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ThreadPool, Object);
+
+  static ThreadPool & GetThreadPool();
+
+  static ThreadPool & GetThreadPool(int maxThreads);
+
+  static SmartPointer<ThreadPool> GetNewThreadPool(int maxThreads);
+
+  static void DeleteSingletonInstance();
+
+  void DestroyPool(int maxPollSecs);
+
+  int AssignWork(ThreadJob worker);
+
+  void InitializeThreads(int maxThreads);
+
+  bool WaitForThread(int id);
+
+  int GetCompletedJobs() const;
+
+protected:
+
+  ThreadPool();
+  ~ThreadPool();
+private:
+  int m_CompletedJobs;
+  bool m_ScheduleForDestruction;
+  static SmartPointer<ThreadPool> m_SThreadPoolInstance;
+  static ThreadPool * m_ThreadPoolInstance;
+  static bool m_InstanceFlag;
+  ThreadPool(ThreadPool const &);              // copy constructor is private
+  ThreadPool & operator=(ThreadPool const &);  // assignment operator is
+  int                    m_ThreadCount;
+  sem_t                  m_WorkAvailable;
+  Queue 		 m_WorkerQueue;
+  std::vector<int>       m_ActiveThreadIds;
+  int                    m_IncompleteWork;
+  int                    m_QueueSize;
+  unsigned int           m_IdCounter;
+  bool                   m_ExceptionOccured;
+  static pthread_mutex_t m_MutexSync;
+  static pthread_mutex_t m_ActiveThreadMutex;
+  static pthread_mutex_t m_MutexWorkCompletion;
+  std::vector<pthread_t> m_ThreadHandles;
+
+  ThreadJob FetchWork();
+  static void * ThreadExecute(void *param);
+  void RemoveActiveId(int id);
+  void AddThread();
+};
+
+}
+#endif
+#endif
+
