7fe7864f80d7dd7f9a00a1a348f6f6951af97ab5
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    WIP: Added a windows and linux based Thread pool.
    NOTE: Checking in this "INCOMPLETE" patch to see what build errors are
    found on a mac. Until I get my hands on a mac
    Fixed a compile time bug
    List of changes:
    1)	Initialize the thread pool with 0 threads and grow
    2)	Allow to add threads to the thread pool
    3)	Always execute a job - no waiting for a thread to be available
    4)      Multithreader accesses the threadpool as singleton
    5)      Removed confusing "GetThreadPool" methods from the ThreadPool
    classes
    6)      Removed itkDebugPrint macro that was added earlier - rolling back
    itkMacro.h to original
    7)      Using ITK's SinpleFastMutex and MutexLockHolder instead of doing
    low level mutex by self
    8)      Code formatting
    9)      Removing some of Hans' changes to fix the thread pool
    functionality.
    10)     Added/removed new methods and variables to fix mac build issue -
    removed casting pthread_t to int
    
    Pending Changes
    1)      Private copy constructor for thread job
    2)      Check to see if PthreadPool and WinThreadPool can inherit from a
    common ThreadPool class
    3)      Document methods and variables
    
    Change-Id: I0eb6abb64a9054373968c226562633cc153db8c2

---
+++ Modules/Core/Common/src/itkWinThreadPoolTester.cxx
@@ -0,0 +1,92 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#pragma once
+#include <iostream>
+#include "itkWinThreadpool.h"
+#include <stdlib.h>
+
+const int ITERATIONS = 10;
+
+void * execute(void *ptr)
+{
+  // Here - get any args from ptr.
+  std::cout << std::endl << "Thread fn starting.." << std::endl;
+  int m = (int)ptr;
+  std::cout << "Ptr received int :" << m << std::endl;
+  int    n = 10;
+  double factorial = 1.0;
+  for( int j = 0; j < 90; j++ )
+    {
+    factorial = 1.0;
+    for( int i = 1; i <= n; i++ )
+      {
+      factorial = factorial * i;
+      }
+    }
+  std::cout << std::endl << "Thread fn DONE" << std::endl;
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+  try
+    {
+    int numThreadsInThreadPool = 25;
+    if( argc > 1 )
+      {
+      numThreadsInThreadPool = atoi(argv[1]);
+      }
+    std::cout << "Testing itkPthreadPool. Num of threads :" << numThreadsInThreadPool << std::endl;
+    WinThreadPool & myPool = WinThreadPool::GetPoolInstance(numThreadsInThreadPool);
+    // assign work
+    for( unsigned int i = 0; i < ITERATIONS; i++ )
+      {
+      WinJob winJob;
+      winJob.ThreadArgs.otherArgs = (void *) i;
+      winJob.ThreadFunction = (&execute);
+      std::cout << "Thread id[" << winJob.Id << "] " << std::endl;
+      int id = myPool.AssignWork(winJob);
+
+      myPool.WaitForThread(id);
+
+      }
+
+    std::cout << "All threads finished" << std::endl;
+
+    WinThreadPool::DeleteInstance();
+
+    }
+  catch( const std::exception& ex )
+    {
+    std::cout << ex.what() << std::endl;
+    return -1;
+    }
+  catch( const std::string& ex )
+    {
+    std::cout << "Exception occured" << std::endl;
+    return -1;
+    }
+  catch( ... )
+    {
+    std::cout << "Uncaught Exception occured" << std::endl;
+    return -1;
+    }
+  return 0;
+}
