5d45ca23f3c20b702cb5d68253377bdfefd1b4cf
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    WIP: Thread pool.
    NOTE: Cleaned up to be ITK style.
    
    Fixed a compile time bug
    List of changes:
    1)	Initialize the thread pool with 0 threads and grow
    2)	Allow to add threads to the thread pool
    3)	Always execute a job - no waiting for a thread to be available
    4)      Multithreader accesses the threadpool as singleton
    5)      Removed confusing "GetThreadPool" methods from the ThreadPool
    classes
    6)      Removed itkDebugPrint macro that was added earlier - rolling back
    itkMacro.h to original
    7)      Using ITK's SinpleFastMutex and MutexLockHolder instead of doing
    low level mutex by self
    8)      Code formatting
    9)      Removing some of Hans' changes to fix the thread pool
    functionality.
    10)     Added/removed new methods and variables to fix mac build issue -
    removed casting pthread_t to int
    11)     Removing ThreadHandle variable from PThreadJob.h
    12)     Added semaphores to control wait and removed mutexes from loop-
    checking to see if this removed the segfaults
    13)     Removed debugmacros from itkPthreadJob.h
    14)     Removed itkWinJob.h and itkPThreadJob.h and created a single file -
    itkThreadJob.h. Also, changed maps to vectors in itkPThreadPool.cxx.
    Documented variables in itkThreadJob.h
    15)     Refactored and documented methods and variables
    16)     Refactored files to match MultiThreader structure
    17)     Removed semaphores so a lot of code can now be shared in
    itkThreadPool.cxx
    18)     Several fixes for SEGFAULTS and others
    
    ENH: Refactor variable names for class functions
    
    Class member functions should begin with m_ and they
    should be private to the class with accessor functions.
    
    This patch changes the name, but does not protect them
    with member access functions yet.
    
    WIP: Trying vector optimization for
    
    Popping and pushing elements, and pre-allocating space
    for the JobThreads threads.
    
    ENH: Rename mutex variables
    Mutex variables should match the variables that they protect.
    
    ENH: Add class for ThreadStructs
    Improve readabiltiy of code by using a small lightweight
    class instead of the std::pair.
    
    ENH: Improve names of sentinal flags
    Values used to indicates non-running job status are now using enumeration
    to make code easier to read.
    
    ENH: Improve variable names.
    ENH: Remove std::cout printfs
    
    This is a new patch worked on May 4, 2014 attempting to improve
    performance.
    
    WIP: Removed while loops with heavy mutex locking.. Now bringing back
    semaphores. Not tested on windows yet..
    
    Using named semaphores for apple/mac osx
    
    Change-Id: Ibf308b14b37b8727b2878bf65306d01a12b809d6

---
+++ Modules/Core/Common/include/itkThreadSemaphorePair.h
@@ -0,0 +1,55 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkThreadSemaphorePair_h
+#define __itkThreadSemaphorePair_h
+#include "itkThreadPool.h"
+
+namespace itk
+{
+/**
+ * \class ThreadSemaphorePair
+ * \brief Inner class to ThreadPool. Used to create a pair of ThreadID and semahore.
+ * \ingroup OSSystemObjects
+ * \ingroup ITKCommon
+ */
+
+
+  class ThreadPool::ThreadSemaphorePair
+  {
+public:
+    ThreadSemaphorePair(const ThreadProcessIDType & tph);
+    int SemaphoreWait();
+    int SemaphorePost();
+#if defined(__APPLE__)
+      semaphore_t           m_Semaphore;
+#elif defined(_WIN32) || defined(_WIN64)
+    HANDLE               m_Semaphore;
+#else
+    sem_t                m_Semaphore;
+#endif
+
+    ThreadSemaphorePair *m_Next;
+    ThreadProcessIDType  m_ThreadProcessHandle;
+
+private:
+    static int m_SemCount;
+    ThreadSemaphorePair(); //purposefully not implemented.
+  };
+  }
+
+#endif
