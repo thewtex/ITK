b82ea572d498565ffede7e6c53c46a91528f7fe0
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    WIP: Added a windows and linux based Thread pool.
    NOTE: Checking in this "INCOMPLETE" patch to see what build errors are
    found on a mac.
    
    Fixed a compile time bug
    List of changes:
    1)	Initialize the thread pool with 0 threads and grow
    2)	Allow to add threads to the thread pool
    3)	Always execute a job - no waiting for a thread to be available
    4)      Multithreader accesses the threadpool as singleton
    5)      Removed confusing "GetThreadPool" methods from the ThreadPool
    classes
    6)      Removed itkDebugPrint macro that was added earlier - rolling back
    itkMacro.h to original
    7)      Using ITK's SinpleFastMutex and MutexLockHolder instead of doing
    low level mutex by self
    8)      Code formatting
    9)      Removing some of Hans' changes to fix the thread pool
    functionality.
    10)     Added/removed new methods and variables to fix mac build issue -
    removed casting pthread_t to int
    11)     Removing ThreadHandle variable from PThreadJob.h
    12)     Added semaphores to control wait and removed mutexes from loop-
    checking to see if this removed the segfaults
    13)     Removed debugmacros from itkPthreadJob.h
    14)     Removed itkWinJob.h and itkPThreadJob.h and created a single file -
    itkThreadJob.h. Also, changed maps to vectors in itkPThreadPool.cxx.
    Documented variables in itkThreadJob.h
    
    Pending Changes
    1)      Private copy constructor for thread job
    2)      Check to see if PthreadPool and WinThreadPool can inherit from a
    common ThreadPool class
    3)      Document methods and variables
    
    Change-Id: I0eb6abb64a9054373968c226562633cc153db8c2

---
+++ Modules/Core/Common/src/itkPThreadPool.cxx
@@ -0,0 +1,384 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include <stdlib.h>
+#include "itkPThreadPool.h"
+//TODO: Not portable sleep function needs this on unix/mac
+#include <unistd.h>
+
+namespace itk
+{
+SimpleFastMutexLock ThreadPool::m_MutexSync;
+SimpleFastMutexLock ThreadPool::m_MutexWorkCompletion;
+SimpleFastMutexLock ThreadPool::m_ActiveThreadMutex;
+SimpleFastMutexLock ThreadPool::m_ThreadIdAccessMutex;
+
+bool                     ThreadPool::m_InstanceFlag = false;
+SmartPointer<ThreadPool> ThreadPool::m_SThreadPoolInstance = 0;
+
+SmartPointer<ThreadPool> ThreadPool::GetThreadPool()
+{
+  if( !m_InstanceFlag )
+    {
+    m_SThreadPoolInstance = ThreadPool::New();
+    m_SThreadPoolInstance->DebugOn();
+    }
+  return m_SThreadPoolInstance;
+}
+
+SmartPointer<ThreadPool> ThreadPool::GetNewThreadPool()
+{
+  m_SThreadPoolInstance = new ThreadPool();
+  return m_SThreadPoolInstance;
+}
+
+ThreadPool::ThreadPool() :  m_ThreadCount(0), m_IdCounter(1)
+{
+  m_InstanceFlag = true;
+  m_CompletedJobs = 0;
+  m_ScheduleForDestruction = false;
+  m_IncompleteWork = 0;
+  sem_init(&m_WorkAvailable, 0, 0);
+}
+
+void ThreadPool::AddThread()
+{
+  std::cout<<"In add thread"<<std::endl;
+
+  this->m_ThreadCount++;
+  pthread_t aThreadHandle;
+  const int rc = pthread_create(&aThreadHandle, NULL, &ThreadPool::ThreadExecute, (void *)this );
+  if( rc )
+    {
+    itkDebugMacro(<< "ERROR; return code from pthread_create() is " << rc << std::endl);
+    itkExceptionMacro(<< "Cannot create thread. Error in return code from pthread_create()");
+    }
+  else
+    {
+    m_ThreadHandles.push_back(aThreadHandle);
+    itkDebugMacro(<< "Thread created with ptid :" << m_ThreadHandles[m_ThreadCount - 1] << std::endl );
+    }
+}
+
+void ThreadPool::InitializeThreads(int maximumThreads)
+{
+  if( maximumThreads < 1 )
+    {
+    maximumThreads = 1;
+    }
+  for( int i = 0; i < maximumThreads; i++ )
+    {
+    AddThread();
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+
+}
+
+/*
+  This method first checks if all threads are finished executing their jobs.
+  If now, it will wait for "poolTime" seconds and then re-check.
+  Once all are done it will Destroy the thread pool.
+*/
+void ThreadPool::DestroyPool(int pollTime = 2)
+{
+  m_ScheduleForDestruction = true;
+  while( m_IncompleteWork > 0 )
+    {
+    itkDebugMacro(<< "Work is still incomplete=" << m_IncompleteWork << std::endl );
+    // TODO:  sleep is not portable across platforms
+    sleep(pollTime);
+    }
+
+  itkDebugMacro(<< "All threads are done" << std::endl << "Terminating threads" << std::endl );
+  for( int i = 0; i < m_ThreadCount; ++i )
+    {
+    int s = pthread_cancel(m_ThreadHandles[i]);
+    if( s != 0 )
+      {
+      itkDebugMacro(<< "Cannot cancel thread with id : " << m_ThreadHandles[i] << std::endl );
+      }
+    }
+
+  sem_destroy(&m_WorkAvailable);
+}
+
+int ThreadPool::AssignWork(ThreadJob pthreadJob)
+{
+  try
+    {
+      {
+      MutexLockHolder<SimpleFastMutexLock> mutexHolder(m_MutexWorkCompletion);
+      m_IncompleteWork++;
+      }
+    if( m_IncompleteWork > m_ThreadCount )
+      {
+      // If more work is pending than available threads, start a thread
+      AddThread();
+      }
+      {
+      MutexLockHolder<SimpleFastMutexLock> mutexHolderSync(m_MutexSync);
+      // adding to queue
+
+      pthreadJob.Id = m_IdCounter++;
+      m_WorkerQueue.push_back(pthreadJob);
+      itkDebugMacro(
+        << "Assigning Worker[" << pthreadJob.Id << "] Address:[" << &pthreadJob << "] to Queue " <<
+        std::endl );
+      }
+
+      {
+      MutexLockHolder<SimpleFastMutexLock> mutexHolderAct(m_ActiveThreadMutex);
+      itkDebugMacro(<< std::endl << "Adding id " << pthreadJob.Id << " to ActiveThreadIds" << std::endl );
+      m_ActiveJobIds.push_back(pthreadJob.Id );
+      itkDebugMacro(<< std::endl << "ActiveThreadids size : " << m_ActiveJobIds.size() << std::endl );
+      std::cout<< std::endl << "ActiveThreadids size : " << m_ActiveJobIds.size() << std::endl ;
+      }
+    sem_t aSemaphore;
+    sem_init(&aSemaphore, 0, 0);
+    m_JobSemaphores[pthreadJob.Id] = aSemaphore;
+
+    sem_post(&m_WorkAvailable);
+
+    return pthreadJob.Id;
+    }
+  catch( std::exception& e )
+    {
+    m_ExceptionOccured = true;
+    itkDebugMacro(<< std::endl << "Failed to assign work. \n" << e.what() << std::endl );
+    itkExceptionMacro(<< e.what() );
+    }
+}
+
+bool ThreadPool::WaitForThread(int id)
+{
+  bool found = true;
+
+  try
+    {
+    while( found )
+      {
+        {
+        MutexLockHolder<SimpleFastMutexLock> mutexHolderAct(m_ActiveThreadMutex);
+        /*
+        We know that a thread id will be in here because the AssignWork fn adds it in
+        So if a thread id is not found in ActiveThreadIds it means it is done.
+        */
+        found = false;
+        for( std::vector<int>::size_type i = 0; i != m_ActiveJobIds.size(); ++i )
+          {
+          if( m_ActiveJobIds[i] == id )
+            {
+            found = true;
+            break;
+            }
+          }
+
+        }
+      }
+    }
+  catch( std::exception & e )
+    {
+    m_ExceptionOccured = true;
+    itkDebugMacro(
+      << "Exception occured while waiting for job with id : " << id << std::endl << e.what() <<
+      std::endl );
+    }
+  return true;
+}
+
+ThreadJob ThreadPool::FetchWork()
+{
+
+  int indexToReturn = -1;
+
+  sem_wait(&m_WorkAvailable);   // wait to get work
+    {
+    MutexLockHolder<SimpleFastMutexLock> mutexHolderSync(m_MutexSync);
+    // get the index of workerthread from queue
+    for( std::vector<int>::size_type i = 0; i != m_WorkerQueue.size(); ++i )
+      {
+      // if not Assigned already
+      if( m_WorkerQueue[i].Assigned == false )
+        {
+        indexToReturn = i;
+        m_WorkerQueue[i].Assigned = true;
+        break;
+        }
+      }
+    itkDebugMacro(<< std::endl << "Getting work from queue at index : " << indexToReturn << std::endl );
+    ThreadJob ret = m_WorkerQueue[indexToReturn];
+
+    return ret;
+    }
+}
+
+int ThreadPool::GetCompletedJobs() const
+{
+  return m_CompletedJobs;
+}
+
+void ThreadPool::RemoveActiveId(int id)
+{
+  try
+    {
+    itkDebugMacro(
+      << std::endl << "ActiveThreadids size : " << m_ActiveJobIds.size() << ". Removing id " <<
+      id << std::endl );
+      {
+      MutexLockHolder<SimpleFastMutexLock> mutexHolderAct(m_ActiveThreadMutex);
+      int                                  index = -1;
+      itkDebugMacro(<< std::endl << "Looping" << std::endl );
+      for( std::vector<int>::size_type i = 0; i != m_ActiveJobIds.size(); ++i )
+        {
+        itkDebugMacro(<< "Id is : " << m_ActiveJobIds[i] << std::endl );
+        if( m_ActiveJobIds[i] == id )
+          {
+          index = i;
+          break;
+          }
+        }
+
+      if( index >= 0 )
+        {
+        m_ActiveJobIds.erase(m_ActiveJobIds.begin() + index);
+        // increase the count of jobs completed
+        m_CompletedJobs++;
+        itkDebugMacro(
+          << std::endl << "Removed id " << id << " from ActiveThreadIds. Now vector size is " <<
+          m_ActiveJobIds.size()
+          << std::endl );
+        }
+      else
+        {
+
+        itkDebugMacro(<< std::endl << "Error occured, couldnt find id : " << id << std::endl );
+        itkExceptionMacro(
+          << "Error occured in RemoveActiveId, couldnt find id in ActiveThreadIds queue to erase.");
+        }
+      }
+    // Delete from vorker queue
+    bool foundToDelete = false;
+      {
+      MutexLockHolder<SimpleFastMutexLock> mutexHolderSync(m_MutexSync);
+
+      int delIndex = -1;
+      for( std::vector<int>::size_type i = 0; i != m_WorkerQueue.size(); ++i )
+        {
+        if( m_WorkerQueue[i].Id == id )
+          {
+          delIndex = i;
+          foundToDelete = true;
+          break;
+          }
+        }
+
+      m_WorkerQueue.erase(m_WorkerQueue.begin() + delIndex);
+
+      //also removing the job id and thread ids from the vectors
+      int pos = find(m_JobIds.begin(), m_JobIds.end(), id) - m_JobIds.begin();
+        {
+        MutexLockHolder<SimpleFastMutexLock> threadIdMutexHolder(m_ThreadIdAccessMutex);
+        m_JobIds.erase(m_JobIds.begin() + pos);
+        m_ThreadIds.erase(m_ThreadIds.begin() + pos);
+        }
+      itkDebugMacro(
+        << std::endl << "Removed index " << delIndex << " from WorkerQueue. Now vector size is " << m_WorkerQueue.size()
+        << std::endl );
+      }
+    if( foundToDelete == false )
+      {
+      itkDebugMacro(
+        << std::endl << "Error occured, couldnt find id in WorkerQueue to mark executed. Id is : " <<
+        id << std::endl );
+      itkExceptionMacro(<< "Error occured in RemoveActiveId, couldnt find id in WorkerQueue to mark executed. ");
+      }
+    }
+  catch( std::exception & e )
+    {
+    itkExceptionMacro(<< e.what() );
+    }
+}
+
+//we need to wait here through a semaphore because the thread executing the job
+// has to set the map we are accessing
+pthread_t ThreadPool::GetThreadHandleForJob(int jobId){
+  sem_wait(&m_JobSemaphores[jobId]);
+  int pos = find(m_JobIds.begin(), m_JobIds.end(), jobId) - m_JobIds.begin();
+    {
+    MutexLockHolder<SimpleFastMutexLock> threadIdMutexHolder(m_ThreadIdAccessMutex);
+
+    return m_ThreadIds[pos];
+    }
+
+}
+
+//in this method we do not need to wait because it is guranteed that the map
+// will have the value we are finding
+int ThreadPool::GetJobIdForThreadHandle(pthread_t pt){
+
+  int pos = find(m_ThreadIds.begin(), m_ThreadIds.end(), pt) - m_ThreadIds.begin();
+    {
+    MutexLockHolder<SimpleFastMutexLock> threadIdMutexHolder(m_ThreadIdAccessMutex);
+    return m_JobIds[pos];
+    }
+}
+
+void * ThreadPool::ThreadExecute(void *param)
+{
+  // get the parameters passed in
+  ThreadPool *pThreadPool = (ThreadPool *)param;
+  const int   s = pthread_setcancelstate(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
+  if( s != 0 )
+    {
+    }
+
+  while( !pThreadPool->m_ScheduleForDestruction )
+    {
+    try
+      {
+      ThreadJob currentPThreadJob = pThreadPool->FetchWork();
+      //add the thread id and job id to map
+        {
+        MutexLockHolder<SimpleFastMutexLock> threadIdMutexHolder(m_ThreadIdAccessMutex);
+        pThreadPool->m_ThreadIds.push_back(pthread_self() );
+        pThreadPool->m_JobIds.push_back(currentPThreadJob.Id);
+        }
+      sem_post(&(pThreadPool->m_JobSemaphores[currentPThreadJob.Id]) );
+      currentPThreadJob.ThreadFunction(currentPThreadJob.ThreadArgs.otherArgs);
+
+      pThreadPool->RemoveActiveId(currentPThreadJob.Id );
+
+        {
+        MutexLockHolder<SimpleFastMutexLock> mutexHolder(m_MutexWorkCompletion);
+        pThreadPool->m_IncompleteWork--;
+        }
+      }
+    catch( std::exception & e )
+      {
+      pThreadPool->m_ExceptionOccured = true;
+      }
+    }
+
+  return 0;
+}
+
+}
