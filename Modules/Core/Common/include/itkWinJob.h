823ae0ed0979b7ec64b28b5c1b06eb209269647b
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    WIP: Added a windows and linux based Thread pool.
    Fixed a compile time bug
    List of changes:
    1)	Initialize the thread pool with 0 threads and grow
    2)	Allow to add threads to the thread pool
    3)	Always execute a job - no waiting for a thread to be available
    4)      Multithreader accesses the threadpool as singleton
    5)      Removed confusing "GetThreadPool" methods from the ThreadPool
    classes
    6)      Removed itkDebugPrint macro that was added earlier - rolling back
    itkMacro.h to original
    
    Pending Changes
    1)      Mutex locking and unlocking - to verify
    2)      Private copy constructor for thread job
    3)      Format code
    
    Change-Id: I0eb6abb64a9054373968c226562633cc153db8c2

---
+++ Modules/Core/Common/include/itkWinJob.h
@@ -0,0 +1,60 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __itkWinJob_h
+#define __itkWinJob_h
+#if defined(_WIN32) || defined(_WIN64)
+
+
+namespace itk
+{
+
+class WinJob
+{
+public:
+  struct ThreadArgs
+    {
+
+    void *otherArgs;
+    };
+  int    Id;
+  bool   Assigned;
+  bool   Executed;
+  int    Ptid;
+  void * UserData;    // any data user wants the function to use
+
+  unsigned int ( __stdcall *ThreadFunction )( void * ptr );
+
+  ThreadArgs ThreadArgs;
+  WinJob() : Assigned(false),
+             Id(-1),
+             Executed(false),
+             Ptid(-1),
+             UserData(NULL)
+  {
+    ThreadArgs.otherArgs = NULL;
+  }
+
+  ~WinJob()
+  {
+  }
+
+};
+}
+#endif
+#endif
