bf5af2249ff10e128932eae69ab9643bf90eea04
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    WIP: Thread pool.
    NOTE: Cleaned up to be ITK style.
    
    Fixed a compile time bug
    List of changes:
    1)	Initialize the thread pool with 0 threads and grow
    2)	Allow to add threads to the thread pool
    3)	Always execute a job - no waiting for a thread to be available
    4)      Multithreader accesses the threadpool as singleton
    5)      Removed confusing "GetThreadPool" methods from the ThreadPool
    classes
    6)      Removed itkDebugPrint macro that was added earlier - rolling back
    itkMacro.h to original
    7)      Using ITK's SinpleFastMutex and MutexLockHolder instead of doing
    low level mutex by self
    8)      Code formatting
    9)      Removing some of Hans' changes to fix the thread pool
    functionality.
    10)     Added/removed new methods and variables to fix mac build issue -
    removed casting pthread_t to int
    11)     Removing ThreadHandle variable from PThreadJob.h
    12)     Added semaphores to control wait and removed mutexes from loop-
    checking to see if this removed the segfaults
    13)     Removed debugmacros from itkPthreadJob.h
    14)     Removed itkWinJob.h and itkPThreadJob.h and created a single file -
    itkThreadJob.h. Also, changed maps to vectors in itkPThreadPool.cxx.
    Documented variables in itkThreadJob.h
    15)     Refactored and documented methods and variables
    16)     Refactored files to match MultiThreader structure
    17)     Removed semaphores so a lot of code can now be shared in
    itkThreadPool.cxx
    18)     Several fixes for SEGFAULTS and others
    
    ENH: Refactor variable names for class functions
    
    Class member functions should begin with m_ and they
    should be private to the class with accessor functions.
    
    This patch changes the name, but does not protect them
    with member access functions yet.
    
    WIP: Trying vector optimization for
    
    Popping and pushing elements, and pre-allocating space
    for the JobThreads threads.
    
    ENH: Rename mutex variables
    Mutex variables should match the variables that they protect.
    
    ENH: Add class for ThreadStructs
    Improve readabiltiy of code by using a small lightweight
    class instead of the std::pair.
    
    ENH: Improve names of sentinal flags
    Values used to indicates non-running job status are now using enumeration
    to make code easier to read.
    
    ENH: Improve variable names.
    ENH: Remove std::cout printfs
    
    This is a new patch worked on May 4, 2014 attempting to improve
    performance.
    
    WIP: Removed while loops with heavy mutex locking.. Now bringing back
    semaphores. Not tested on windows yet..
    
    Using named semaphores for apple/mac osx
    
    Change-Id: Ibf308b14b37b8727b2878bf65306d01a12b809d6

---
+++ Modules/Core/Common/src/itkWinThreadPool.hxx
@@ -0,0 +1,238 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkWinThreadPool_hxx
+#define __itkWinThreadPool_hxx
+
+namespace itk
+{
+
+ThreadPool
+::ThreadSemaphorePair
+::ThreadSemaphorePair(const ThreadProcessIdType & tph) :
+  m_ThreadProcessHandle(tph)
+{
+
+  m_Semaphore = CreateSemaphore(
+    NULL,     // default security attributes
+    0,        // initial count
+    1000, // maximum count
+    NULL);    // unnamed semaphore
+  if (m_Semaphore == NULL)
+    {
+    itkGenericExceptionMacro(<< "CreateSemaphore error" << GetLastError());
+    }
+}
+
+
+int
+ThreadPool
+::ThreadSemaphorePair::SemaphoreWait()
+{
+  DWORD dwWaitResult = WaitForSingleObject(m_Semaphore,       // handle to semaphore
+                                           INFINITE);
+  switch( dwWaitResult )
+    {
+    // The thread got ownership of the mutex
+    case WAIT_OBJECT_0:
+      return 0; //success
+      break;
+    case WAIT_ABANDONED:
+      return -1;
+    case WAIT_FAILED:
+      return -1;
+    default:
+      return -1;
+    }
+}
+
+int
+ThreadPool
+::ThreadSemaphorePair::SemaphorePost()
+{
+  if(!ReleaseSemaphore(
+       m_Semaphore,   // handle to semaphore
+       1,               // increase count by one
+       NULL))
+    {
+    return -1;
+    }
+  return 0;
+}
+
+
+void
+ThreadPool
+::AddThread()
+{
+  m_ThreadCount++;
+  HANDLE newlyAddedThreadHandle;
+  DWORD  dwThreadId = (DWORD) m_ThreadCount;
+
+  newlyAddedThreadHandle = CreateThread(
+    NULL,
+    0,
+    (LPTHREAD_START_ROUTINE) ThreadPool
+    ::ThreadExecute,     // thread function
+    this,
+    0,
+    &dwThreadId);
+  if( newlyAddedThreadHandle == NULL )
+    {
+    itkDebugMacro(<< "ERROR; adding thread to thread pool");
+    itkExceptionMacro(<< "Cannot create thread.");
+    }
+  else
+    {
+
+    m_ThreadHandles.insert(newlyAddedThreadHandle);
+    m_ThreadIdHandlePairingQueue.push_back(ThreadProcessStatusPair(JOB_THREADHANDLE_JUST_ADDED,
+                                                                   newlyAddedThreadHandle) );
+
+    m_ThreadSemHandlePairingForWaitQueue.push_back(new ThreadSemaphorePair(newlyAddedThreadHandle));
+    m_ThreadSemHandlePairingQueue.push_back(new ThreadSemaphorePair(newlyAddedThreadHandle));
+    }
+
+}
+
+bool
+ThreadPool
+::CompareThreadHandles(ThreadProcessIdType t1, ThreadProcessIdType t2)
+{
+  if (t1 == t2) return true;
+  return false;
+}
+
+void
+ThreadPool
+::DeallocateThreadSemSet(ThreadSemHandlePairingQueueType &q)
+{
+  for (ThreadSemHandlePairingQueueType::iterator it = q.begin(), end = q.end();
+       it != end; ++it)
+    {
+    CloseHandle((*it)->m_Semaphore);
+    delete *it;
+    }
+  q.clear();
+}
+
+
+/*
+    This method first checks if all threads are finished executing their jobs.
+    If now, it will wait for "poolTime" seconds and then re-check.
+    Once all are done it will Destroy the thread pool.
+  */
+void
+ThreadPool
+::DestroyPool(int pollTime = 2)
+{
+  m_ScheduleForDestruction = true;
+  while( m_NumberOfPendingJobsToBeRun > 0 )
+    {
+    itkDebugMacro(<<  "Work is still incomplete=" << m_NumberOfPendingJobsToBeRun << std::endl);
+    Sleep(pollTime);
+    }
+
+  itkDebugMacro(<<  "All threads are done" << std::endl << "Terminating threads" << std::endl);
+  for(ThreadProcessIdContainerType::iterator it = this->m_ThreadHandles.begin();
+      it != this->m_ThreadHandles.end(); ++it)
+    {
+    DWORD dwExit = 0;
+    TerminateThread((*it), dwExit);
+    CloseHandle((*it));
+
+    }
+  DeallocateThreadSemSet(m_ThreadSemHandlePairingQueue);
+  DeallocateThreadSemSet(m_ThreadSemHandlePairingForWaitQueue);
+
+  //clear vectors
+  //clear vectors
+  m_ThreadIdHandlePairingQueue.clear();
+  ThreadIdHandlePairingContainerType(m_ThreadIdHandlePairingQueue).swap(m_ThreadIdHandlePairingQueue);
+
+  m_ThreadHandles.clear();
+  ThreadProcessIdContainerType(m_ThreadHandles).swap(m_ThreadHandles);
+
+  m_ActiveJobIds.clear();
+  ThreadJobIdsContainerType(m_ActiveJobIds).swap(m_ActiveJobIds);
+
+  m_WorkerQueue.clear();
+  ThreadJobContainerType(m_WorkerQueue).swap(m_WorkerQueue);
+}
+
+HANDLE
+ThreadPool::
+GetThreadHandleForThreadId(ThreadIdType id)
+{
+
+  MutexLockHolder<SimpleFastMutexLock> threadStructMutexHolder(m_ThreadIdHandlePairingQueueMutex);
+  for (ThreadIdHandlePairingContainerType::iterator tpIt = m_ThreadIdHandlePairingQueue.begin(),
+         end = m_ThreadIdHandlePairingQueue.end(); tpIt != end; ++tpIt)
+    {
+    if (tpIt->m_ThreadNumericId)
+      {
+      return tpIt->m_ThreadProcessHandle;
+      }
+    }
+  itkExceptionMacro(<< "Error occured finding thread handle for thread id " << id);
+}
+
+void *
+ThreadPool
+::ThreadExecute(void *param)
+{
+  // get the parameters passed inf
+  //ThreadPool *winThreadPool = (ThreadPool *)param;
+  //ThreadPool::ThreadArgStruct *threadStruct = (ThreadPool::ThreadArgStruct *)param;
+  ThreadPool *winThreadPool = (ThreadPool *)param;
+  ThreadIdType myId;
+  ThreadProcessIdType threadId = GetCurrentThread();
+  ThreadIdHandlePairingContainerType::iterator tpIter = winThreadPool->m_ThreadIdHandlePairingQueue.begin(),
+    end = winThreadPool->m_ThreadIdHandlePairingQueue.end();
+  for (; tpIter != end; ++tpIter)
+    {
+    if (tpIter->m_ThreadProcessHandle == threadId)
+      {
+      myId = tpIter->m_ThreadNumericId;
+      break;
+      }
+    }
+
+  HANDLE handle = winThreadPool->GetThreadHandleForThreadId(myId);
+
+  while( !winThreadPool->m_ScheduleForDestruction )
+    {
+    ThreadJob currentWinJob = winThreadPool->FetchWork( handle);
+    if( currentWinJob.m_Id < 0 || currentWinJob.m_Assigned == false )
+      {
+      itkDebugStatement(std::cerr << "In thread pool thread : Empty job returned from FetchWork so ignoring and continuing .."
+                        << std::endl);
+      continue;
+      }
+    currentWinJob.m_ThreadFunction(currentWinJob.m_UserData);
+    winThreadPool->RemoveActiveId(currentWinJob.m_Id);
+    //signal that current job is done
+    if(winThreadPool->GetSemaphoreForThreadWait(handle )->SemaphorePost() != 0)
+      {
+      itkGenericExceptionMacro(<<"******************************Error in semaphore post");
+      }
+    }
+  return 0;
+}
+
+}
+#endif // __itkWinThreadPool_hxx
