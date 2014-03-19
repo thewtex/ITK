9e722380ffef823d9102869ec9ffa16d85a594d0
Umang B <brahmaks@ge.com>
2014-03-19 10:13:10 -0400

    WIP: Added a windows and linux based Thread pool.
    NOTE: Cleaned up to be ITK style.
    
    NOTE: Checking in this "INCOMPLETE" patch to see what build errors are
    found on a mac.
    
    Fixed a compile time bug
    List of changes:
    1)	Initialize the thread pool with 0 threads and grow
    2)	Allow to add threads to the thread pool
    3)	Always execute a job - no waiting for a thread to be available
    4)      Multithreader accesses the threadpool as singleton
    5)      Removed confusing "GetThreadPool" methods from the ThreadPool
    classes
    6)      Removed itkDebugPrint macro that was added earlier - rolling back
    itkMacro.h to original
    7)      Using ITK's SinpleFastMutex and MutexLockHolder instead of doing
    low level mutex by self
    8)      Code formatting
    9)      Removing some of Hans' changes to fix the thread pool
    functionality.
    10)     Added/removed new methods and variables to fix mac build issue -
    removed casting pthread_t to int
    11)     Removing ThreadHandle variable from PThreadJob.h
    12)     Added semaphores to control wait and removed mutexes from loop-
    checking to see if this removed the segfaults
    13)     Removed debugmacros from itkPthreadJob.h
    14)     Removed itkWinJob.h and itkPThreadJob.h and created a single file -
    itkThreadJob.h. Also, changed maps to vectors in itkPThreadPool.cxx.
    Documented variables in itkThreadJob.h
    15)     Refactored and documented methods and variables
    16)     Refactored files to match MultiThreader structure
    17)     Removed semaphores so a lot of code can now be shared in
    itkThreadPool.cxx
    18)     Several fixes for SEGFAULTS and others
    
    ENH: Refactor variable names for class functions
    
    Class member functions should begin with m_ and they
    should be private to the class with accessor functions.
    
    This patch changes the name, but does not protect them
    with member access functions yet.
    
    Change-Id: I0eb6abb64a9054373968c226562633cc153db8c2

---
+++ Modules/Core/Common/src/itkWinThreadPool.cxx
@@ -0,0 +1,111 @@
+/*=========================================================================
+*
+*  Copyright Insight Software Consortium
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*         http://www.apache.org/licenses/LICENSE-2.0.txt
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*=========================================================================*/
+
+#include <stdlib.h>
+#include "itkThreadPool.h"
+
+namespace itk
+{
+
+void ThreadPool::AddThread()
+{
+  this->m_ThreadCount++;
+  HANDLE aThreadHandle;
+  DWORD  dwThreadId;
+  aThreadHandle = CreateThread(
+      NULL,                                                   // default
+                                                              // security
+                                                              // attributes
+      0,                                                      // use default
+                                                              // stack size
+      (LPTHREAD_START_ROUTINE) ThreadPool::ThreadExecute,     // thread function
+                                                              // name
+      this,                                                   // argument to
+                                                              // thread function
+      0,                                                      // use default
+                                                              // creation flags
+      &dwThreadId);                                           // returns the
+                                                              // thread
+                                                              // identifier
+  if( aThreadHandle == NULL )
+    {
+    ExitProcess(3);
+    }
+    {
+    MutexLockHolder<SimpleFastMutexLock> threadStructMutexHolder(m_VectorQMutex);
+    m_ThreadHandles.push_back(aThreadHandle);
+    m_ThreadStructs.push_back(std::make_pair(-2, aThreadHandle) );
+    }
+  itkDebugMacro(<< "Thread created with handle :" << m_ThreadHandles[m_ThreadCount - 1] << std::endl );
+}
+
+/*
+This method first checks if all threads are finished executing their jobs.
+If now, it will wait for "poolTime" seconds and then re-check.
+Once all are done it will Destroy the thread pool.
+*/
+void ThreadPool::DestroyPool(int pollTime = 2)
+{
+  m_ScheduleForDestruction = true;
+  while( m_IncompleteWork > 0 )
+    {
+    itkDebugMacro(<<  "Work is still incomplete=" << m_IncompleteWork << std::endl);
+    Sleep(pollTime);
+    }
+
+  itkDebugMacro(<<  "All threads are done" << std::endl << "Terminating threads" << std::endl);
+  for( int i = 0; i < m_ThreadCount; i++ )
+    {
+    DWORD dwExit = 0;
+    TerminateThread(m_ThreadHandles[i], dwExit);
+    CloseHandle(m_ThreadHandles[i]);
+
+    }
+}
+
+void * ThreadPool::ThreadExecute(void *param)
+{
+  // get the parameters passed in
+  ThreadPool *winThreadPool = (ThreadPool *)param;
+
+  while( !winThreadPool->m_ScheduleForDestruction )
+    {
+
+    ThreadJob currentWinJob = winThreadPool->FetchWork(GetCurrentThread() );
+    if( currentWinJob.Id < 0 || currentWinJob.Assigned == false )
+      {
+      std::cout << "\nIn thread pool thread : Empty job returned from FetchWork so ignoring and continuing ..\n";
+      continue;
+      }
+    std::cout << "\n In thread pool thread " << GetCurrentThread() << " : Work fetched. Job id is : "
+              << currentWinJob.Id
+              << std::endl;
+    currentWinJob.ThreadFunction(currentWinJob.ThreadArgs.otherArgs);
+    std::cout << "\n Thread done with job id :" << currentWinJob.Id << "\n Now removing...\n\n";
+    winThreadPool->RemoveActiveId(currentWinJob.Id);
+      {
+      MutexLockHolder<SimpleFastMutexLock> mutexHolderSync(m_MutexWorkCompletion);
+      winThreadPool->m_IncompleteWork--;
+      }
+
+    }
+
+  return 0;
+}
+
+}
