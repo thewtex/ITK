14ee79ee78fc0996733a51f5aeebac11d2d5c24b
Matt McCormick <matt.mccormick@kitware.com>
2014-06-08 21:01:46 -0400

    WIP: Move FFTComplexToComplexImageFilter out of Review.
    The two tests were consolidated by using templates to test both 2 and 3
    dimensions and both float and double based pixel types.
    
    The test was not previous executed.  It is now executed with a comparison to
    a baseline.
    
    Likely due to copy/paste from the RealToComplex class of FFT's there was code
    related to possible truncated output due to symmetry. However, since it is a
    full complex to complex transform, we do not presume there will be symmetry
    and only support the full transform.
    
    Change-Id: Ia52a0a39f01d04f5e4c66a7d0e4d67d8d6048e2f

--- Modules/Filtering/FFT/include/itkFFTComplexToComplexImageFilter.hxx
+++ Modules/Filtering/FFT/include/itkFFTComplexToComplexImageFilter.hxx
@@ -49,7 +49,7 @@ typename FFTComplexToComplexImageFilter< TImage >::Pointer
 FFTComplexToComplexImageFilter< TImage >
 ::New(void)
 {
-  Pointer smartPtr = ::itk::ObjectFactory< Self >::Create();
+  Pointer smartPtr = ObjectFactory< Self >::Create();
 
 #ifdef ITK_USE_FFTWD
   if ( smartPtr.IsNull() )
@@ -77,71 +77,13 @@ FFTComplexToComplexImageFilter< TImage >
 
 template< typename TImage >
 void
-FFTComplexToComplexImageFilter< TImage >::GenerateOutputInformation()
-{
-  // call the superclass' implementation of this method
-  Superclass::GenerateOutputInformation();
-  //
-  // If this implementation returns a full result
-  // instead of a 'half-complex' matrix, then none of this
-  // is necessary
-  if ( this->FullMatrix() )
-    {
-    return;
-    }
-
-  // get pointers to the input and output
-  typename InputImageType::ConstPointer inputPtr  = this->GetInput();
-  typename OutputImageType::Pointer outputPtr = this->GetOutput();
-
-  if ( !inputPtr || !outputPtr )
-    {
-    return;
-    }
-
-  //
-  // This is all based on the same function in itk::ShrinkImageFilter
-  // ShrinkImageFilter also modifies the image spacing, but spacing
-  // has no meaning in the result of an FFT. For an IFFT, since the
-  // spacing is propagated to the complex result, we can use the spacing
-  // from the input to propagate back to the output.
-  unsigned int i;
-  const typename InputImageType::SizeType &   inputSize =
-    inputPtr->GetLargestPossibleRegion().GetSize();
-  const typename InputImageType::IndexType &  inputStartIndex =
-    inputPtr->GetLargestPossibleRegion().GetIndex();
-
-  typename OutputImageType::SizeType outputSize;
-  typename OutputImageType::IndexType outputStartIndex;
-
-  //
-  // Size of output FFT:C2C is the same as input
-  //
-
-  outputSize[0] = inputSize[0];
-  outputStartIndex[0] = inputStartIndex[0];
-
-  for ( i = 1; i < OutputImageType::ImageDimension; i++ )
-    {
-    outputSize[i] = inputSize[i];
-    outputStartIndex[i] = inputStartIndex[i];
-    }
-  typename OutputImageType::RegionType outputLargestPossibleRegion;
-  outputLargestPossibleRegion.SetSize(outputSize);
-  outputLargestPossibleRegion.SetIndex(outputStartIndex);
-
-  outputPtr->SetLargestPossibleRegion(outputLargestPossibleRegion);
-}
-
-template< typename TImage >
-void
 FFTComplexToComplexImageFilter< TImage >::GenerateInputRequestedRegion()
 {
   Superclass::GenerateInputRequestedRegion();
   // get pointers to the input and output
-  typename InputImageType::Pointer inputPtr  =
+  typename InputImageType::Pointer input  =
     const_cast< InputImageType * >( this->GetInput() );
-  inputPtr->SetRequestedRegionToLargestPossibleRegion();
+  input->SetRequestedRegionToLargestPossibleRegion();
 }
 }
 #endif
