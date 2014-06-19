bf77e2a07d92ed22f185e981e6f3a526ef38cbc1
Matt McCormick <matt.mccormick@kitware.com>
2014-06-19 11:42:14 -0400

    WIP: Add Sphinx examples as a Remote Module.
    Executables are created in Modules/Remote/SphinxExamples/bin.
    
    Change-Id: I3e8e13b01420f96ffcd30bf72df34f1cc7bf048d

---
+++ Modules/Remote/SphinxExamples.remote.cmake
@@ -0,0 +1,7 @@
+# Contact: Matt McCormick <matt.mccormick@kitware.com>
+itk_fetch_module(SphinxExamples
+  "This module builds the examples found at http://itk.org/ITKExamples/"
+  GIT_REPOSITORY http://itk.org/ITKExamples.git
+  # 2014-06-19
+  GIT_TAG master
+  )
