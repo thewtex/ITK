dca95adbce17742d81daf0030d8d642c3f0a2609
David Froger <david.froger@inria.fr>
2014-08-06 21:46:23 +0200

    ENH: Add observer on HDF5 regions when streaming.
    HDF5 supports reading and writing in a subregion of an array stored in a file
    via the use of the selectHyperslab method. The HDF5 reader/writer module in ITK
    can make use of this HDF5 feature to implement ITK streaming.
    
    This commit add an IterationEvent invoked by HDF5ImageIO when it modifies the
    m_H5RegionStart and m_H5RegionSize arrays before reading/writing. These arrays,
    which are passed to the HDF5 functions, define the subregion to read/write.
    
    This commit also add the class HDF5CommandRegionRecorder, which can be added as
    an observer of the IterationEvent to record the start of size of the
    read/written regions, which provides usefull informations to debug and test
    the streaming capability of this module.
    
    It is used in the test itkHDF5ImageIOStreamingReadWriteTest.cxx, which shows
    that streaming correctly works when reading HDF5 file, but do no work when
    writing file, because the writer input is not streamable.

--- Modules/IO/HDF5/include/itkHDF5ImageIO.h
+++ Modules/IO/HDF5/include/itkHDF5ImageIO.h
@@ -40,6 +40,8 @@ class DataSet;
 }
 
 #include "itkStreamingImageIOBase.h"
+#include <itkCommand.h>
+#include "itk_H5Cpp.h"
 
 namespace itk
 {
@@ -130,6 +132,10 @@ public:
    * that the IORegions has been set properly. */
   virtual void Write(const void *buffer) ITK_OVERRIDE;
 
+  itkGetConstMacro(H5RegionDim,const int);
+  itkGetConstMacro(H5RegionStart,const hsize_t*);
+  itkGetConstMacro(H5RegionSize,const hsize_t*);
+
 protected:
   HDF5ImageIO();
   ~HDF5ImageIO();
@@ -190,7 +196,141 @@ private:
   H5::H5File  *m_H5File;
   H5::DataSet *m_VoxelDataSet;
   bool         m_ImageInformationWritten;
+
+  int m_H5RegionDim;
+  hsize_t m_H5RegionStart[4], m_H5RegionSize[4];
 };
+
+/** \class HDF5CommandRegionRecorder
+ *
+ * \author David Froger
+ *
+ * \brief Records debug informations about HDF5 read/write operation.
+ *
+ * \ingroup ITKIOHDF5
+ *
+ * HDF5 supports reading and writing in a subregion of an multidimensional
+ * array stored in a file via the use of the selectHyperslab method.
+ * The HDF5 reader/writer module in ITK make use of this HDF5 feature to
+ * implement ITK streaming/pasting.
+ *
+ * HDF5ImageIO invokes an IterationEvent when it modifies the m_H5RegionStart
+ * and m_H5RegionSize arrays. These arrays, which are passed to the HDF5
+ * functions, define the subregion to read/write. Adding this class as an
+ * observer of the IterationEvent allows to record the start of size of the
+ * read/written regions, which provides usefull informations to debug and
+ * test the streaming/pasting capability of this module.
+ */
+class HDF5CommandRegionRecorder : public itk::Command
+{
+public:
+  /** Standard class typedefs. */
+  typedef HDF5CommandRegionRecorder                    Self;
+  typedef itk::Command                                 Superclass;
+  typedef itk::SmartPointer<HDF5CommandRegionRecorder> Pointer;
+
+  /** Type of the start or size of a region*/
+  typedef std::vector<hsize_t> H5IndexType;
+
+  /** Type of a list of H5IndexType*/
+  typedef std::vector<H5IndexType> H5IndexHistoryType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro( HDF5CommandRegionRecorder );
+
+protected:
+  HDF5CommandRegionRecorder() {};
+
+public:
+  /** Record start and size of the current read/written region*/
+  void Execute(itk::Object *caller, const itk::EventObject & event)
+    {
+    Execute( (const itk::Object *)caller, event);
+    }
+
+  /** Record start and size of the current read/written region*/
+  void Execute(const itk::Object * object, const itk::EventObject & event)
+  {
+    const itk::HDF5ImageIO * filter = dynamic_cast< const itk::HDF5ImageIO * >( object );
+    if( !(itk::IterationEvent().CheckEvent( &event )) )
+      {
+      return;
+      }
+
+      H5IndexType H5RegionStart;
+      H5IndexType H5RegionSize;
+
+      for (int i=0; i<filter->GetH5RegionDim(); i++)
+        {
+        H5RegionStart.push_back( filter->GetH5RegionStart()[i] );
+        H5RegionSize.push_back( filter->GetH5RegionSize()[i] );
+        }
+
+      m_StartHistory.push_back( H5RegionStart );
+      m_SizeHistory.push_back( H5RegionSize );
+  }
+
+  /** Print history of all the read/written regions */
+  void PrintHistory(const H5IndexHistoryType &startHistory,
+    const H5IndexHistoryType &sizeHistory,
+    std::ostream &out)
+  {
+    for (unsigned it = 0; it < sizeHistory.size(); it++)
+      {
+      out << "Iteration: " << it << " ";
+      H5IndexType H5RegionStart = startHistory[it];
+      H5IndexType H5RegionSize = sizeHistory[it];
+      out << "start: [";
+      for (unsigned i = 0; i < H5RegionStart.size(); i++)
+        {
+          out << H5RegionStart[i] << " ";
+        }
+      out << "] Size: [";
+      for (unsigned i = 0; i < H5RegionSize.size(); i++)
+        {
+          out << H5RegionSize[i] << " ";
+        }
+        out << "]" << std::endl;
+      }
+  }
+
+  /** Print self history of all the read/written regions */
+  void PrintSelfHistory(std::ostream &out)
+  {
+    this->PrintHistory(this->m_StartHistory, this->m_SizeHistory, out);
+  }
+
+  /** Compare history of all the read/written regions with expected one
+   * \return Returns true if this history is as expected.
+   */
+  bool CheckHistory(const H5IndexHistoryType &expectedStartHistory,
+    const H5IndexHistoryType &expectedSizeHistory,
+    std::ostream &out)
+  {
+    if (m_StartHistory == expectedStartHistory and m_SizeHistory == expectedSizeHistory)
+      {
+      out << "As expected, history is: " << std::endl;
+      this->PrintHistory(m_StartHistory, m_SizeHistory, out);
+      return true;
+      }
+    else
+      {
+      out << "Expected history:" << std::endl;
+      this->PrintHistory(expectedStartHistory, expectedSizeHistory, out);
+      out << "But got:" << std::endl;
+      this->PrintHistory(m_StartHistory, m_SizeHistory, out);
+      return false;
+      }
+  }
+
+private:
+  /** Historic of the start of the read/write regions */
+  H5IndexHistoryType m_StartHistory;
+
+  /** Historic of the size of the read/write regions */
+  H5IndexHistoryType m_SizeHistory;
+};
+
 } // end namespace itk
 
 #endif // __itkHDF5ImageIO_h
