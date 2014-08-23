f0c6359d2b6c10f131cb8e00ed5258b4c4d1b277
Michka Popoff <michkapopoff@gmail.com>
2014-08-23 18:59:10 +0200

    ENH: Add new Info() method to the template class
    This allows to know what types are wrapped for a class. This is useful because you don't
    have to go and look in the .wrap file, and we currently don't build documentation for the wrappings.
    
    This method could be extended to display more details and informations, like the list of available methods in the class.
    
    I also removed unused sys imports and the unused has_key method.
    
    Info usage Example:
    
    >>> import itk
    >>> itk.Image.Info()
    <itkTemplate itk::Image>
    Options:
      [<itkCType double>, 2]
      [<itkCType double>, 3]
      [<itkCType float>, 2]
      [<itkCType float>, 3]
      [<itkCType signed short>, 2]
      [<itkCType signed short>, 3]
      [<itkCType unsigned char>, 2]
      [<itkCType unsigned char>, 3]
      [<itkCType unsigned long>, 2]
      [<itkCType unsigned long>, 3]
      [<class 'itkCovariantVectorPython.itkCovariantVectorF2'>, 2]
      [<class 'itkCovariantVectorPython.itkCovariantVectorF3'>, 3]
      [<class 'itkFixedArrayPython.itkFixedArrayF2'>, 2]
      [<class 'itkFixedArrayPython.itkFixedArrayF3'>, 3]
      [<class 'itkOffsetPython.itkOffset2'>, 2]
      [<class 'itkOffsetPython.itkOffset3'>, 3]
      [<class 'itkRGBAPixelPython.itkRGBAPixelUC'>, 2]
      [<class 'itkRGBAPixelPython.itkRGBAPixelUC'>, 3]
      [<class 'itkRGBPixelPython.itkRGBPixelUC'>, 2]
      [<class 'itkRGBPixelPython.itkRGBPixelUC'>, 3]
      [<class 'itkSparseFieldFourthOrderLevelSetImageFilterPython.itkNormalBandNodeIF2'>, 2]
      [<class 'itkSparseFieldFourthOrderLevelSetImageFilterPython.itkNormalBandNodeIF3'>, 3]
      [<class 'itkSymmetricSecondRankTensorPython.itkSymmetricSecondRankTensorD2'>, 2]
      [<class 'itkSymmetricSecondRankTensorPython.itkSymmetricSecondRankTensorD3'>, 3]
      [<class 'itkVectorPython.itkVectorD2'>, 2]
      [<class 'itkVectorPython.itkVectorD2'>, 3]
      [<class 'itkVectorPython.itkVectorD3'>, 3]
      [<class 'itkVectorPython.itkVectorD3'>, 4]
      [<class 'itkVectorPython.itkVectorF2'>, 2]
      [<class 'itkVectorPython.itkVectorF3'>, 3]
      [<class 'vcl_complexPython.vcl_complexF'>, 2]
      [<class 'vcl_complexPython.vcl_complexF'>, 3]
    
    Change-Id: I4322c588bbc605dd802ddc7215a56f6777d31c8b

--- Wrapping/Generators/Python/itkTemplate.py
+++ Wrapping/Generators/Python/itkTemplate.py
@@ -20,7 +20,6 @@ from __future__ import print_function
 
 import types
 import inspect
-import sys
 import os
 import warnings
 import itkConfig
@@ -326,13 +325,6 @@ class itkTemplate(object):
         for k in self.keys():
             yield k
 
-    def has_key(self, key):
-        try:
-            value = self[key]
-        except KeyError:
-            return False
-        return True
-
     def __contains__(self, key):
         return key in self
 
@@ -364,10 +356,34 @@ class itkTemplate(object):
     def __len__(self):
         return len(self.keys())
 
+    def Info(self):
+        """Helper method which prints out the allowed template parameters."""
+
+        print("<itkTemplate %s>" % self.__name__)
+        print("Options:")
+
+        # Make a list of allowed types, and sort them
+        ctypes = []
+        classes = []
+        for key_tuple in self.__template__:
+            key = str(key_tuple)
+            if "itkCType" in key:
+                ctypes.append(key)
+            elif "class" in key:
+                classes.append(key)
+
+        # Sort the lists
+        ctypes = sorted(ctypes)
+        classes = sorted(classes)
+
+        for ct in ctypes:
+            print("  " + str(ct).replace("(", "[").replace(")", "]"))
+        for cl in classes:
+            print("  " + str(cl).replace("(", "[").replace(")", "]"))
+
 
 # create a new New function which accepts parameters
 def New(self, *args, **kargs):
-    import sys
     import itk
 
     itk.set_inputs(self, args, kargs)
